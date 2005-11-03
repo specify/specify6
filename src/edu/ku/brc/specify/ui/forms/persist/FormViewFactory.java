39ce94904daa3cb104cd070dbe0ee421f7cf9bfa
Rod Spears <rods@ku.edu>
2005-11-03 21:56:28 +0000

    added gpl
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@93 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/ui/forms/persist/FormViewFactory.java
+++ src/edu/ku/brc/specify/ui/forms/persist/FormViewFactory.java
@@ -1,21 +1,35 @@
+/* Filename:    $RCSfile: FormViewFactory,v $
+ * Author:      $Author: rods $
+ * Revision:    $Revision: 1.1 $
+ * Date:        $Date: 2005/10/12 16:52:27 $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package edu.ku.brc.specify.ui.forms.persist;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.*;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Vector;
 
 import org.apache.commons.betwixt.XMLIntrospector;
 import org.apache.commons.betwixt.io.BeanWriter;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
-import org.dom4j.Attribute;
-import org.dom4j.DocumentException;
 import org.dom4j.Element;
-import org.dom4j.Node;
 
-import edu.ku.brc.specify.InitializeData;
-import edu.ku.brc.specify.prefs.PrefGroup;
 import edu.ku.brc.specify.exceptions.ConfigurationException;
 
 public class FormViewFactory
