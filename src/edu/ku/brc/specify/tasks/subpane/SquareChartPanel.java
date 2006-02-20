a21fe8fddbabd17bd855b0450799906adf138ee1
Rod Spears <rods@ku.edu>
2006-02-20 15:39:53 +0000

    initial
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@177 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/tasks/subpane/SquareChartPanel.java
@@ -0,0 +1,75 @@
+/* Filename:    $RCSfile: SquareChartPanel.java,v $
+ * Author:      $Author: rods $
+ * Revision:    $Revision: 1.1 $
+ * Date:        $Date: 2005/10/19 19:59:54 $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ku.brc.specify.tasks.subpane;
+
+import java.awt.Rectangle;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+
+/**
+ * Simple derived class from ChartPanel that enables the Chart to always be a square instead of being stretched.
+ * At this time it always sets the width to the height.
+ *
+ * @author rods
+ *
+ */
+public class SquareChartPanel extends ChartPanel
+{
+
+    public SquareChartPanel(JFreeChart arg0)
+    {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    public SquareChartPanel(JFreeChart arg0, boolean arg1)
+    {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+
+    public SquareChartPanel(JFreeChart arg0, boolean arg1, boolean arg2,
+            boolean arg3, boolean arg4, boolean arg5)
+    {
+        super(arg0, arg1, arg2, arg3, arg4, arg5);
+        // TODO Auto-generated constructor stub
+    }
+
+    public SquareChartPanel(JFreeChart arg0, int arg1, int arg2, int arg3,
+            int arg4, int arg5, int arg6, boolean arg7, boolean arg8,
+            boolean arg9, boolean arg10, boolean arg11, boolean arg12)
+    {
+        super(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9,
+                arg10, arg11, arg12);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setBounds(Rectangle r)
+    {
+        setBounds(r.x, r.y, r.height, r.height);
+    }
+
+    public void setBounds(int x, int y, int width, int height)
+    {
+        super.setBounds(x, y, height, height);
+    }
+
+}
