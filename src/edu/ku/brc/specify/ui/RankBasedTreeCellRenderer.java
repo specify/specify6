f8f6e7d4c48b54d54d2d9edb97e19fd781bb7610
Josh Stewart <jds@ku.edu>
2006-03-24 17:04:25 +0000

    Initial import of RankBasedTreeCellRenderer.  This renderer takes in a Map from ranks to Icons for use when rendering the node icons.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@232 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/ui/RankBasedTreeCellRenderer.java
@@ -0,0 +1,80 @@
+package edu.ku.brc.specify.ui;
+
+import java.awt.Component;
+import java.util.Map;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import edu.ku.brc.specify.datamodel.Treeable;
+
+/**
+ * A DefaultTreeCellRenderer that queries a Map to determine which Icon
+ * to use to display nodes of a given rank.  This renderer assumes that
+ * the underlying tree contains DefaultMutableTreeNodes whos user objects
+ * implement the treeable interface.
+ * 
+ * @author jstewart
+ */
+public class RankBasedTreeCellRenderer extends DefaultTreeCellRenderer
+{
+	Map<Integer,Icon> iconMap;
+	
+	/**
+	 * Creates a new RandBasedTreeCellRenderer using the passed in Map to
+	 * determine which icon to use for each rank
+	 * 
+	 * @param ranksToIcons
+	 */
+	public RankBasedTreeCellRenderer( Map<Integer,Icon> ranksToIcons )
+	{
+		super();
+		iconMap = ranksToIcons;
+	}
+
+	/**
+	 * Configures the renderer based on the underlying DefaultTreeCellRenderers choices
+	 * and modifies the node icon based on the rank of <i>value</i>, which must be
+	 * a DefaultMutableTreeNode containing a Treeable object as the user object.
+	 * 
+	 * @see javax.swing.tree.TreeCellRenderer#getTreeCellRendererComponent(javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, int, boolean)
+	 */
+	@Override
+    public Component getTreeCellRendererComponent(JTree tree,
+    												Object value,
+    												boolean sel,
+    												boolean expanded,
+    												boolean leaf,
+    												int row,
+    												boolean hasFocus)
+    {
+	    // TODO Auto-generated method stub
+	    Component c = super.getTreeCellRendererComponent(tree,
+	    												value,
+	    												sel,
+	    												expanded,
+	    												leaf,
+	    												row,
+	    												hasFocus);
+	    
+	    DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+	    Treeable t = (Treeable)node.getUserObject();
+	    	    
+	    JLabel l = (JLabel)c;
+	    l.setText(t.getName());
+	    
+	    if( t.getRankId() != null )
+	    {
+	    	Icon icon = iconMap.get(t.getRankId());
+	    	if( icon != null )
+	    	{
+	    		l.setIcon(icon);
+	    	}
+	    }
+
+	    return l;
+    }
+}
