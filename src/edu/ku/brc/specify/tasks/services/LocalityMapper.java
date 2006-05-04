aa49be7cf0547a8ee38c53ac10e643f25f9b1d5c
Josh Stewart <jds@ku.edu>
2006-05-04 14:59:10 +0000

    Added the MapGrabber and LocalityMapper to the tasks.services package
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@440 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/tasks/services/LocalityMapper.java
@@ -0,0 +1,554 @@
+package edu.ku.brc.specify.tasks.services;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.ku.brc.specify.datamodel.Locality;
+import edu.ku.brc.specify.ui.GraphicsUtils;
+import edu.ku.brc.specify.ui.SimpleCircleIcon;
+import edu.ku.brc.util.Pair;
+
+public class LocalityMapper
+{
+	private static Log log = LogFactory.getLog(LocalityMapper.class);
+	
+	protected List<Locality> localities;
+	protected List<String> labels;
+	protected List<Point> markerLocations;
+	protected MapGrabber mapGrabber;
+	
+	// the actual bounds of the collecting localities
+	protected double minLat;
+	protected double maxLat;
+	protected double minLong;
+	protected double maxLong;
+	
+	// the bounds of the map after including a 5% buffer region
+	protected double mapMinLat;
+	protected double mapMaxLat;
+	protected double mapMinLong;
+	protected double mapMaxLong;
+	
+	protected double mapLatRange;
+	protected double mapLongRange;
+	
+	protected double pixelPerLatRatio;
+	protected double pixelPerLongRatio;
+	
+	protected Integer mapWidth;
+	protected Integer mapHeight;
+	
+	protected int mostRecentPaintedX;
+	protected int mostRecentPaintedY;
+
+	// an icon for the little marker dots
+	// this icon will get repainted in each location
+	protected SimpleCircleIcon marker;
+	
+	// some configuration of the image
+	protected boolean showArrows;
+	protected boolean showLabels;
+	protected Color arrowColor;
+	protected Color labelColor;
+	
+	// the cached information
+	protected Icon mapIcon;
+	protected boolean cacheValid;
+	
+	protected String exceptionText;
+	
+	public LocalityMapper()
+	{
+		minLat = -90;
+		minLong = -180;
+		maxLat = 90;
+		maxLong = 180;
+		
+		this.localities = new Vector<Locality>();
+		this.labels = new Vector<String>();
+		this.markerLocations = new Vector<Point>();
+		this.mapGrabber = new MapGrabber();
+		
+		showArrows = true;
+		showLabels = true;
+		
+		labelColor = Color.BLACK;
+		arrowColor = Color.BLACK;
+		
+		cacheValid = false;
+		
+		marker = new SimpleCircleIcon(8,Color.BLACK);
+	}
+	
+	public LocalityMapper( List<Locality> localities )
+	{
+		this();
+		this.localities.addAll(localities);
+		for( int i = 0; i < localities.size(); ++i )
+		{
+			labels.add(null);
+		}
+		for( int i = 0; i < localities.size(); ++i )
+		{
+			markerLocations.add(null);
+		}
+		recalculateBoundingBox();
+	}
+	
+	public LocalityMapper( List<Locality> localities, List<String> labels )
+	{
+		this();
+		this.localities.addAll(localities);
+		this.labels.addAll(labels);
+		for( int i = 0; i < localities.size(); ++i )
+		{
+			markerLocations.add(null);
+		}
+		recalculateBoundingBox();
+	}
+	
+	public void addLocalityAndLabel( Locality loc, String label )
+	{
+		localities.add(loc);
+		labels.add(label);
+		
+		Point iconLoc = determinePixelCoordsOfLocality(loc);
+		markerLocations.add(iconLoc);
+		
+		if( localities.size() == 1 )
+		{
+			recalculateBoundingBox();
+		}
+		
+		// instead of recalculating the bounding box from scratch
+		// just factor in how this new data point might change it
+		Pair<Double,Double> latLong = getLatLong(loc);
+		double lat = latLong.first;
+		double lon = latLong.second;
+		
+		if( lat < minLat )
+		{
+			minLat = lat;
+			cacheValid = false;
+		}
+		if( lat > maxLat )
+		{
+			maxLat = lat;
+			cacheValid = false;
+		}
+		if( lon < minLong )
+		{
+			minLong = lon;
+			cacheValid = false;
+		}
+		if( lon > maxLong )
+		{
+			maxLong = lon;
+			cacheValid = false;
+		}
+		
+		createBoundingBoxBufferRegion();
+	}
+
+	public void removeLocalityAndLabel( Locality loc )
+	{
+		int index = localities.indexOf(loc);
+		localities.remove(index);
+		labels.remove(index);
+		markerLocations.remove(index);
+
+		Pair<Double,Double> latLong = getLatLong(loc);
+		double lat = latLong.first;
+		double lon = latLong.second;
+		
+		if( minLat == lat || maxLat == lat || minLong == lon || maxLong == lon )
+		{
+			recalculateBoundingBox();
+		}
+	}
+	
+	/**
+	 * @return Returns the arrowColor.
+	 */
+	public Color getArrowColor()
+	{
+		return arrowColor;
+	}
+
+	/**
+	 * @param arrowColor The arrowColor to set.
+	 */
+	public void setArrowColor(Color arrowColor)
+	{
+		this.arrowColor = arrowColor;
+	}
+
+	/**
+	 * @return Returns the dotColor.
+	 */
+	public Color getDotColor()
+	{
+		return marker.getColor();
+	}
+
+	/**
+	 * @param dotColor The dotColor to set.
+	 */
+	public void setDotColor(Color dotColor)
+	{
+		marker.setColor(dotColor);
+	}
+
+	/**
+	 * @return Returns the dotSize.
+	 */
+	public int getDotSize()
+	{
+		return marker.getSize();
+	}
+
+	/**
+	 * @param dotSize The dotSize to set.
+	 */
+	public void setDotSize(int dotSize)
+	{
+		marker.setSize(dotSize);
+	}
+
+	/**
+	 * @return Returns the labelColor.
+	 */
+	public Color getLabelColor()
+	{
+		return labelColor;
+	}
+
+	/**
+	 * @param labelColor The labelColor to set.
+	 */
+	public void setLabelColor(Color labelColor)
+	{
+		this.labelColor = labelColor;
+	}
+
+	/**
+	 * @return Returns the showArrows.
+	 */
+	public boolean getShowArrows()
+	{
+		return showArrows;
+	}
+
+	/**
+	 * @param showArrows The showArrows to set.
+	 */
+	public void setShowArrows(boolean showArrows)
+	{
+		this.showArrows = showArrows;
+	}
+
+	/**
+	 * @return Returns the showLabels.
+	 */
+	public boolean getShowLabels()
+	{
+		return showLabels;
+	}
+
+	/**
+	 * @param showLabels The showLabels to set.
+	 */
+	public void setShowLabels(boolean showLabels)
+	{
+		this.showLabels = showLabels;
+	}
+
+	public Integer getMapHeight()
+	{
+		return mapHeight;
+	}
+	
+	public void setPreferredMapHeight(Integer mapHeight)
+	{
+		this.mapHeight = mapHeight;
+	}
+
+	public Integer getMapWidth()
+	{
+		return mapWidth;
+	}
+
+	public void setPreferredMapWidth(Integer mapWidth)
+	{
+		this.mapWidth = mapWidth;
+	}
+
+	private Pair<Double, Double> getLatLong(Locality loc)
+	{
+		Double lat1 = loc.getLatitude1();
+		Double lat2 = loc.getLatitude2();
+		Double long1 = loc.getLongitude1();
+		Double long2 = loc.getLongitude2();
+		
+		if( lat2 != null && long2 != null )
+		{
+			return centerOfBBox(lat1,lat2,long1,long2);
+		}
+		
+		return new Pair<Double,Double>(lat1,long1);
+	}
+	
+	private Pair<Double,Double> centerOfBBox(Double lat1, Double lat2, Double long1, Double long2)
+	{
+		Pair<Double,Double> center = new Pair<Double,Double>();
+		center.first = (lat1+lat2)/2;
+		center.second = (long1+long2)/2;
+		return center;
+	}
+
+	protected double getLatLongRatio()
+	{
+		double longRange = maxLong - minLong;
+		double latRange = maxLat - minLat;
+		return latRange/longRange;
+	}
+	
+	protected void recalculateBoundingBox()
+	{
+		cacheValid = false;
+		
+		if( localities.isEmpty() )
+		{
+			minLat = -90;
+			minLong = -180;
+			maxLat = 90;
+			maxLong = 180;
+			return;
+		}
+
+		// setup the minimums so they are guaranteed to be changed
+		minLong = 180;
+		maxLong = -180;
+		minLat  = 90;
+		maxLat  = -90;
+		
+		for( Locality loc: localities )
+		{
+			Pair<Double,Double> latLong = getLatLong(loc);
+			if(latLong.first < minLat)
+			{
+				minLat = latLong.first;
+			}
+			if(latLong.second < minLong)
+			{
+				minLong = latLong.second;
+			}
+			if(latLong.first > maxLat)
+			{
+				maxLat = latLong.first;
+			}
+			if(latLong.second > maxLong)
+			{
+				maxLong = latLong.second;
+			}
+		}
+		
+		createBoundingBoxBufferRegion();
+	}
+	
+	protected void createBoundingBoxBufferRegion()
+	{
+		// make sure we have a 5% buffer around the edge of the map
+		double latSpread = maxLat - minLat;
+		if( latSpread == 0 )
+		{
+			latSpread += 10;
+		}
+		double longSpread = maxLong - minLong;
+		if( longSpread == 0 )
+		{
+			longSpread += 10;
+		}
+
+		double bufferFactor = .05;
+		
+		mapMinLat = Math.max(-90, minLat - latSpread*bufferFactor);
+		mapMinLong = Math.max(-180, minLong - longSpread*bufferFactor);
+		mapMaxLat = Math.min(90, maxLat + latSpread*bufferFactor);
+		mapMaxLong = Math.min(180, maxLong + longSpread*bufferFactor);
+	}
+	
+	protected Image getMapFromService(double minLat, double minLong,
+			double maxLat, double maxLong) throws HttpException, IOException
+	{
+		mapGrabber.setMinLat(minLat);
+		mapGrabber.setMaxLat(maxLat);
+		mapGrabber.setMinLong(minLong);
+		mapGrabber.setMaxLong(maxLong);
+
+		mapGrabber.setPreferredHeight(mapHeight);
+		mapGrabber.setPreferredWidth(mapWidth);
+		
+		return mapGrabber.getMap();
+	}
+
+	protected Point determinePixelCoordsOfLocality( Locality loc )
+	{
+		Pair<Double,Double> latLong = getLatLong(loc);
+		double y = latLong.first - mapMinLat;
+		double x = latLong.second - mapMinLong;
+		y = mapHeight - y * pixelPerLatRatio;
+		x = x * pixelPerLongRatio;
+		
+		return new Point((int)x,(int)y);
+	}
+	
+	protected boolean pointIsOnMapIcon(int x, int y)
+	{
+		if( mostRecentPaintedX > x || mostRecentPaintedX + mapWidth < x )
+		{
+			return false;
+		}
+		
+		if( mostRecentPaintedY > y || mostRecentPaintedY + mapHeight < y )
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public Pair<Double,Double> getLatLongForPointOnMapIcon(int x, int y)
+	{
+		if( !pointIsOnMapIcon(x, y) )
+		{
+			return null;
+		}
+		
+		// calculate the latitude
+		double lat=-1;
+		int relativeY = y - mostRecentPaintedY;
+		lat = mapMaxLat - relativeY / pixelPerLatRatio;
+		
+		// calculate the longitude
+		double lon=-1;
+		int relativeX = x - mostRecentPaintedX;
+		lon = relativeX / pixelPerLongRatio + mapMinLong;
+		
+		return new Pair<Double,Double>(lat,lon);
+	}
+	
+	public Icon getMap() throws HttpException, IOException
+	{
+		if( !cacheValid )
+		{
+			Image mapImage = getMapFromService(mapMinLat, mapMinLong, mapMaxLat, mapMaxLong);
+			mapIcon = new ImageIcon(mapImage);
+			cacheValid = true;
+
+			mapWidth = mapIcon.getIconWidth();
+			mapHeight = mapIcon.getIconHeight();
+
+			mapLatRange = mapMaxLat - mapMinLat;
+			mapLongRange = mapMaxLong - mapMinLong;
+
+			pixelPerLatRatio = (double)mapHeight / (double)mapLatRange;
+			pixelPerLongRatio = (double)mapWidth / (double)mapLongRange;
+
+			for( int i = 0; i < localities.size(); ++i )
+			{
+				Locality loc = localities.get(i);
+				Point iconLoc = determinePixelCoordsOfLocality(loc);
+				markerLocations.set(i, iconLoc);
+			}
+		}
+		
+		Icon icon = new Icon()
+		{
+			public void paintIcon(Component c, Graphics g, int x, int y)
+			{
+				// log the x and y for the MouseMotionListener
+				mostRecentPaintedX = x;
+				mostRecentPaintedY = y;
+
+				mapIcon.paintIcon(c, g, x, y);
+				Point lastLoc = null;
+				for( int i = 0; i < localities.size(); ++i )
+				{
+					Point markerLoc = markerLocations.get(i);
+					String label = labels.get(i);
+
+					if( markerLoc == null )
+					{
+						log.info("A marker location is null");
+						continue;
+					}
+					if(!pointIsOnMapIcon(x+markerLoc.x, y+markerLoc.y))
+					{
+						log.info("A marker location is off the map");
+						continue;
+					}
+					
+					marker.paintIcon(c, g, markerLoc.x+x, markerLoc.y+y);
+					if( label != null )
+					{
+						Color origColor = g.getColor();
+						g.setColor(labelColor);
+						GraphicsUtils.drawCenteredString(label, g, markerLoc.x+x, markerLoc.y+y);
+						g.setColor(origColor);
+					}
+
+					// TODO: draw an arrow from lastLoc to iconLoc
+					if( showArrows && lastLoc != null )
+					{
+						int x1 = x + lastLoc.x;
+						int y1 = y + lastLoc.y;
+						int x2 = x + markerLoc.x;
+						int y2 = y + markerLoc.y;
+						Color origColor = g.getColor();
+						g.setColor(arrowColor);
+						GraphicsUtils.drawArrow((Graphics2D)g, x1, y1, x2, y2, 2);
+						g.setColor(origColor);
+					}
+					
+					lastLoc = markerLoc;
+				}
+				
+				if( exceptionText != null )
+				{
+					Color origColor = g.getColor();
+					g.setColor(labelColor);
+					GraphicsUtils.drawCenteredString(exceptionText, g, x+mapWidth/2, y+mapHeight/2);
+					g.setColor(origColor);			
+				}
+			}
+			
+			public int getIconWidth()
+			{
+				return mapWidth;
+			}
+			
+			public int getIconHeight()
+			{
+				return mapHeight;
+			}
+		};
+		
+		// put the decorations on it
+		return icon;
+	}
+}
---
+++ src/edu/ku/brc/specify/tasks/services/MapGrabber.java
@@ -0,0 +1,294 @@
+package edu.ku.brc.specify.tasks.services;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MapGrabber
+{
+	private static Log log = LogFactory.getLog(MapGrabber.class);
+	// setup some default values
+	// TODO: remove these from any final versions
+	
+	protected HttpClient httpClient;
+	protected String host = "129.237.201.104";
+	protected String layers = "bmng";
+	protected double minLat = -90;
+	protected double minLong = -180;
+	protected double maxLat = 90;
+	protected double maxLong = 180;
+	protected Integer height = null;
+	protected Integer width = null;
+	protected int defaultHeight = 300;
+	protected int defaultWidth = 500;
+
+	public MapGrabber()
+	{
+		httpClient = new HttpClient();
+	}
+	
+	/**
+	 * @return Returns the host.
+	 */
+	public String getHost()
+	{
+		return host;
+	}
+
+	/**
+	 * @param host The host to set.
+	 */
+	public void setHost(String host)
+	{
+		this.host = host;
+	}
+
+	/**
+	 * @return Returns the layers.
+	 */
+	public String getLayers()
+	{
+		return layers;
+	}
+
+	/**
+	 * @param layers The layers to set.
+	 */
+	public void setLayers(String layers)
+	{
+		this.layers = layers;
+	}
+	
+	/**
+	 * @return Returns the height.
+	 */
+	public Integer getHeight()
+	{
+		return height;
+	}
+
+	/**
+	 * @param height The height to set.
+	 */
+	public void setPreferredHeight(Integer height)
+	{
+		this.height = height;
+	}
+
+	/**
+	 * @return Returns the width.
+	 */
+	public Integer getWidth()
+	{
+		return width;
+	}
+
+	/**
+	 * @param width The width to set.
+	 */
+	public void setPreferredWidth(Integer width)
+	{
+		this.width = width;
+	}
+	
+	/**
+	 * @return Returns the defaultHeight.
+	 */
+	public int getDefaultHeight()
+	{
+		return defaultHeight;
+	}
+
+	/**
+	 * @param defaultHeight The defaultHeight to set.
+	 */
+	public void setDefaultHeight(int defaultHeight)
+	{
+		this.defaultHeight = defaultHeight;
+	}
+
+	/**
+	 * @return Returns the defaultWidth.
+	 */
+	public int getDefaultWidth()
+	{
+		return defaultWidth;
+	}
+
+	/**
+	 * @param defaultWidth The defaultWidth to set.
+	 */
+	public void setDefaultWidth(int defaultWidth)
+	{
+		this.defaultWidth = defaultWidth;
+	}
+
+	/**
+	 * @return Returns the maxLat.
+	 */
+	public double getMaxLat()
+	{
+		return maxLat;
+	}
+
+	/**
+	 * @param maxLat The maxLat to set.
+	 */
+	public void setMaxLat(double maxLat)
+	{
+		this.maxLat = maxLat;
+	}
+
+	/**
+	 * @return Returns the maxLong.
+	 */
+	public double getMaxLong()
+	{
+		return maxLong;
+	}
+
+	/**
+	 * @param maxLong The maxLong to set.
+	 */
+	public void setMaxLong(double maxLong)
+	{
+		this.maxLong = maxLong;
+	}
+
+	/**
+	 * @return Returns the minLat.
+	 */
+	public double getMinLat()
+	{
+		return minLat;
+	}
+
+	/**
+	 * @param minLat The minLat to set.
+	 */
+	public void setMinLat(double minLat)
+	{
+		this.minLat = minLat;
+	}
+
+	/**
+	 * @return Returns the minLong.
+	 */
+	public double getMinLong()
+	{
+		return minLong;
+	}
+
+	/**
+	 * @param minLong The minLong to set.
+	 */
+	public void setMinLong(double minLong)
+	{
+		this.minLong = minLong;
+	}
+
+	public Image getMap(Integer width,
+						Integer height)
+		throws HttpException, IOException
+	{
+		setPreferredWidth(width);
+		setPreferredHeight(height);
+		return getMap();
+	}
+
+	public Image getMap(Integer width,
+						Integer height,
+						int minLat,
+						int minLong,
+						int maxLat,
+						int maxLong)
+		throws HttpException, IOException
+	{
+		setMinLat(minLat);
+		setMinLong(minLong);
+		setMaxLat(maxLat);
+		setMaxLong(maxLong);
+		setPreferredWidth(width);
+		setPreferredHeight(height);
+		return getMap();
+	}
+	
+	protected double getLatLongRatio()
+	{
+		double longRange = maxLong - minLong;
+		double latRange = maxLat - minLat;
+		return (double)(latRange/longRange);
+	}
+	
+	protected void calcHeight()
+	{
+		height = (int)(width * getLatLongRatio());
+	}
+	
+	protected void calcWidth()
+	{
+		width = (int)(height / getLatLongRatio());
+	}
+	
+	public Image getMap() throws HttpException, IOException
+	{
+		if( width == null && height == null )
+		{
+			width = defaultWidth;
+			height = defaultHeight;
+		}
+		else if( width == null )
+		{
+			calcWidth();
+		}
+		else if( height == null )
+		{
+			calcHeight();
+		}
+		
+		if( width > 2048 )
+		{
+			width = 2048;
+			calcHeight();
+		}
+		
+		if( height > 2048 )
+		{
+			height = 2048;
+			calcWidth();
+		}
+		
+		StringBuilder url = new StringBuilder("http://");
+		url.append(host);
+		url.append("/cgi-bin/ogc.cgi/bmortho?version=1.1.1&service=WMS&Request=GetMap&format=image/png&transparent=true&styles=&srs=epsg:4326&layers=");
+		url.append(layers);
+		
+		// set bounding box
+		url.append("&bbox=");
+		url.append(minLong);
+		url.append(",");
+		url.append(minLat);
+		url.append(",");
+		url.append(maxLong);
+		url.append(",");
+		url.append(maxLat);
+		
+		// set size
+		url.append("&height=");
+		url.append(height);
+		url.append("&width=");
+		url.append(width);
+		
+		GetMethod get = new GetMethod(url.toString());
+		get.setFollowRedirects(true);
+		int resultCode = httpClient.executeMethod(get);
+		log.debug("GET " + url.toString() + " returned " + resultCode );
+		return Toolkit.getDefaultToolkit().createImage(get.getResponseBody());
+	}
+}
