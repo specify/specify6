5e285d5ca32f720d2b39055a62d3b6365e8318a2
Rod Spears <rods@ku.edu>
2006-05-08 13:31:52 +0000

    new files need for FishBase integration
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@450 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/extras/FishBaseInfoGetter.java
@@ -0,0 +1,250 @@
+/* Filename:    $RCSfile: FishBaseInfoGetter.java,v $
+ * Author:      $Author: rods $
+ * Revision:    $Revision: 1.1 $
+ * Date:        $Date: 2005/10/12 16:52:28 $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ku.brc.specify.extras;
+
+
+import java.awt.Image;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.imageio.stream.FileImageOutputStream;
+import javax.swing.ImageIcon;
+
+import org.apache.commons.lang.StringUtils;
+import org.dom4j.Element;
+
+import edu.ku.brc.specify.helpers.XMLHelper;
+
+/**
+ * @author rods
+ *
+ */
+public class FishBaseInfoGetter extends HTTPGetter
+{
+    public enum InfoType {Summary, Image, Thumbnail, PictureList};
+
+    protected FishBaseInfoGetterListener consumer;
+
+    protected InfoType  type;
+    protected String    genus;
+    protected String    species;
+    protected Image     image    = null;
+
+    protected String    tmpDir   = null;
+    protected Element   dom      = null;
+    protected String    data     = null;
+
+
+  public FishBaseInfoGetter(final FishBaseInfoGetterListener consumer,
+                            final InfoType type,
+                            final String   genus,
+                            final String   species)
+    {
+        this.consumer = consumer;
+        this.type     = type;
+        this.genus    = genus;
+        this.species  = species;
+
+        tmpDir = System.getProperty("java.io.tmpdir");
+    }
+
+    public Element getDom()
+    {
+        return dom;
+    }
+
+    public Image getImage()
+    {
+        return image;
+    }
+
+    public void setConsumer(FishBaseInfoGetterListener consumer) {
+		this.consumer = consumer;
+	}
+
+	/**
+     * Performs a "generic" HTTP request and fill member variable with results use
+     * "getDigirResultsetStr" to get the results as a String
+     *
+     */
+    public Image getImage(final String fileName, final String url)
+    {
+        String fullPath = tmpDir + File.separator + fileName;
+        System.out.println(fullPath);
+        File file = new File(fullPath);
+        if (file.exists())
+        {
+            try
+            {
+                ImageIcon image = new ImageIcon(fullPath);
+                return image.getImage();
+
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                status = ErrorCode.Error;
+            }
+
+        } else
+        {
+            byte[] bytes = super.doHTTPRequest(url);
+
+            try
+            {
+                FileImageOutputStream fos = new FileImageOutputStream(file);
+                fos.write(bytes);
+                fos.flush();
+                fos.close();
+
+                ImageIcon image = new ImageIcon(bytes);
+                return image.getImage();
+
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                status = ErrorCode.Error;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Performs a "generic" HTTP request and fill member variable with results use
+     * "getDigirResultsetStr" to get the results as a String
+     *
+     */
+    public void getDOMDoc(final String url, final InfoType infoType)
+    {
+        dom = null;
+        String fileName = tmpDir + File.separator + genus + "_" + species + "_" + infoType.toString() + ".xml";
+        System.out.println(fileName);
+        File file = new File(fileName);
+        if (file.exists())
+        {
+            try
+            {
+                dom = XMLHelper.readFileToDOM4J(file);
+
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                status = ErrorCode.Error;
+            }
+
+        } else
+        {
+            byte[] bytes = super.doHTTPRequest(url);
+
+            data = new String(bytes);
+            int inx = data.indexOf("<?");
+            if (inx > -1)
+            {
+                data = data.substring(inx, data.length());
+            }
+            System.out.println(data);
+            try
+            {
+                Writer output = new BufferedWriter(new FileWriter(file));
+                output.write(data);
+                output.flush();
+                output.close();
+
+                // Is is cheating and slow, but I will do it for now
+                // XXX FIXME!
+                dom = XMLHelper.readFileToDOM4J(file);
+
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                status = ErrorCode.Error;
+            }
+        }
+    }
+
+    public void run()
+    {
+        if (type == InfoType.Summary)
+        {
+            urlStr = "http://www.fishbase.org.ph/webservice/SpeciesSummary/SpeciesSummary.asp?Genus="+genus+"&Species="+species;
+            getDOMDoc(urlStr, type);
+
+        } else if (type == InfoType.Thumbnail || type == InfoType.Image)
+        {
+            urlStr = "http://www.fishbase.org/webservice/Photos/FishPicsList.php?Genus=" + genus + "&Species=" + species;
+            getDOMDoc(urlStr, InfoType.PictureList);
+
+            if (status == ErrorCode.NoError)
+            {
+                Element species = (Element)dom.selectSingleNode("/fishbase/species");
+                if (species != null)
+                {
+                    int numPictures = XMLHelper.getAttr(species, "total_adult", 0);
+                    if (numPictures > 0)
+                    {
+                        List adults = dom.selectNodes("/fishbase/pictures[@type='adult']");
+                        for ( Iterator iter = adults.iterator(); iter.hasNext(); )
+                        {
+                            Element pictureElement = (Element) iter.next();
+                            Element nameElement = (Element)pictureElement.selectSingleNode(type == InfoType.Thumbnail ? "thumbnail" : "actual");
+                            if (nameElement != null)
+                            {
+                                String fileURL = nameElement.getTextTrim();
+                                System.out.println(fileURL);
+                                if (StringUtils.isNotEmpty(fileURL))
+                                {
+                                    String shortName = "";
+                                    int inx = fileURL.lastIndexOf("/");
+                                    if (inx > -1)
+                                    {
+                                        shortName = fileURL.substring(inx, fileURL.length());
+                                    }
+                                    image = getImage(shortName, fileURL);
+                                    if (image != null)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (consumer != null)
+                {
+                    if (status == ErrorCode.NoError)
+                    {
+                        consumer.infoArrived(this);
+
+                    } else
+                    {
+                        consumer.infoGetWasInError(this);
+                    }
+                }
+            }
+        }
+        stop();
+    }
+
+}
---
+++ src/edu/ku/brc/specify/extras/HTTPGetter.java
@@ -0,0 +1,178 @@
+/* Filename:    $RCSfile: HTTPGetter.java,v $
+ * Author:      $Author: rods $
+ * Revision:    $Revision: 1.1 $
+ * Date:        $Date: 2005/10/12 16:52:28 $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ku.brc.specify.extras;
+
+import java.io.IOException;
+import java.net.ConnectException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author rods
+ *
+ */
+public class HTTPGetter implements Runnable
+{
+    private static Log log = LogFactory.getLog(HTTPGetter.class);
+
+    public enum ErrorCode {
+        NoError, Error, HttpError, NotDoneError, IOError, URLError
+    };
+
+    protected ErrorCode status = ErrorCode.NoError;
+    protected Thread    thread = null;
+    protected String    urlStr;
+
+
+    public HTTPGetter()
+    {
+        this.urlStr = null;
+    }
+
+    public HTTPGetter(final String urlStr, final boolean cacheFile)
+    {
+        this.urlStr = urlStr;
+    }
+
+    public String getUrlStr()
+    {
+        return urlStr;
+    }
+
+    public void setUrlStr(String urlStr)
+    {
+        this.urlStr = urlStr;
+    }
+
+    public ErrorCode getStatus()
+    {
+        return status;
+    }
+
+    /**
+     * Performs a "generic" HTTP request and fill member variable with results
+     * use "getDigirResultsetStr" to get the results as a String
+     *
+     * @param aURL
+     *            URL to be executaed
+     * @return returns an error code
+     */
+    public byte[] doHTTPRequest(final String url)
+    {
+        byte[] bytes = null;
+        status = ErrorCode.NoError;
+
+        // Create an HttpClient with the MultiThreadedHttpConnectionManager.
+        // This connection manager must be used if more than one thread will
+        // be using the HttpClient.
+        HttpClient httpClient = new HttpClient(new MultiThreadedHttpConnectionManager());
+
+        GetMethod method = new GetMethod(url);
+        try
+        {
+
+            log.debug("getting " + method.getURI());
+            // execute the method
+            httpClient.executeMethod(method);
+
+            System.out.println("Get executed");
+            // get the response body as an array of bytes
+            bytes = method.getResponseBody();
+
+            log.debug(bytes.length + " bytes read");
+
+        } catch (ConnectException ce)
+        {
+            log.error("Could not make HTTP connection.  ("
+                    + ce.toString() + ")");
+            status = ErrorCode.HttpError;
+
+        } catch (HttpException he)
+        {
+            log.error("Http problem making request.  ("
+                    + he.toString() + ")");
+            status = ErrorCode.HttpError;
+
+        } catch (IOException ioe)
+        {
+            log.error("IO problem making request.  (" + ioe.toString()
+                    + ")");
+            status = ErrorCode.IOError;
+
+        } catch (Exception e)
+        {
+            log.error("Error: " + e);
+            status = ErrorCode.Error;
+
+        } finally
+        {
+            // always release the connection after we're done
+            method.releaseConnection();
+            log.debug("Connection released");
+        }
+        return bytes;
+    }
+
+    // ----------------------------------------------------------------
+    // -- Runnable Interface
+    // ----------------------------------------------------------------
+    public void start()
+    {
+        if (thread == null)
+        {
+            thread = new Thread(this);
+            thread.setPriority(Thread.MIN_PRIORITY);
+            thread.start();
+        }
+    }
+
+    /**
+     *
+     *
+     */
+    public synchronized void stop()
+    {
+        if (thread != null)
+        {
+            thread.interrupt();
+        }
+        thread = null;
+        notifyAll();
+    }
+
+    /**
+     *
+     */
+    public void run()
+    {
+        //Thread me = Thread.currentThread();
+
+        doHTTPRequest(urlStr);
+
+        stop();
+    }
+}
