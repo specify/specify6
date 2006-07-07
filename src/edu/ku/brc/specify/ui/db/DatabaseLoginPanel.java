e48f44cbdf089b3c2fecf3027e6be4af1f1af8ff
Rod Spears <rods@ku.edu>
2006-07-07 16:54:38 +0000

    fixed enabling the login button issue
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@588 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/ui/db/DatabaseLoginPanel.java
+++ src/edu/ku/brc/specify/ui/db/DatabaseLoginPanel.java
@@ -23,6 +23,8 @@ import static edu.ku.brc.specify.ui.UICacheManager.getResourceString;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Collections;
@@ -58,42 +60,42 @@ import edu.ku.brc.specify.ui.UICacheManager;
 
 /**
  * This panel enables the user to configure all the params necessary to log into a jDBC database.
- * The login is done asynchronously and the panel is notified if it was successful or not. 
+ * The login is done asynchronously and the panel is notified if it was successful or not.
  * A DatabaseLoginListener can be registered to be notified of a successful login or when the cancel button is pressed.
- * 
+ *
  *  NOTE: This dialog can only be closed for two reasons: 1) A valid login, 2) It was cancelled by the user.
- *  
+ *
  * @author rods
  *
  */
 public class DatabaseLoginPanel extends JPanel
 {
     private static final Logger log  = Logger.getLogger(DatabaseLoginPanel.class);
-    
+
     // Form Stuff
 
     protected JTextField       username;
     protected JPasswordField   password;
-    
+
     protected JEditComboBox    databases;
     protected JEditComboBox    servers;
-    
+
     protected JCheckBox        rememberUsernameCBX;
     protected JCheckBox        autoLoginCBX;
-    
+
     protected JButton          cancelBtn;
     protected JButton          loginBtn;
-    
+
     protected JStatusBar       statusBar;
 
     protected JDialog          thisDlg;
     protected boolean          isCancelled = true;
-    
+
     protected DatabaseLoginListener dbListener;
-    
+
 
     //private String[]    serverNameStrs     = {
-    //        "jdbc:mysql://localhost/", 
+    //        "jdbc:mysql://localhost/",
     //        "jdbc:inetdae7://129.237.201.110/",
     //        "jdbc:mysql://129.237.201.110/",
     //        "jdbc:microsoft:sqlserver://129.237.201.110/" };                   // "jdbc:inetdae7://129.237.201.110/"};
@@ -103,7 +105,7 @@ public class DatabaseLoginPanel extends JPanel
     private String      usernameStr   = "";
     private String      passwordStr   = "";
     private String      driverName    = "com.mysql.jdbc.Driver"; // XXX HARD CODED VALUE!
-    
+
     private Preferences prefNode;
 
     /**
@@ -112,14 +114,14 @@ public class DatabaseLoginPanel extends JPanel
     public DatabaseLoginPanel(final DatabaseLoginListener dbListener)
     {
         this.dbListener = dbListener;
-        
+
         Preferences appsNode = UICacheManager.getAppPrefs();
         prefNode = appsNode.node("login");
-        
+
         createUI();
 
     }
-    
+
     /**
      * @return the login btn
      */
@@ -143,7 +145,7 @@ public class DatabaseLoginPanel extends JPanel
         y += 2;
         return y;
     }
-    
+
     /**
      * Creates the proper form from the view system
      * @param viewSetName the viewset name
@@ -155,37 +157,47 @@ public class DatabaseLoginPanel extends JPanel
 
         PropertiesPickListAdapter dbPickList = new PropertiesPickListAdapter(prefNode, "databases");
         PropertiesPickListAdapter svPickList = new PropertiesPickListAdapter(prefNode, "servers");
-        
+
+        class MyFocusAdapter extends FocusAdapter
+        {
+            public void focusLost(FocusEvent e)
+            {
+                updateUIControls();
+            }
+        }
         username  = new JTextField(20);
         password  = new JPasswordField(20);
-        
+
+        username.addFocusListener(new MyFocusAdapter());
+        password.addFocusListener(new MyFocusAdapter());
+
         databases = new JEditComboBox(dbPickList);
         servers   = new JEditComboBox(svPickList);
         dbPickList.setComboBox(databases);
         svPickList.setComboBox(servers);
-        
+
         autoLoginCBX        = new JCheckBox(getResourceString("autologin"));
         rememberUsernameCBX = new JCheckBox(getResourceString("rememberuser"));
-        
+
         statusBar = new JStatusBar();
-        
+
         cancelBtn = new JButton(getResourceString("cancel"));
         loginBtn  = new JButton(getResourceString("login"));
-        
+
         autoLoginCBX.setSelected(prefNode.getBoolean("autologin", false));
         rememberUsernameCBX.setSelected(prefNode.getBoolean("rememberuser", false));
-        
+
         if (autoLoginCBX.isSelected())
         {
             username.setText(prefNode.get("username", ""));
             password.setText(Encryption.decrypt(prefNode.get("password", "")));
             username.requestFocus();
-            
+
         } else if (rememberUsernameCBX.isSelected())
         {
             username.setText(prefNode.get("username", ""));
             password.requestFocus();
-            
+
             SwingUtilities.invokeLater(new Runnable() {
                 public void run()
                 {
@@ -204,7 +216,7 @@ public class DatabaseLoginPanel extends JPanel
                 }
              }
          });
-        
+
         loginBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e)
             {
@@ -212,11 +224,11 @@ public class DatabaseLoginPanel extends JPanel
             }
          });
 
-        
+
         // Ask the PropertiesPickListAdapter to set the index from the prefs
         dbPickList.setSelectedIndex();
         svPickList.setSelectedIndex();
-        
+
         servers.getTextField().addKeyListener(new KeyAdapter()
                 {
             public void keyReleased(KeyEvent e)
@@ -224,7 +236,7 @@ public class DatabaseLoginPanel extends JPanel
                 updateUIControls();
             }
         });
-        
+
         databases.getTextField().addKeyListener(new KeyAdapter()
                 {
             public void keyReleased(KeyEvent e)
@@ -238,7 +250,7 @@ public class DatabaseLoginPanel extends JPanel
         PanelBuilder formBuilder = new PanelBuilder(new FormLayout("p,3dlu,max(220px;p)", "p,2dlu,p,2dlu,p,2dlu,p,2dlu,p,2dlu,p,2dlu,p,2dlu,p"));
         CellConstraints cc = new CellConstraints();
         formBuilder.addSeparator(getResourceString("logintitle"), cc.xywh(1,1,3,1));
-        
+
         int y = 3;
         y = addLine("username",  username, formBuilder, cc, y);
         y = addLine("password",  password, formBuilder, cc, y);
@@ -246,35 +258,35 @@ public class DatabaseLoginPanel extends JPanel
         y = addLine("servers",   servers, formBuilder, cc, y);
         y = addLine(null,        rememberUsernameCBX, formBuilder, cc, y);
         y = addLine(null,        autoLoginCBX, formBuilder, cc, y);
-        
+
         PanelBuilder outerPanel = new PanelBuilder(new FormLayout("p,3dlu,p", "p,2dlu,p,2dlu,p"), this);
         ImageDisplay icon       = new ImageDisplay(IconManager.getImage("SpecifyLargeIcon"), false, false);
-        
+
         formBuilder.getPanel().setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
-        
+
         outerPanel.add(icon, cc.xy(1, 1));
-        outerPanel.add(formBuilder.getPanel(), cc.xy(3, 1));     
+        outerPanel.add(formBuilder.getPanel(), cc.xy(3, 1));
         outerPanel.add(ButtonBarFactory.buildOKCancelBar(loginBtn, cancelBtn), cc.xywh(1,3,3,1));
         outerPanel.add(statusBar, cc.xywh(1,5,3,1));
 
-        
-        
+
+
         updateUIControls();
     }
-    
+
     /**
      * Enables or disables the UI based of the values of the controls
      */
     protected void updateUIControls()
     {
-        loginBtn.setEnabled(StringUtils.isNotEmpty(username.getText()) && 
-                            StringUtils.isNotEmpty(new String(password.getPassword())) && 
-                            StringUtils.isNotEmpty(servers.getTextField().getText()) && 
+        loginBtn.setEnabled(StringUtils.isNotEmpty(username.getText()) &&
+                            StringUtils.isNotEmpty(new String(password.getPassword())) &&
+                            StringUtils.isNotEmpty(servers.getTextField().getText()) &&
                             StringUtils.isNotEmpty(databases.getTextField().getText()));
-        
+
         rememberUsernameCBX.setEnabled(!autoLoginCBX.isSelected());
     }
-    
+
     /**
      * Sets a string into the status bar
      * @param msg the msg for the status bar
@@ -298,21 +310,21 @@ public class DatabaseLoginPanel extends JPanel
     {
         databases.getDBAdapter().save();
         servers.getDBAdapter().save();
-        
+
         prefNode.putBoolean("rememberuser", rememberUsernameCBX.isSelected());
         prefNode.putBoolean("autologin", autoLoginCBX.isSelected());
-        
+
         if (autoLoginCBX.isSelected())
         {
             prefNode.put("username", username.getText());
             prefNode.put("password", Encryption.encrypt(new String(password.getPassword())));
-            
+
         } else if (rememberUsernameCBX.isSelected())
         {
             prefNode.put("username", username.getText());
         }
     }
-    
+
     /**
      * Grabs all the values from the UI
      */
@@ -324,7 +336,7 @@ public class DatabaseLoginPanel extends JPanel
         passwordStr  = new String(password.getPassword());
 
     }
-    
+
     /**
      * Indicates the login is OK and closes the dialog for the user to conitinue on
      */
@@ -337,7 +349,7 @@ public class DatabaseLoginPanel extends JPanel
         }
     }
 
-    
+
     /**
      * Performs a login on a separate thread and then notifies the dialog if it was successful.
      */
@@ -346,17 +358,17 @@ public class DatabaseLoginPanel extends JPanel
         getValuesFromUI();
 
         save();
-        
+
         statusBar.setIndeterminate(true);
         cancelBtn.setEnabled(false);
         loginBtn.setEnabled(false);
-        
+
         setMessage(String.format(getResourceString("LoggingIn"), new Object[] {databaseName}), false);
-               
+
         final SwingWorker worker = new SwingWorker()
         {
             boolean isLoggedIn = false;
-            
+
             public Object construct()
             {
                 isLoggedIn = UIHelper.tryLogin(driverName, serverName, databaseName, usernameStr, passwordStr);
@@ -374,7 +386,7 @@ public class DatabaseLoginPanel extends JPanel
                 {
                     DBConnection dbConn = DBConnection.getInstance();
                     setMessage(dbConn.getErrorMsg(), true);
-                        
+
                 } else
                 {
                     loginOK();
@@ -404,12 +416,12 @@ public class DatabaseLoginPanel extends JPanel
     {
         return passwordStr;
     }
-     
+
     public String getDriverName()
     {
         return driverName;
     }
-     
+
     /**
      * Returns true if doing auto login
      * @return true if doing auto login
@@ -418,7 +430,7 @@ public class DatabaseLoginPanel extends JPanel
     {
         return prefNode.getBoolean("autologin", false);
     }
-    
+
     /**
      * Return whether dialog was cancelled
      * @return whether dialog was cancelled
@@ -431,8 +443,8 @@ public class DatabaseLoginPanel extends JPanel
     //-------------------------------------------------------------------------
     //-- Inner Classes
     //-------------------------------------------------------------------------
-    
-    /* 
+
+    /*
      * This derived class of the PickListDBAdapter enables a PickList to have it's contents
      * come from a Pref with a scomma separated list of values, instead of from the database.
      * This is certainly a candidate for be pulled out and made a "full class"
@@ -445,22 +457,22 @@ public class DatabaseLoginPanel extends JPanel
         protected JEditComboBox     comboBox;
         protected boolean           savePickList = true;
 
-        public PropertiesPickListAdapter(final Preferences prefNode, 
+        public PropertiesPickListAdapter(final Preferences prefNode,
                                          final String      prefName)
         {
             super();
-            
+
             this.prefNode = prefNode;
             this.prefName = prefName;
-            
+
             this.prefSelectedName = prefName + "_selected";
-            
+
             if (savePickList)
             {
                 readData();
             }
         }
-        
+
         public void setComboBox(JEditComboBox comboBox)
         {
             this.comboBox = comboBox;
@@ -480,7 +492,7 @@ public class DatabaseLoginPanel extends JPanel
         {
             String valuesStr = prefNode.get(prefName, "");
             //log.debug("["+prefName+"]["+valuesStr+"]");
-            
+
             if (StringUtils.isNotEmpty(valuesStr))
             {
                 String[] strs = StringUtils.split(valuesStr, ",");
@@ -497,15 +509,15 @@ public class DatabaseLoginPanel extends JPanel
             }
 
         }
-        
+
         /**
-         * Sets the proper index from the pref 
+         * Sets the proper index from the pref
          */
         public void setSelectedIndex()
         {
             String selectStr = prefNode.get(prefSelectedName, "");
             //log.debug("["+prefSelectedName+"]["+selectStr+"]");
-            
+
             int selectedIndex = -1;
             int i = 0;
             for (PickListItem item : items)
@@ -518,7 +530,7 @@ public class DatabaseLoginPanel extends JPanel
             }
             comboBox.setSelectedIndex(selectedIndex);
         }
-        
+
         /**
          * @param model
          * @return
@@ -529,11 +541,11 @@ public class DatabaseLoginPanel extends JPanel
             for (PickListItem item : pickList.getItems())
             {
                 if (strBuf.length() > 0) strBuf.append(",");
-                strBuf.append(item.getValue());   
+                strBuf.append(item.getValue());
             }
             return strBuf.toString();
         }
-        
+
 
         /* (non-Javadoc)
          * @see edu.ku.brc.specify.ui.db.PickListDBAdapter#save()
@@ -546,13 +558,13 @@ public class DatabaseLoginPanel extends JPanel
                 prefNode.put(prefName, convertModelToStr(pickList));
                 log.debug("["+prefName+"]["+convertModelToStr(pickList)+"]");
             }
-            
+
             Object selectedItem = comboBox.getModel().getSelectedItem();
             if (selectedItem == null && comboBox.getTextField() != null)
             {
                 selectedItem = comboBox.getTextField().getText();
             }
-            
+
             if (selectedItem != null)
             {
                 prefNode.put(prefSelectedName, selectedItem.toString());
