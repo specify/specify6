bb071c116cae3b38b9b7032f8f839767d1ba9731
Josh Stewart <jds@ku.edu>
2006-07-11 21:08:29 +0000

    Initial import of the ListDragDropCallback interface.  I wrote this interface to decouple the TreeDataJList from any knowledge of Treeables.  If we can move UICacheManager and some of the GhostActionable stuff out of edu.ku.brc.specify, we can make TreeDataJList completely specify unaware.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@601 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/ui/ListDragDropCallback.java
@@ -0,0 +1,19 @@
+package edu.ku.brc.ui;
+
+/**
+ * Provides callbacks for drop events on JLists.
+ *
+ * @author jstewart
+ * @version %I% %G%
+ */
+public interface ListDragDropCallback
+{
+	/**
+	 * Performs all tasks required by the event of <code>dragged</code>
+	 * being dropped on <code>droppedOn</code>.
+	 *
+	 * @param draggedObj the dragged object
+	 * @param dropLocObj the object that <code>dragged</code> was dropped on
+	 */
+	public void dropOccurred(Object dragged, Object droppedOn );
+}
