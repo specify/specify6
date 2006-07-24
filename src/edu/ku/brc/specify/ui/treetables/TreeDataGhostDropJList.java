c54b8afb74dda746e94e9d8ab365c32ac4521ac6
Josh Stewart <jds@ku.edu>
2006-07-24 18:07:37 +0000

    Initial import of TreeDataGhostDropJList.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@648 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/ui/treetables/TreeDataGhostDropJList.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+
+package edu.ku.brc.specify.ui.treetables;
+
+import javax.swing.ListModel;
+
+import edu.ku.brc.ui.DragDropCallback;
+import edu.ku.brc.ui.GhostDropJList;
+
+/**
+ * A custom JList that is capable of ignoring clicks on list items in order to
+ * modify item selection behavior from the mouse.  An "outside" MouseListener
+ * would determine if the click should trigger a change in selection or not.  If not,
+ * the listener should call {@link #setClickOnText(boolean)} with a value of <code>false</code>.
+ *
+ * NOTE: the workings of this class <i>MIGHT</i> be dependent on the order of MouseListeners
+ * that are being processed.  If the "outside" MouseListener is registered on a parent component,
+ * it should fire before the internal MouseListener used by the JList to notify the
+ * ListSelectionModel of user selection changes.
+ * 
+ * @status CodeFreeze
+ * @author jstewart
+ * @version %I% %G%
+ */
+public class TreeDataGhostDropJList extends GhostDropJList
+{
+	protected boolean clickOnText;
+	
+	/**
+	 *
+	 *
+	 * @param model
+	 * @param dragDropCallback
+	 */
+	public TreeDataGhostDropJList(ListModel model, DragDropCallback dragDropCallback)
+	{
+		super(model,dragDropCallback);
+		clickOnText = false;
+	}
+	
+	public void setClickOnText(boolean onText)
+	{
+		clickOnText = onText;
+	}
+	
+	/**
+	 *
+	 *
+	 * @see javax.swing.JList#setSelectedIndices(int[])
+	 * @param indices
+	 */
+	@Override
+	public void setSelectedIndices(int[] indices)
+	{
+		if( clickOnText )
+		{
+			return;
+		}
+		clickOnText = true;
+	}
+
+	/**
+	 *
+	 *
+	 * @see javax.swing.JList#setSelectedValue(java.lang.Object, boolean)
+	 * @param anObject
+	 * @param shouldScroll
+	 */
+	@Override
+	public void setSelectedValue(Object anObject, boolean shouldScroll)
+	{
+		if( clickOnText )
+		{
+			super.setSelectedValue(anObject,shouldScroll);
+		}
+		clickOnText = true;
+	}
+
+	/**
+	 *
+	 *
+	 * @see javax.swing.JList#setSelectionInterval(int, int)
+	 * @param anchor
+	 * @param lead
+	 */
+	@Override
+	public void setSelectionInterval(int anchor, int lead)
+	{
+		if( clickOnText )
+		{
+			super.setSelectionInterval(anchor,lead);
+		}
+		clickOnText = true;
+	}
+
+	/**
+	 *
+	 *
+	 * @see javax.swing.JList#setSelectedIndex(int)
+	 * @param index
+	 */
+	@Override
+	public void setSelectedIndex(int index)
+	{
+		if( clickOnText )
+		{
+			super.setSelectedIndex(index);
+		}
+		clickOnText = true;
+	}
+}
