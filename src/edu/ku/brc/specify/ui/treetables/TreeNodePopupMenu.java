a5be728b07ba75c6ec10fe9516a410beb52c7e37
Josh Stewart <jds@ku.edu>
2006-08-28 18:49:18 +0000

    Forgot to add this to my checkin earlier.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@742 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/ui/treetables/TreeNodePopupMenu.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package edu.ku.brc.specify.ui.treetables;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+/**
+ *
+ *
+ * @author jstewart
+ * @version %I% %G%
+ */
+public class TreeNodePopupMenu extends JPopupMenu
+{
+	protected JList list;
+	protected TreeTableViewer ttv;
+	
+	/**
+	 *
+	 *
+	 */
+	public TreeNodePopupMenu(TreeTableViewer owner)
+	{
+		this.ttv = owner;
+		
+		JMenuItem subtree = new JMenuItem("Subtree");
+		JMenuItem expand = new JMenuItem("Expand all descendants");
+		JMenuItem find = new JMenuItem("Find...");
+		JMenuItem edit = new JMenuItem("Edit...");
+		JMenuItem delete = new JMenuItem("Delete");
+		JMenuItem newChild = new JMenuItem("New child...");
+		
+		subtree.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				ttv.showSubtreeOfSelection(list);
+			}
+		});
+
+		expand.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				ttv.expandAllDescendantsOfSelection(list);
+			}
+		});
+
+		find.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				// show a find dialog
+				System.out.println("Setup some search functionality");
+			}
+		});
+
+		edit.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				ttv.editSelectedNode(list);
+			}
+		});
+
+		delete.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				ttv.deleteSelectedNode(list);
+			}
+		});
+
+		newChild.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				ttv.addChildToSelectedNode(list);
+			}
+		});
+		
+		this.add(subtree);
+		this.add(expand);
+		this.add(find);
+		this.add(edit);
+		this.add(delete);
+		this.add(newChild);
+	}
+	
+	public void setList(JList list)
+	{
+		this.list = list;
+	}
+}
