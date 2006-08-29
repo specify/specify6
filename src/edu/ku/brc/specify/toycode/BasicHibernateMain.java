c2948939f665b42c74978c01efdf3705759bffb7
Josh Stewart <jds@ku.edu>
2006-08-29 20:18:51 +0000

    Initial import of BasicHibernateMain.  This class can be really useful if you need to get a basic app up and running and want access to our DB using Hibernate.  Simply put your custom code in the area that says "// do stuff here".
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@750 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/toycode/BasicHibernateMain.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package edu.ku.brc.specify.toycode;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
+import com.jgoodies.looks.plastic.PlasticLookAndFeel;
+import com.jgoodies.looks.plastic.theme.DesertBlue;
+
+import edu.ku.brc.af.prefs.AppPreferences;
+import edu.ku.brc.helpers.UIHelper;
+import edu.ku.brc.specify.Specify;
+import edu.ku.brc.specify.SpecifyAppPrefs;
+import edu.ku.brc.ui.IconManager;
+import edu.ku.brc.ui.UICacheManager;
+import edu.ku.brc.ui.db.DatabaseLoginListener;
+
+/**
+ *
+ *
+ * @author jstewart
+ * @version %I% %G%
+ */
+public class BasicHibernateMain implements DatabaseLoginListener
+{
+    public void loggedIn(final String databaseName, final String userName)
+    {
+    	SpecifyAppPrefs.initialPrefs();
+
+    	// do stuff here
+    	
+    	System.exit(0);
+    }
+    
+    public void cancelled()
+    {
+        System.exit(0);
+    }
+
+	/**
+	 *
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) throws UnsupportedLookAndFeelException
+	{
+        if (!System.getProperty("os.name").equals("Mac OS X"))
+        {
+            UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
+            PlasticLookAndFeel.setMyCurrentTheme(new DesertBlue());
+        }
+
+        // Name factories
+        System.setProperty("edu.ku.brc.af.core.AppContextMgrFactory", "edu.ku.brc.specify.config.SpecifyAppContextMgr");
+        System.setProperty("AppPrefsIOClassName", "edu.ku.brc.specify.config.AppPrefsDBIOIImpl");
+
+        IconManager.setApplicationClass(Specify.class);
+        UICacheManager.getInstance(); // initializes it first thing
+        UICacheManager.setAppName("Specify");
+
+        // Load Local Prefs
+        AppPreferences localPrefs = AppPreferences.getLocalPrefs();
+        localPrefs.setDirPath(UICacheManager.getDefaultWorkingPath());
+        localPrefs.load();
+
+        BasicHibernateMain tester = new BasicHibernateMain();
+		UIHelper.doLogin(false, true, false, tester);
+	}
+}
