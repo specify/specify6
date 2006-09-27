66bd0cedc2ea4f63cc6010c58eaeb0b87d68c3a0
Josh Stewart <jds@ku.edu>
2006-09-27 18:48:44 +0000

    Oops.  A previous commit was missing a new entry in resources_en.properties.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@850 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/AttachmentsTask.java
+++ src/edu/ku/brc/specify/tasks/AttachmentsTask.java
@@ -11,10 +11,13 @@ import static edu.ku.brc.ui.UICacheManager.getResourceString;
 import java.util.List;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 import edu.ku.brc.af.core.SubPaneIFace;
 import edu.ku.brc.af.plugins.MenuItemDesc;
 import edu.ku.brc.af.plugins.ToolBarItemDesc;
 import edu.ku.brc.af.tasks.BaseTask;
+import edu.ku.brc.ui.CommandAction;
 
 /**
  *
@@ -23,7 +26,12 @@ import edu.ku.brc.af.tasks.BaseTask;
  */
 public class AttachmentsTask extends BaseTask
 {
-    public static final String ATTACHMENTS = "Attachments_Support";
+    protected static final Logger log = Logger.getLogger(AttachmentsTask.class);
+            
+    public static final String      ATTACHMENTS                = "Attachments_Support";
+    public static final String      NEW_ATTACHMENT_ACTION      = "NewAttachment";
+    public static final String      GET_ATTACHMENT_INFO_ACTION = "AttachmentInfoRequest";
+    public static final String      OPEN_ATTACHMENT_ACTION     = "OpenAttachment";
     
     protected List<ToolBarItemDesc> toolBarItems;
     protected List<MenuItemDesc> menuItems;
@@ -36,6 +44,15 @@ public class AttachmentsTask extends BaseTask
         menuItems = new Vector<MenuItemDesc>();
     }
     
+    /* (non-Javadoc)
+     * @see edu.ku.brc.af.tasks.BaseTask#initialize()
+     */
+    @Override
+    public void initialize()
+    {
+        super.initialize();
+    }
+
     /* (non-Javadoc)
      * @see edu.ku.brc.af.tasks.BaseTask#getMenuItems()
      */
@@ -61,5 +78,36 @@ public class AttachmentsTask extends BaseTask
     public List<ToolBarItemDesc> getToolBarItems()
     {
         return toolBarItems;
+    }
+    
+    //-------------------------------------------------------
+    // CommandListener Interface
+    //-------------------------------------------------------
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.af.tasks.BaseTask#doCommand(edu.ku.brc.ui.CommandAction)
+     */
+    @Override
+    public void doCommand(CommandAction cmdAction)
+    {
+        if (cmdAction.getType()!=ATTACHMENTS)
+        {
+            // not a format I recognize, maybe it's for somebody else
+            return;
+        }
+        
+        String action = cmdAction.getAction();
+        if (action.equals(NEW_ATTACHMENT_ACTION))
+        {
+            // TODO: implement this
+        }
+        else if (action.equals(GET_ATTACHMENT_INFO_ACTION))
+        {
+            // TODO: implement this
+        }
+        else if (action.equals(OPEN_ATTACHMENT_ACTION))
+        {
+            // TODO: implement this
+        }
     }
 }
