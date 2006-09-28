898c81b88858336efbbab3f16d6675ca616299f0
Josh Stewart <jds@ku.edu>
2006-09-28 17:55:11 +0000

    Cleaned up DeterminationStatus.java.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@860 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/DeterminationStatus.java
+++ src/edu/ku/brc/specify/datamodel/DeterminationStatus.java
@@ -1,18 +1,7 @@
-/* This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 /**
+ * Copyright (C) 2006  The University of Kansas
+ *
+ * [INSERT KU-APPROVED LICENSE TEXT HERE]
  * 
  */
 package edu.ku.brc.specify.datamodel;
@@ -23,119 +12,148 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- *
- *
+ * 
+ * @code_status Beta
  * @author jstewart
- * @version %I% %G%
  */
 public class DeterminationStatus extends DataModelObjBase implements Serializable
 {
-	protected Long determinationStatusId;
-	protected String name;
-	protected String remarks;
-    protected Date timestampCreated;
-    protected Date timestampModified;
-    protected String lastEditedBy;
-	protected Set<Determination> determinations;
-	
-	public DeterminationStatus()
-	{
-		super();
-	}
-	
-	public DeterminationStatus(Long determinationStatusId)
-	{
-		super();
-		this.determinationStatusId = determinationStatusId;
-	}
-	
-	public void initialize()
-	{
-		determinationStatusId = null;
-		name = null;
+    protected Long               determinationStatusId;
+    protected String             name;
+    protected String             remarks;
+    protected Date               timestampCreated;
+    protected Date               timestampModified;
+    protected String             lastEditedBy;
+    protected Set<Determination> determinations;
+
+    public DeterminationStatus()
+    {
+        super();
+    }
+
+    public DeterminationStatus(Long determinationStatusId)
+    {
+        super();
+        this.determinationStatusId = determinationStatusId;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#initialize()
+     */
+    @Override
+    public void initialize()
+    {
+        determinationStatusId = null;
+        name = null;
         remarks = null;
         timestampCreated = new Date();
         timestampModified = null;
         lastEditedBy = null;
-		determinations = new HashSet<Determination>();
-	}
+        determinations = new HashSet<Determination>();
+    }
 
-	public Set<Determination> getDeterminations()
-	{
-		return determinations;
-	}
+    public Set<Determination> getDeterminations()
+    {
+        return determinations;
+    }
 
-	public void setDeterminations(Set<Determination> determinations)
-	{
-		this.determinations = determinations;
-	}
+    public void setDeterminations(Set<Determination> determinations)
+    {
+        this.determinations = determinations;
+    }
 
-	public Long getDeterminationStatusId()
-	{
-		return determinationStatusId;
-	}
+    public Long getDeterminationStatusId()
+    {
+        return determinationStatusId;
+    }
 
-    /**
-     * Generic Getter for the ID Property.
-     * @returns ID Property.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getId()
      */
+    @Override
     public Long getId()
     {
         return this.determinationStatusId;
     }
 
-	public void setDeterminationStatusId(Long determinationStatusId)
-	{
-		this.determinationStatusId = determinationStatusId;
-	}
+    public void setDeterminationStatusId(Long determinationStatusId)
+    {
+        this.determinationStatusId = determinationStatusId;
+    }
 
-	public String getName()
-	{
-		return name;
-	}
+    public String getName()
+    {
+        return name;
+    }
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 
-	public String getRemarks()
-	{
-		return remarks;
-	}
+    public String getRemarks()
+    {
+        return remarks;
+    }
 
-	public void setRemarks(String remarks)
-	{
-		this.remarks = remarks;
-	}
+    public void setRemarks(String remarks)
+    {
+        this.remarks = remarks;
+    }
 
-	public String getLastEditedBy()
-	{
-		return lastEditedBy;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getLastEditedBy()
+     */
+    @Override
+    public String getLastEditedBy()
+    {
+        return lastEditedBy;
+    }
 
-	public void setLastEditedBy(String lastEditedBy)
-	{
-		this.lastEditedBy = lastEditedBy;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#setLastEditedBy(java.lang.String)
+     */
+    @Override
+    public void setLastEditedBy(String lastEditedBy)
+    {
+        this.lastEditedBy = lastEditedBy;
+    }
 
-	public Date getTimestampCreated()
-	{
-		return timestampCreated;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getTimestampCreated()
+     */
+    @Override
+    public Date getTimestampCreated()
+    {
+        return timestampCreated;
+    }
 
-	public void setTimestampCreated(Date timestampCreated)
-	{
-		this.timestampCreated = timestampCreated;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#setTimestampCreated(java.util.Date)
+     */
+    @Override
+    public void setTimestampCreated(Date timestampCreated)
+    {
+        this.timestampCreated = timestampCreated;
+    }
 
-	public Date getTimestampModified()
-	{
-		return timestampModified;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getTimestampModified()
+     */
+    @Override
+    public Date getTimestampModified()
+    {
+        return timestampModified;
+    }
 
-	public void setTimestampModified(Date timestampModified)
-	{
-		this.timestampModified = timestampModified;
-	}
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#setTimestampModified(java.util.Date)
+     */
+    @Override
+    public void setTimestampModified(Date timestampModified)
+    {
+        this.timestampModified = timestampModified;
+    }
 }
