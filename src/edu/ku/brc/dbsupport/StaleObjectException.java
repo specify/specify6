e5b5e471e85f49ac1455ded905068922bc8acd33
Rod Spears <rods@ku.edu>
2006-10-12 12:28:09 +0000

    missed adding file
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@908 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/dbsupport/StaleObjectException.java
@@ -0,0 +1,56 @@
+/**
+* Copyright (C) 2006  The University of Kansas
+*
+* [INSERT KU-APPROVED LICENSE TEXT HERE]
+*
+*/
+package edu.ku.brc.dbsupport;
+
+/**
+ * Simple wrapper for Hibernate's StaleObjectStateException.
+ * s
+ * @author rods
+ *
+ * @code_status Complete
+ *
+ */
+public class StaleObjectException extends Exception
+{
+
+    /**
+     * Default Constructor.
+     */
+    public StaleObjectException()
+    {
+        // node code
+    }
+
+    /**
+     * Constructs with a message.
+     * @param message the message
+     */
+    public StaleObjectException(String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Constructs with a Throwable.
+     * @param cause the Throwable
+     */
+    public StaleObjectException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    /**
+     * Constructs with a message and Throwable.
+     * @param message the message
+     * @param cause Throwable
+     */
+    public StaleObjectException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+}
