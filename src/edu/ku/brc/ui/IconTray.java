1c65bf1906b192af2b06f86cdc830911e1ac3bc5
Josh Stewart <jds@ku.edu>
2006-12-04 17:00:45 +0000

    removed unused import
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1122 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/ui/IconTray.java
+++ src/edu/ku/brc/ui/IconTray.java
@@ -27,7 +27,6 @@ import javax.swing.ScrollPaneConstants;
 
 import org.apache.log4j.Logger;
 
-import edu.ku.brc.specify.datamodel.DataModelObjBase;
 import edu.ku.brc.ui.forms.FormDataObjIFace;
 import edu.ku.brc.ui.renderers.TrayListCellRenderer;
 import edu.ku.brc.util.FormDataObjComparator;
