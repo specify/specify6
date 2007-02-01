9df81d9b25d2056200f391bacebedbef5e70d634
Josh Stewart <jds@ku.edu>
2007-02-01 22:46:59 +0000

    Adding the needed change to get annotations working.  Right now the line is commentted out.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1313 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/dbsupport/HibernateUtil.java
+++ src/edu/ku/brc/dbsupport/HibernateUtil.java
@@ -14,6 +14,7 @@ import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.dialect.function.ClassicAvgFunction;
@@ -208,6 +209,7 @@ public class HibernateUtil {
         try 
         {
             configuration = new Configuration();
+//            configuration = new AnnotationConfiguration();
             AuditInterceptor auditInter = AuditInterceptor.getInstance();
             if (auditInter != null)
             {
