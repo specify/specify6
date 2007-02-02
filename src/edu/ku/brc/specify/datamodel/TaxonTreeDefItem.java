14253bc4a39cd1bb49b5fee9cdb9ace0648fe142
Josh Stewart <jds@ku.edu>
2007-02-02 16:46:45 +0000

    Changed from FetchType.EAGER to FetchType.LAZY on a few things in the taxon-related classes.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1317 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/TaxonTreeDefItem.java
+++ src/edu/ku/brc/specify/datamodel/TaxonTreeDefItem.java
@@ -1,30 +1,8 @@
-/* This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+/**
+ * Copyright (C) 2006  The University of Kansas
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-/* This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * [INSERT KU-APPROVED LICENSE TEXT HERE]
+ * 
  */
 package edu.ku.brc.specify.datamodel;
 
@@ -220,7 +198,7 @@ public class TaxonTreeDefItem extends DataModelObjBase implements Serializable, 
         this.fullNameSeparator = fullNameSeparator;
     }
 
-    @ManyToOne(cascade = {}, fetch = FetchType.EAGER)
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
     @Cascade( { CascadeType.SAVE_UPDATE, CascadeType.LOCK })
     @JoinColumn(name = "TaxonTreeDefID", unique = false, nullable = false, insertable = true, updatable = true)
     public TaxonTreeDef getTreeDef()
@@ -233,7 +211,7 @@ public class TaxonTreeDefItem extends DataModelObjBase implements Serializable, 
 		this.treeDef = treeDef;
 	}
 
-    @ManyToOne(cascade = {}, fetch = FetchType.EAGER)
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
     @Cascade( { CascadeType.SAVE_UPDATE, CascadeType.LOCK })
     @JoinColumn(name = "ParentItemID", unique = false, nullable = true, insertable = true, updatable = true)
 	public TaxonTreeDefItem getParent()
