e83d38ae2ed3bddb2a04f1b11b0825714a0990b1
Josh Stewart <jds@ku.edu>
2007-02-21 05:40:50 +0000

    More Javadocs.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1415 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/ui/DefaultClassActionHandler.java
+++ src/edu/ku/brc/ui/DefaultClassActionHandler.java
@@ -10,20 +10,35 @@ import java.awt.event.ActionListener;
 import java.util.Hashtable;
 
 /**
+ * A class that provides mappings from classes to ActionListeners
+ * that should fire when objects of those classes are 'activated'
+ * in some manner, most likely by a double-click action from a user.
  *
- * @code_status Alpha
+ * @code_status Beta
  * @author jstewart
  */
 public class DefaultClassActionHandler
 {
+    /** The singleton instance of the class. */
     protected static DefaultClassActionHandler instance;
+    
+    /** The mapping from classes to the action handlers. */
     protected Hashtable<Class<?>, ActionListener> handlers;
     
+    /**
+     * Creates an instance.
+     */
     protected DefaultClassActionHandler()
     {
         handlers = new Hashtable<Class<?>, ActionListener>();
     }
     
+    /**
+     * Returns the singleton instance of the class, creating one
+     * if needed.
+     *
+     * @return the singleton instance
+     */
     public static synchronized DefaultClassActionHandler getInstance()
     {
         if (instance==null)
@@ -34,11 +49,25 @@ public class DefaultClassActionHandler
         return instance;
     }
     
+    /**
+     * Registers the given {@link ActionListener} to handle actions on instances
+     * of the given class.
+     *
+     * @param clazz the class handled by the given ActionListener
+     * @param handler the handler
+     */
     public void registerActionHandler(Class<?> clazz, ActionListener handler)
     {
         handlers.put(clazz, handler);
     }
     
+    /**
+     * Returns the handler registered for the given class, or null if one
+     * is not registered.
+     *
+     * @param clazz the class in question
+     * @return the registered handler, or null if none is registered
+     */
     public ActionListener getDefaultClassActionHandler(Class<?> clazz)
     {
         return handlers.get(clazz);
