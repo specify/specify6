2406f3f374fffb2d3e8b7ee92bba9619b9246190
Josh Stewart <jds@ku.edu>
2007-02-28 03:18:08 +0000

    Created a static helper method (in a new class) to help load all of the objects in a RecordSet.  If something like this already exists, we can remove this one, of course.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1438 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/dbsupport/RecordSetLoader.java
@@ -0,0 +1,31 @@
+package edu.ku.brc.specify.dbsupport;
+
+import java.util.List;
+import java.util.Vector;
+
+import edu.ku.brc.dbsupport.DBTableIdMgr;
+import edu.ku.brc.dbsupport.DataProviderFactory;
+import edu.ku.brc.dbsupport.DataProviderIFace;
+import edu.ku.brc.dbsupport.DataProviderSessionIFace;
+import edu.ku.brc.dbsupport.RecordSetIFace;
+import edu.ku.brc.dbsupport.RecordSetItemIFace;
+import edu.ku.brc.dbsupport.DBTableIdMgr.TableInfo;
+
+public class RecordSetLoader
+{
+	public static List<Object> loadRecordSet(RecordSetIFace recordSet)
+	{
+		TableInfo tableInfo = DBTableIdMgr.getInfoById(recordSet.getTableId());
+		Class<?> recordClass = tableInfo.getClassObj();
+		DataProviderIFace dataProvider = DataProviderFactory.getInstance();
+		DataProviderSessionIFace session = dataProvider.createSession();
+		Vector<Object> records = new Vector<Object>();
+		for (RecordSetItemIFace rsItem: recordSet.getItems())
+		{
+			Long id = rsItem.getRecordId();
+			Object record = session.get(recordClass,id);
+			records.add(record);
+		}
+		return records;
+	}
+}
