a8bc86aa77761f66ee4132ff6428cfeb7d45ec31
Josh Stewart <jds@ku.edu>
2007-03-16 16:45:28 +0000

    Got the GenericKmlGenerator up and running.  Now I have to make use of it from the Google Earth Exporter.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@1538 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/util/services/GenericKmlGenerator.java
+++ src/edu/ku/brc/util/services/GenericKmlGenerator.java
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 import edu.ku.brc.util.Pair;
 
 /**
@@ -27,15 +29,26 @@ import edu.ku.brc.util.Pair;
  */
 public class GenericKmlGenerator
 {
-    /**
-     * A list of all points to be included in the generated KML.
-     */
-    List<Pair<Double, Double>> points = new Vector<Pair<Double,Double>>();
+    /** Logger used to emit any messages from this class. */
+    private static final Logger log = Logger.getLogger(GenericKmlGenerator.class);
 
-    /**
-     * A mapping from a geo reference point to the HTML text description for that point.
-     */
-    Map<Pair<Double,Double>, String> pointDescMap = new Hashtable<Pair<Double,Double>, String>();
+    /** Standard XML file type declaration. */
+    protected static String XML_DECLARATION = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    /** Keyhole Markup Language namespace declaration. */
+    protected static String KML_NAMESPACE_DECL = "<kml xmlns=\"http://earth.google.com/kml/2.1\">\n";
+
+    /** A list of all points to be included in the generated KML. */
+    protected List<Pair<Double, Double>> points = new Vector<Pair<Double,Double>>();
+
+    /** A mapping from a geo reference point to the name of the placemark. */
+    protected Map<Pair<Double,Double>, String> pointNameMap = new Hashtable<Pair<Double,Double>, String>();
+    
+    /** A mapping from a geo reference point to the HTML text description for that point. */
+    protected Map<Pair<Double,Double>, String> pointDescMap = new Hashtable<Pair<Double,Double>, String>();
+    
+    /** A URL to an image file to be used as the placemark icon. */
+    protected String placemarkIconURL;
     
     /**
      * Constructs an instance.
@@ -46,29 +59,119 @@ public class GenericKmlGenerator
     }
     
     /**
-     * Includes the given point and HTML text to be included in the generated KML output.
+     * Adds the given point, name and HTML text to be included in the generated KML output.
      * 
      * @param point the geo reference point
+     * @param name the name of the placemark
      * @param htmlDescription the text description (as HTML)
      */
-    public void addPointAndDescription(Pair<Double,Double> point, String htmlDescription)
+    public void addPlacemark(Pair<Double,Double> point, String name, String htmlDescription)
     {
+        log.debug("Adding point to KML generator: " + point + "   " + htmlDescription);
         points.add(point);
+        pointNameMap.put(point, name);
         pointDescMap.put(point, htmlDescription);
     }
     
     /**
-     * Generates KML output based on the current points and descriptions given to the generator.
+     * Returns the URL to the placemark icon.
+     * 
+     * @return the URL to the icon image
+     */
+    public String getPlacemarkIconURL()
+    {
+        return placemarkIconURL;
+    }
+
+    /**
+     * Sets the URL to the placemark icon.
+     * 
+     * @param placemarkIconURL the URL to the icon image
+     */
+    public void setPlacemarkIconURL(String placemarkIconURL)
+    {
+        this.placemarkIconURL = placemarkIconURL;
+    }
+
+    /**
+     * Generates KML output based on the current points, names and descriptions given to the generator.
      * 
      * @return a String containing the generated KML
      */
     public String generateKML()
     {
-        return "";
+        StringBuilder kmlBuilder = new StringBuilder();
+        kmlBuilder.append(XML_DECLARATION);
+        kmlBuilder.append(KML_NAMESPACE_DECL);
+        kmlBuilder.append("<Document>\n");
+
+        // setup the custom icon, if any
+        if (placemarkIconURL!=null)
+        {
+            kmlBuilder.append("<Style id=\"customPlacemark\">\n");
+            kmlBuilder.append("<IconStyle>\n");
+            kmlBuilder.append("<Icon>\n");
+            kmlBuilder.append("<href>");
+            kmlBuilder.append(placemarkIconURL);
+            kmlBuilder.append("</href>\n");
+            kmlBuilder.append("</Icon>\n");
+            kmlBuilder.append("</IconStyle>\n");
+            kmlBuilder.append("</Style>\n"); 
+        }
+        
+        // generate a placemark for each point
+        for (Pair<Double,Double> point: points)
+        {
+            String name = pointNameMap.get(point);
+            String htmlDesc = pointDescMap.get(point);
+            kmlBuilder.append(buildPlacemark(point,name,htmlDesc));
+        }
+        
+        kmlBuilder.append("</Document>\n");
+        kmlBuilder.append("</kml>\n");
+        return kmlBuilder.toString();
+    }
+    
+    protected String buildPlacemark(Pair<Double,Double> point, String name, String htmlDesc)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(generateXmlStartTag("Placemark"));
+        sb.append(generateXmlElement("name", name));
+        if (placemarkIconURL!=null)
+        {
+            sb.append(generateXmlElement("styleURL", "#customPlacemark"));
+        }
+        sb.append(generateXmlElement("description", "<![CDATA[" + htmlDesc + "]]>"));
+        sb.append(buildPointAndLookAt(point));
+        sb.append(generateXmlEndTag("Placemark"));
+        
+        return sb.toString();
+    }
+    
+    protected String buildPointAndLookAt(Pair<Double,Double> point)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<LookAt>\n");
+        sb.append("<latitude>");
+        sb.append(point.first);
+        sb.append("</latitude>\n");
+        sb.append("<longitude>");
+        sb.append(point.second);
+        sb.append("</longitude>\n");
+        sb.append("<range>300000.00</range>\n");
+        sb.append("</LookAt>\n");
+        sb.append("<Point>\n");
+        sb.append("<coordinates>");
+        sb.append(point.second);
+        sb.append(",");
+        sb.append(point.first);
+        sb.append("</coordinates>\n");
+        sb.append("</Point>\n");
+        return sb.toString();
     }
     
     /**
-     * Generates KML output based on the current points and descriptions given to the generator.
+     * Generates KML output based on the current points, names and descriptions given to the generator.
      * 
      * @param out a stream to which the KML is written
      * @throws IOException if an I/O error occurs
@@ -80,7 +183,7 @@ public class GenericKmlGenerator
     }
 
     /**
-     * Generates KML output based on the current points and descriptions given to the generator.
+     * Generates KML output based on the current points, names and descriptions given to the generator.
      * 
      * @param outputFile the file in which to write the KML output
      * @throws IOException if an I/O error occurs while writing the output
@@ -91,10 +194,39 @@ public class GenericKmlGenerator
         generateKML(fos);
     }
     
+    protected String generateXmlStartTag(String name)
+    {
+        return "<" + name + ">";
+    }
+
+    protected String generateXmlEndTag(String name)
+    {
+        return "</" + name + ">";
+    }
+    
+    protected String generateXmlElement(String name, String content)
+    {
+        if (content == null || content.equals("") )
+        {
+            return "<" + name + "/>";
+        }
+
+        return "<" + name + ">" + content + "</" + name + ">";
+    }
+    
     /**
      * @param args
      */
     public static void main(String[] args)
     {
+        // tests
+        Pair<Double,Double> allenFieldHouse = new Pair<Double, Double>(38.954,-95.252);
+        String name = "Allen Fieldhouse";
+        String htmlDesc = "<b>Allen Fieldhouse</b>\n<br/><a href=\"http://www.google.com\">Google</a>";
+        GenericKmlGenerator kmlGen = new GenericKmlGenerator();
+        kmlGen.setPlacemarkIconURL("http://redbud.nhm.ku.edu/specify/images/MammalCircle.png");
+        kmlGen.addPlacemark(allenFieldHouse, name, htmlDesc);
+        
+        System.out.println(kmlGen.generateKML());
     }
 }
