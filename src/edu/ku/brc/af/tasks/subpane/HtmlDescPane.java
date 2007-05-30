6b61c57d8ecd3c9ecbcbeb37aa53de5913b6823c
Josh Stewart <jds@ku.edu>
2007-05-30 15:54:47 +0000

    Documentation
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2215 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/af/tasks/subpane/HtmlDescPane.java
+++ src/edu/ku/brc/af/tasks/subpane/HtmlDescPane.java
@@ -15,23 +15,21 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
+import edu.ku.brc.af.core.SubPaneIFace;
 import edu.ku.brc.af.core.Taskable;
 import edu.ku.brc.util.AttachmentUtils;
 
 /**
- * 
- * Creates a SubPane that display HTML.
+ * A {@link SubPaneIFace} that displays HTML content.
  * 
  * @author jds
- *
- * @code_status Alpha
- *
- *
+ * @code_status Complete
  */
 public class HtmlDescPane extends SimpleDescPane implements HyperlinkListener
 {
     /**
-     * Creates a SubPane that display HTML.
+     * Constructor.
+     * 
      * @param name the name of the pane
      * @param task the owning task
      * @param htmlDesc the HTML to be rendered
@@ -63,6 +61,7 @@ public class HtmlDescPane extends SimpleDescPane implements HyperlinkListener
      */
     public void hyperlinkUpdate(HyperlinkEvent e)
     {
+        // if a hyperlink was clicked, try to open it in an external viewer
         if (e.getEventType()==HyperlinkEvent.EventType.ACTIVATED)
         {
             try
