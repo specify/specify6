f5f78fa6c597c2f971125844bece666000f991c3
Josh Stewart <jds@ku.edu>
2007-06-08 19:42:03 +0000

    Small mod to improve performance a VERY LITTLE bit.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2288 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/wb/WorkbenchPaneSS.java
+++ src/edu/ku/brc/specify/tasks/subpane/wb/WorkbenchPaneSS.java
@@ -424,8 +424,8 @@ public class WorkbenchPaneSS extends BaseSubPane
             public void actionPerformed(ActionEvent ae)
             {
                 AppPreferences remotePrefs = AppPreferences.getRemote();
-                String tool = remotePrefs.get("georef_tool", null);
-                if (StringUtils.isNotEmpty(tool) && tool.equalsIgnoreCase("geolocate"))
+                String tool = remotePrefs.get("georef_tool", "");
+                if (tool.equalsIgnoreCase("geolocate"))
                 {
                     doGeoLocateLookup();    
                 } else
