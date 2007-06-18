555490dc7b9abf01a28d7076e7b7070eb6fc410d
Josh Stewart <jds@ku.edu>
2007-06-18 21:37:55 +0000

    removed a couple of unused imports from WorkbenchPaneSS
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2323 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/wb/WorkbenchPaneSS.java
+++ src/edu/ku/brc/specify/tasks/subpane/wb/WorkbenchPaneSS.java
@@ -33,8 +33,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
