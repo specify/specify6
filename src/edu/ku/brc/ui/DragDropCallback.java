de5a798e2a8d8a885d9d1a29e6cd46577b89c824
Josh Stewart <jds@ku.edu>
2007-07-05 20:01:08 +0000

    more docs for a DnD interface
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2390 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/ui/DragDropCallback.java
+++ src/edu/ku/brc/ui/DragDropCallback.java
@@ -2,22 +2,31 @@ package edu.ku.brc.ui;
 
 /**
  * Provides callbacks for drop events on JLists.
- 
- * @code_status Beta
- **
+ *
+ * @code_status Complete
  * @author jstewart
- * @version %I% %G%
  */
 public interface DragDropCallback
 {
-	/**
-	 * Performs all tasks required by the event of <code>dragged</code>
-	 * being dropped on <code>droppedOn</code>.
-	 *
-	 * @param draggedObj the dragged object
-	 * @param dropLocObj the object that <code>dragged</code> was dropped on
-	 */
-	public boolean dropOccurred(Object dragged, Object droppedOn, int dropAction);
-	
-	public boolean dropAcceptable(Object dragged, Object droppedOn, int dropAction);
+    /**
+     * Performs all tasks required by the event of <code>dragged</code>
+     * being dropped on <code>droppedOn</code>.
+     *
+     * @param draggedObj the dragged object
+     * @param dropLocObj the object that <code>dragged</code> was dropped on
+     * @param dropAction the type of drop action
+     * @return true if the drop was successfully handled
+     */
+    public boolean dropOccurred(Object dragged, Object droppedOn, int dropAction);
+    
+    /**
+     * Determines if a drop of <code>dragged</code> being dropped on <code>droppedOn</code>
+     * is acceptable.
+     * 
+     * @param dragged the dragged object
+     * @param droppedOn the object that <code>dragged</code> is being dropped on
+     * @param dropAction the type of drop action
+     * @return true if the drop is acceptable
+     */
+    public boolean dropAcceptable(Object dragged, Object droppedOn, int dropAction);
 }
