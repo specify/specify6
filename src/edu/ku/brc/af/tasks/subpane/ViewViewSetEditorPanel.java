a3a016302b09549c01f397ba4d9a0a442aec8f46
Rod Spears <rods@ku.edu>
2007-10-10 18:46:16 +0000

    Adding missing class
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2842 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/af/tasks/subpane/ViewViewSetEditorPanel.java
@@ -0,0 +1,270 @@
+/*
+     * Copyright (C) 2007  The University of Kansas
+     *
+     * [INSERT KU-APPROVED LICENSE TEXT HERE]
+     *
+     */
+/**
+ * 
+ */
+package edu.ku.brc.af.tasks.subpane;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import edu.ku.brc.af.core.AppContextMgr;
+import edu.ku.brc.ui.UIHelper;
+import edu.ku.brc.ui.forms.MultiView;
+import edu.ku.brc.ui.forms.persist.AltViewIFace;
+import edu.ku.brc.ui.forms.persist.ViewDefIFace;
+import edu.ku.brc.ui.forms.persist.ViewIFace;
+import edu.ku.brc.ui.forms.persist.ViewSetIFace;
+
+/**
+ * @author rod
+ *
+ * @code_status Alpha
+ *
+ * Sep 29, 2007
+ *
+ */
+public class ViewViewSetEditorPanel extends JPanel
+{
+    protected ViewSetIFace         viewSet;
+    protected Vector<ViewIFace>    views;
+    protected Vector<ViewDefIFace> viewDefs;
+    
+    protected ViewIFace            selectedView    = null;
+    protected AltViewIFace         selectedAltView = null;
+    
+    // UI
+    protected JList                viewList;
+    protected JList                altViewList;
+    protected JList                viewDefList;
+    protected DefaultListModel     altViewModel = new DefaultListModel();
+    
+    
+    protected MultiView            viewsMultiView;
+    protected ViewIFace            viewsFormView;
+    protected MultiView            altViewsMultiView;
+    protected ViewIFace            altViewsFormView;
+    protected MultiView            viewDefMultiView;
+    protected ViewIFace            viewDefFormView;
+    
+    protected CardLayout           cardLayout = new CardLayout();
+    protected JPanel               cardPanel;
+    
+    /**
+     * 
+     */
+    public ViewViewSetEditorPanel(final ViewSetIFace viewSet)
+    {
+        this.viewSet = viewSet;
+        
+        PanelBuilder    pb = new PanelBuilder(new FormLayout("f:p:g,max(200;p),10px,p,f:p:g", 
+                                                             "p,2px,p,2px,p,2px,p,2px,p,2px,p,10px, p,5px,p,2px,p,2px,p,p:g"), this);
+        CellConstraints cc = new CellConstraints();
+        
+        viewsFormView = AppContextMgr.getInstance().getView("SystemSetup", "ViewProps");
+        
+        viewsMultiView = new MultiView(null,
+                                  null, 
+                                  viewsFormView, 
+                                  AltViewIFace.CreationMode.EDIT,
+                                  MultiView.IS_EDITTING, null);
+
+        altViewsFormView = AppContextMgr.getInstance().getView("SystemSetup", "AltViewProps");
+        
+        altViewsMultiView = new MultiView(null,
+                                  null, 
+                                  altViewsFormView, 
+                                  AltViewIFace.CreationMode.EDIT,
+                                  MultiView.IS_EDITTING, null);
+
+        viewDefFormView = AppContextMgr.getInstance().getView("SystemSetup", "ViewDefProps");
+        
+        viewDefMultiView = new MultiView(null,
+                                  null, 
+                                  viewDefFormView, 
+                                  AltViewIFace.CreationMode.EDIT,
+                                  MultiView.IS_EDITTING, null);
+
+        cardPanel = new JPanel(cardLayout);
+        cardPanel.add("View",    viewsMultiView);
+        cardPanel.add("AltView", altViewsMultiView);
+        
+        //setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 10));
+        
+        views = new Vector<ViewIFace>(viewSet.getViews().values());
+        Collections.sort(views);
+        
+        viewDefs = new Vector<ViewDefIFace>(viewSet.getViewDefs().values());
+        Collections.sort(views);
+        
+        viewList    = new JList(views);
+        altViewList = new JList(altViewModel);
+        viewDefList = new JList(viewDefs);
+        
+        altViewList.setVisibleRowCount(4);
+        
+        JPanel viewBtnBar    = createViewListBtnBar();
+        JPanel altViewBtnBar = createAltViewListBtnBar();
+        JPanel viewDefBtnBar = createAltViewListBtnBar();
+        
+        JScrollPane vsp  = new JScrollPane(viewList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane asp  = new JScrollPane(altViewList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane vdsp = new JScrollPane(viewDefList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
+        pb.add(new JLabel("Views"),     cc.xy(2, 1));
+        pb.add(vsp,                     cc.xy(2, 3));
+        pb.add(viewBtnBar,              cc.xy(2, 5));
+        
+
+        pb.add(new JLabel("Alt Views"), cc.xy(2, 7));
+        pb.add(asp,                     cc.xy(2, 9));
+        pb.add(altViewBtnBar,           cc.xy(2, 11));
+
+        pb.add(cardPanel,               cc.xywh(4, 1, 1, 11));
+        
+        pb.addSeparator("",             cc.xywh(1, 13, 5, 1));
+        
+        pb.add(new JLabel("View Defs"), cc.xy(2, 15));
+        pb.add(vdsp,                    cc.xy(2, 17));
+        pb.add(viewDefBtnBar,           cc.xy(2, 19));
+        
+        pb.add(viewDefMultiView,        cc.xywh(4, 15, 1, 6));
+
+        // Hooking upListeners
+        viewList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e)
+            {
+                if (!e.getValueIsAdjusting())
+                {
+                    viewSelected();
+                }
+            }
+        });
+        
+        altViewList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e)
+            {
+                if (!e.getValueIsAdjusting())
+                {
+                    altViewSelected();
+                }
+            }
+        });
+        viewDefList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e)
+            {
+                if (!e.getValueIsAdjusting())
+                {
+                    viewDefSelected();
+                }
+            }
+        });
+    }
+    
+    /**
+     * 
+     */
+    protected void viewSelected()
+    {
+        ViewIFace view = (ViewIFace)viewList.getSelectedValue();
+        if (view != selectedView)
+        {
+            // fill
+            selectedView = view;
+            viewsMultiView.setData(view);
+            
+            altViewModel.clear();
+            for (AltViewIFace av : selectedView.getAltViews())
+            {
+                altViewModel.addElement(av);
+            }
+        }
+        cardLayout.show(cardPanel, "View");
+    }
+    
+    /**
+     * 
+     */
+    protected void altViewSelected()
+    {
+        AltViewIFace altView = (AltViewIFace)altViewList.getSelectedValue();
+        if (altView != selectedAltView)
+        {
+            selectedAltView = altView;
+            altViewsMultiView.setData(selectedAltView);
+        }
+        cardLayout.show(cardPanel, "AltView");
+    }
+    
+    /**
+     * 
+     */
+    protected void viewDefSelected()
+    {
+        ViewDefIFace viewDef = (ViewDefIFace)viewDefList.getSelectedValue();
+        viewDefMultiView.setData(viewDef);
+    }
+    
+    /**
+     * @return
+     */
+    protected JPanel createViewListBtnBar()
+    {
+        ActionListener addAL = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            }
+        };
+        ActionListener removeAL = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            }
+        };
+        return UIHelper.createAddRemoveEditBtnBar(addAL, "", removeAL, "", null, null);
+    }
+    
+    /**
+     * @return
+     */
+    protected JPanel createAltViewListBtnBar()
+    {
+        ActionListener addAL = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            }
+        };
+        ActionListener removeAL = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            }
+        };
+        return UIHelper.createAddRemoveEditBtnBar(addAL, "", removeAL, "", null, null);
+    }
+
+}
