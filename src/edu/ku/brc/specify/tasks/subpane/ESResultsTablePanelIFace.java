e11f91d0812701094191d43bc7f1909490f209ad
Rod Spears <rods@ku.edu>
2007-10-22 21:08:34 +0000

    Didn't mean to check in the other files that use this
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2906 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/tasks/subpane/ESResultsTablePanelIFace.java
@@ -0,0 +1,66 @@
+/* This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ku.brc.specify.tasks.subpane;
+
+import java.awt.Component;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import edu.ku.brc.af.core.expresssearch.QueryForIdResultsIFace;
+
+/**
+ * The important 
+ * 
+ * @author rods
+ *
+ * @code_status Alpha
+ *
+ * Created Date: Oct 18, 2007
+ *
+ */
+public interface ESResultsTablePanelIFace extends PropertyChangeListener
+{
+    
+    /**
+     * Initializes 
+     * @param esrPane
+     * @param results
+     */
+    public abstract void initialize(final ExpressSearchResultsPaneIFace esrPane,
+                                    final QueryForIdResultsIFace        results);
+    /**
+     * @return the UI component for the results panel
+     */
+    public Component getUIComponent();
+    
+    /**
+     * Cleans up references to other objects.
+     */
+    public abstract void cleanUp();
+
+    /**
+     * Returns a list of recordIds.
+     * @param returnAll indicates whether all the records should be returned if nothing was selected
+     * @return a list of recordIds
+     */
+    public abstract List<Integer> getListOfIds(final boolean returnAll);
+
+    /** 
+     * Register a single property listener.
+     * @param pcl the listener
+     */
+    public abstract void setPropertyChangeListener(PropertyChangeListener pcl);
+    
+}
