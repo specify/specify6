b55e3a96e1074a62377709b9de8d8c56af707c84
Josh Stewart <jds@ku.edu>
2007-10-24 21:16:13 +0000

    removed unused import
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@2933 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/BaseTreeTask.java
+++ src/edu/ku/brc/specify/tasks/BaseTreeTask.java
@@ -49,7 +49,6 @@ import edu.ku.brc.ui.GetSetValueIFace;
 import edu.ku.brc.ui.RolloverCommand;
 import edu.ku.brc.ui.UIRegistry;
 import edu.ku.brc.ui.forms.FormViewObj;
-import edu.ku.brc.ui.forms.MultiView;
 import edu.ku.brc.ui.forms.persist.AltViewIFace.CreationMode;
 import edu.ku.brc.ui.forms.validation.ValComboBox;
 
