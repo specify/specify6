8cf4bedcdc958139cd18b1b0606e29c19e69f7eb
Josh Stewart <jds@ku.edu>
2007-11-15 22:32:07 +0000

    removed a couple of unused imports
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@3108 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/BaseTreeTask.java
+++ src/edu/ku/brc/specify/tasks/BaseTreeTask.java
@@ -11,8 +11,6 @@ import static edu.ku.brc.ui.UIRegistry.getResourceString;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.Collections;
