0d94fcd44d5c29ee941b2111973cff5c004326c0
Rod Spears <rods@ku.edu>
2008-05-22 19:37:23 +0000

    Added ireports
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@4133 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- packaging/mkzips.sh
+++ packaging/mkzips.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-tar -cvf windows.tar Specify SpecifyBuilder.exe SpecifyDBInit.exe Specify.exe SpecifyLocalizer.exe
+tar -cvf windows.tar Specify SpecifyBuilder.exe SpecifyDBInit.exe Specify.exe SpecifyLocalizer.exe iReports4Specify.exe
 gzip windows.tar
 
-tar -cvf linux.tar Specify specifybuilder.sh specifyschema.sh specify.sh specifydbinit.sh
+tar -cvf linux.tar Specify specifybuilder.sh specifyschema.sh specify.sh specifydbinit.sh ireportlaunch.sh
 gzip linux.tar
 
-tar -cvf mac.tar Specify.app SpecifyBuilder.app SpecifyDBInit.app SpecifySchema.app
+tar -cvf mac.tar Specify.app SpecifyBuilder.app SpecifyDBInit.app SpecifySchema.app iReports4Specify.app
 gzip mac.tar
 
 tar -cvf mac_brief.tar Specify.app SpecifyBuilder.app
