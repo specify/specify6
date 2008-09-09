dba2b4074e0343eed3d0ba387c7641f87dd96b4a
Rod Spears <rods@ku.edu>
2008-09-09 20:04:11 +0000

    Speeding up numeric auto numbering by using straight SQL and a little Hibernate.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@4862 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/dbsupport/CollectionAutoNumber.java
+++ src/edu/ku/brc/specify/dbsupport/CollectionAutoNumber.java
@@ -9,15 +9,24 @@
  */
 package edu.ku.brc.specify.dbsupport;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
+import sun.org.mozilla.javascript.internal.IdScriptableObject;
+
+import edu.ku.brc.af.auth.specify.policy.DatabaseService;
 import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.core.db.AutoNumberGeneric;
 import edu.ku.brc.specify.datamodel.AutoNumberingScheme;
@@ -38,6 +47,8 @@ import edu.ku.brc.util.Pair;
  */
 public class CollectionAutoNumber extends AutoNumberGeneric
 {
+    protected static final Logger log = Logger.getLogger(CollectionAutoNumber.class);
+    
     /**
      * Default Constructor. 
      */
@@ -67,7 +78,7 @@ public class CollectionAutoNumber extends AutoNumberGeneric
                                       final Pair<Integer, Integer> yearPos,
                                       final Pair<Integer, Integer> pos) throws Exception
     {
-        boolean doDebug = false;
+        boolean doDebug = true;
         
         AutoNumberingScheme cns = AppContextMgr.getInstance().getClassObject(Collection.class).getNumberingSchemesByType(CollectionObject.getClassTableId());
         
@@ -82,13 +93,80 @@ public class CollectionAutoNumber extends AutoNumberGeneric
             ids.add(collection.getCollectionId());
         }
         
-        // XXX (Needs try block)
-        Criteria criteria = session.createCriteria(classObj);
-        criteria.addOrder( Order.desc(fieldName) );
-        criteria.createCriteria("collection").add(Restrictions.in("collectionId", ids));
-        criteria.setMaxResults(1);
-        if (doDebug) System.out.println("Criteria ID: "+criteria.toString());
-        List<?> list = criteria.list();
+        // It is amazing how much faster the straigh MySQL is compared to
+        // the Hibernate. I don't think 'setMaxResults' really does much.
+        List<?> list = null;
+        if (false)
+        {
+            // XXX (Needs try block)
+            Criteria criteria = session.createCriteria(classObj);
+            criteria.addOrder( Order.desc(fieldName) );
+            criteria.createCriteria("collection").add(Restrictions.in("collectionId", ids));
+            criteria.setMaxResults(1);
+            if (doDebug) log.debug("Criteria ID: "+criteria.toString());
+            
+            list = criteria.list();
+        } else
+        {
+            StringBuilder sb = new StringBuilder("SELECT ");
+            Connection    conn = null;
+            Statement     stmt = null;
+            
+            sb.append(fieldName);
+            sb.append(" FROM collectionobject INNER JOIN collection ON collectionobject.CollectionMemberID = collection.userGroupScopeId ");
+            sb.append("WHERE collection.userGroupScopeId IN (");
+            for (int i=0;i<ids.size();i++)
+            {
+                if (i > 0) sb.append(',');
+                sb.append(ids.get(i));
+            }
+            sb.append(") ORDER BY ");
+            sb.append(fieldName);
+            sb.append(" desc");
+            log.debug(sb.toString());
+            
+            try
+            {
+                conn = DatabaseService.getInstance().getConnection();
+                stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rs = stmt.executeQuery(sb.toString());
+                
+                String idStr = null;
+                if (rs.next())
+                {
+                    idStr = rs.getString(1);
+                }
+                
+                if (idStr == null)
+                {
+                    return null;
+                }
+                
+                sb.setLength(0);
+                sb.append("FROM "+classObj.getSimpleName()+" WHERE "+fieldName+" = \'"+idStr+"\'");
+                log.debug(sb.toString());
+                Query query = session.createQuery(sb.toString());
+                
+                list = query.list();
+                
+            } catch (SQLException e)
+            {
+                log.error("Exception caught: " + e); //$NON-NLS-1$
+                e.printStackTrace();
+            } finally
+            {
+                try
+                {
+                    if (conn != null)  conn.close();
+                    if (stmt != null)  stmt.close(); 
+                } catch (SQLException e)
+                {
+                    log.error("Exception caught: " + e.toString()); //$NON-NLS-1$
+                    e.printStackTrace();
+                }
+            }
+        }
+        
         if (list.size() == 1)
         {
             if (doDebug) System.out.println("Mac Obj: "+list.get(0));
