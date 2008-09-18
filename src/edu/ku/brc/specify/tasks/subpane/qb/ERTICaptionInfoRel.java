3e218fd7819accc16d55eb6aa1314436dd332291
Tim Noble <timo@ku.edu>
2008-09-18 20:54:42 +0000

    added caching of formatted objects for many-to-one relationships while displaying query results.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@4933 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/qb/ERTICaptionInfoRel.java
+++ src/edu/ku/brc/specify/tasks/subpane/qb/ERTICaptionInfoRel.java
@@ -10,6 +10,9 @@
 package edu.ku.brc.specify.tasks.subpane.qb;
 
 import java.util.Collection;
+import java.util.LinkedList;
+
+import org.apache.log4j.Logger;
 
 import edu.ku.brc.af.core.db.DBRelationshipInfo;
 import edu.ku.brc.af.core.db.DBTableIdMgr;
@@ -19,6 +22,7 @@ import edu.ku.brc.af.ui.forms.formatters.DataObjSwitchFormatter;
 import edu.ku.brc.af.ui.forms.formatters.UIFieldFormatterIFace;
 import edu.ku.brc.dbsupport.DataProviderFactory;
 import edu.ku.brc.dbsupport.DataProviderSessionIFace;
+import edu.ku.brc.util.Pair;
 
 /**
  * @author timbo
@@ -32,11 +36,32 @@ import edu.ku.brc.dbsupport.DataProviderSessionIFace;
  */
 public class ERTICaptionInfoRel extends ERTICaptionInfoQB
 {
+    protected static final Logger                            log              = Logger.getLogger(ERTICaptionInfoRel.class);
+
+    
     protected final DBRelationshipInfo relationship;
     /**
      * hql to retrieve list of related objects for one-to-many relationships.
      */
     protected final String listHql;
+    
+    /*
+     * lookupTbl and lookupSize implement a really quick and dirty cache for recently formatted many-to-one related records.
+     * If there is much repetition of related data, the cache can really speed up results display. And even when there are
+     * far more adds to the cache than finds, performance does not seem to worsen.
+     * 
+     * NOTE: This cache has no effect on the performance of one-to-many relationships which even slower than many-to-ones.
+     * 
+     * Still need to check this on some large, real datasets to accurately judge how effective it is.
+     */
+    protected LinkedList<Pair<Integer, Object>> lookupTbl = null;
+    protected static final int lookupSize = 20;
+    protected boolean useCache = true;
+    
+    //for debugging...
+    private int finds = 0; 
+    private int adds = 0;
+    private static boolean debugging = false;
     
     public ERTICaptionInfoRel(String  colName, 
                            String  colLabel, 
@@ -60,6 +85,10 @@ public class ERTICaptionInfoRel extends ERTICaptionInfoQB
         else
         {
             listHql = null;
+            if (useCache)
+            {
+                lookupTbl = new LinkedList<Pair<Integer, Object>>();
+            }
         }
     }
     
@@ -142,21 +171,46 @@ public class ERTICaptionInfoRel extends ERTICaptionInfoQB
         {
             return DataObjFieldFormatMgr.getInstance().aggregate(getList(key), relationship.getDataClass());
         }
-        Object value = lookupKey(key);
+        Object value = useCache ? lookupKey((Integer )key) : null;
         if (value == null)
         {
             value = DataObjFieldFormatMgr.getInstance().format(getObject(key), relationship.getDataClass());
-            addKey(key, value);
+            if (useCache && key != null)
+            {
+                addKey((Integer )key, value);
+            }
+            else if (!useCache)
+            {
+                logDaBug(getColLabel() + " -- " + ++adds + ": processed " + key);
+            }
         }
         return value;
     }
     
+    private void logDaBug(Object bug)
+    {
+        if (debugging)
+        {
+            log.debug(bug);
+        }
+    }
     /**
      * @param key
      * @return
      */
-    protected Object lookupKey(final Object key)
+    protected Object lookupKey(final Integer key)
     {
+        if (key != null)
+        {
+            for (Pair<Integer, Object> entry : lookupTbl)
+            {
+                if (entry.getFirst().equals(key))
+                {
+                    logDaBug(this.getColLabel() + " -- " + ++finds + ": Found: " + key);
+                    return entry.getSecond();
+                }
+            }
+        }
         return null;
     }
 
@@ -164,10 +218,16 @@ public class ERTICaptionInfoRel extends ERTICaptionInfoQB
      * @param key
      * @param value
      */
-    protected void addKey(final Object key, final Object value)
+    protected void addKey(final Integer key, final Object value)
     {
-        // nuthin yet.
+        if (lookupTbl.size() == lookupSize)
+        {
+            lookupTbl.remove();
+        }
+        logDaBug(this.getColLabel() + " -- " + ++adds + ": added: " + key);
+        lookupTbl.add(new Pair<Integer, Object>(key, value));
     }
+    
     /**
      * @param value
      * @return List of related of objects if value is a key, or value cast to a collection if it is
