0776e4a575bd64151e31684bcdbd460994a367ed
Rod Spears <rods@ku.edu>
2008-09-22 14:16:56 +0000

    Bug 5587 Now uses straight SQL to get the CO Ids for related items, it is a lot faster.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@4964 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/TaxonTreeTask.java
+++ src/edu/ku/brc/specify/tasks/TaxonTreeTask.java
@@ -11,16 +11,19 @@ import static edu.ku.brc.ui.UIRegistry.getResourceString;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.persistence.Transient;
 import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 import edu.ku.brc.af.core.AppContextMgr;
-import edu.ku.brc.af.core.db.DBTableIdMgr;
-import edu.ku.brc.specify.datamodel.Collection;
+import edu.ku.brc.af.core.expresssearch.QueryAdjusterForDomain;
+import edu.ku.brc.dbsupport.DBConnection;
 import edu.ku.brc.specify.datamodel.CollectionObject;
-import edu.ku.brc.specify.datamodel.Determination;
 import edu.ku.brc.specify.datamodel.Discipline;
 import edu.ku.brc.specify.datamodel.RecordSet;
 import edu.ku.brc.specify.datamodel.Taxon;
@@ -67,6 +70,62 @@ public class TaxonTreeTask extends BaseTreeTask<Taxon,TaxonTreeDef,TaxonTreeDefI
     {
         return AppContextMgr.getInstance().getClassObject(Discipline.class).getTaxonTreeDef();
     }
+    
+    /**
+     * Get all the CollectionObjectIDs that that use this Taxon as the Current determination and put
+     * them into the RecordSet.
+     * @param taxon the Taxon
+     * @param recordSet the RecordSet to be filled.
+     */
+    protected void fillRecordSet(final Taxon taxon, final RecordSet recordSet)
+    {
+        // The old way using Hibernate relationships was too slow, 
+        // so instead I am using straight SQL it is a lot faster.
+        String sql = "SELECT co.CollectionObjectID FROM taxon as tx INNER JOIN determination as dt ON tx.TaxonID = dt.TaxonID " +
+                     "INNER JOIN determinationstatus as ds ON dt.DeterminationStatusID = ds.DeterminationStatusID " +
+                     "INNER JOIN collectionobject as co ON dt.CollectionObjectID = co.CollectionObjectID " +
+                     "WHERE tx.TaxonID = %d AND co.CollectionMemberID = COLMEMID AND ds.Type = 1";
+        sql = QueryAdjusterForDomain.getInstance().adjustSQL(String.format(sql, taxon.getTaxonId()));
+        
+        Connection conn = null;        
+        Statement  stmt = null;
+        try
+        {
+            conn = DBConnection.getInstance().createConnection();
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next())
+            {
+                recordSet.addItem(rs.getInt(1));
+            }
+            rs.close();
+
+        } 
+        catch (SQLException ex)
+        {
+            log.error("SQLException: " + ex.toString()); //$NON-NLS-1$
+            log.error(ex.getMessage());
+            
+        } finally
+        {
+            try 
+            {
+                if (stmt != null)
+                {
+                    stmt.close();
+                }
+                if (conn != null)
+                {
+                    conn.close();
+                }
+                
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
 
     /* (non-Javadoc)
      * @see edu.ku.brc.specify.tasks.BaseTreeTask#createTreeViewer(boolean)
@@ -85,7 +144,7 @@ public class TaxonTreeTask extends BaseTreeTask<Taxon,TaxonTreeDef,TaxonTreeDefI
             {
                 public void actionPerformed(ActionEvent e)
                 {
-                    StringBuilder itisURL = new StringBuilder("http://www.cbif.gc.ca/pls/itisca/taxastep?p_action=containing&p_format=html&taxa=");
+                    StringBuilder itisURL = new StringBuilder("http://www.cbif.gc.ca/pls/itisca/taxastep?p_action=containing&p_format=html&taxa="); // XXX Externalize URL
                     Taxon taxon = ttv.getSelectedNode(popup.getList());
                     String kingdom = taxon.getLevelName(TaxonTreeDef.KINGDOM);
                     String fullName = taxon.getFullName();
@@ -124,28 +183,18 @@ public class TaxonTreeTask extends BaseTreeTask<Taxon,TaxonTreeDef,TaxonTreeDefI
                         return;
                     }
 
-                    int collObjTableID = DBTableIdMgr.getInstance().getIdByClassName(CollectionObject.class.getName());
-                    RecordSet recordSet = new RecordSet();
+                    final RecordSet recordSet = new RecordSet();
                     recordSet.initialize();
-                    recordSet.set(UIRegistry.getResourceString("TTV.showCollectionObjects"), collObjTableID, RecordSet.GLOBAL);
+                    recordSet.set(UIRegistry.getResourceString("TTV.showCollectionObjects"), CollectionObject.getClassTableId(), RecordSet.GLOBAL);
 
-                    Collection collection = AppContextMgr.getInstance().getClassObject(Collection.class);
-                    Integer    colMemId   = collection != null ? collection.getCollectionId() : null;
-                    for(Determination deter : taxon.getDeterminations())
-                    {
-                        if (colMemId == null || deter.getCollectionMemberId().equals(colMemId))
-                        {
-                            recordSet.addItem(deter.getCollectionObject().getId());
-                        }
-                    }
+                    fillRecordSet(taxon, recordSet);
 
-                    final RecordSet rs = recordSet;
                     UIRegistry.getStatusBar().setText(getResourceString("TTV_OPENING_CO_FORM"));
                     // This is needed so the StatusBar gets updated
                     SwingUtilities.invokeLater(new Runnable() {
                         public void run()
                         {
-                            CommandDispatcher.dispatch(new CommandAction(DataEntryTask.DATA_ENTRY, DataEntryTask.EDIT_DATA, rs));
+                            CommandDispatcher.dispatch(new CommandAction(DataEntryTask.DATA_ENTRY, DataEntryTask.EDIT_DATA, recordSet));
                         }
                     });
                 }
