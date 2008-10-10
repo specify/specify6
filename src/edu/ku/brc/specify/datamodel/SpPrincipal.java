f36beeb02f2d52f424e73f2a228c2d487f8ddca5
Rod Spears <rods@ku.edu>
2008-10-10 21:13:14 +0000

    Minor - fixing imports
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5153 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/SpPrincipal.java
+++ src/edu/ku/brc/specify/datamodel/SpPrincipal.java
@@ -42,7 +42,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
