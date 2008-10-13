5da9efe60bcd3dd6dfa9f950e2cfb4e12f14e76a
Rod Spears <rods@ku.edu>
2008-10-13 13:41:52 +0000

    updates
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5163 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/toycode/BugParse.java
+++ src/edu/ku/brc/specify/toycode/BugParse.java
@@ -41,84 +41,114 @@ public class BugParse
     protected Hashtable<Integer, Hashtable<Integer, Integer>> hashRes = new Hashtable<Integer, Hashtable<Integer,Integer>>();
     protected Hashtable<Integer, Hashtable<Integer, Integer>> hashDif = new Hashtable<Integer, Hashtable<Integer,Integer>>();
     
+    protected Hashtable<Integer, Bug> bugHash = new Hashtable<Integer, Bug>();
+    
     public BugParse()
     {
-        process("20060901.html", hashNew);
-        process("20070901.html", hashNew);
-        process("20080301.html", hashNew);
-        process("20081001.html", hashNew);
+        parse("20060901_.txt");
+        parse("20070901_.txt");
+        parse("20080301_.txt");
+        parse("20080701_.txt");
+        parse("Now_.txt");
         
-        StringBuilder yearStr = new StringBuilder();
-        StringBuilder dataStr = new StringBuilder();
-        
-        Vector<Integer> keys = new Vector<Integer>(hashNew.keySet());
+        String eng = null;//"rods";
+        boolean doPriority = false;
+        int cnt = 0;
+        Vector<Integer> keys = new Vector<Integer>(bugHash.keySet());
         Collections.sort(keys);
-        
-        for (Integer year : keys)
+        for (Bug bug : bugHash.values())
         {
-            Hashtable<Integer, Integer> yearHash = hashNew.get(year);
-            
-            Vector<Integer> mKeys = new Vector<Integer>(yearHash.keySet());
-            Collections.sort(mKeys);
-            for (Integer mon : mKeys)
+            if ((eng == null || bug.getEngineer().startsWith(eng)) && (!doPriority || bug.getPriority().equals("P1") || bug.getPriority().equals("P1")))
             {
-                if (yearStr.length() > 0) yearStr.append(',');
-                yearStr.append(mon+"/"+Integer.toString(year-2000));
-                if (dataStr.length() > 0) dataStr.append(',');
-                dataStr.append(yearHash.get(mon));
-
-                System.out.println(year+"  "+mon+"  "+yearHash.get(mon));
+                process(hashNew, bug.getOpenYear(), bug.getOpenMon());
+                if (bug.isClosed)
+                {
+                    process(hashRes, bug.getModYear(), bug.getModMon());
+                }
+                System.out.println(bug.getNumber()+"  "+bug.getOpenDate()+"  "+bug.getModDate()+"  "+bug.isClosed());
+                cnt++;
             }
         }
         
-        System.out.println(yearStr.toString());
-        System.out.println(dataStr.toString());
+        System.err.println(cnt);
+        StringBuilder yearStr = new StringBuilder();
+        StringBuilder dataStr = new StringBuilder();
+
+        for (int year=2005;year<2009;year++)
+        {
+            int start = year == 2005 ? 7 : 1;
+            int end   = year == 2008 ? 11 : 13;
+            
+            Hashtable<Integer, Integer> yearHashNew = hashNew.get(year);
+            for (int mon=start;mon<end;mon++)
+            {
+                Integer monNew = yearHashNew != null ? yearHashNew.get(mon) : null;
+                if (monNew == null)
+                {
+                    monNew = 0;
+                }
+                if (yearStr.length() > 0) yearStr.append(',');
+                yearStr.append(mon+"/"+Integer.toString(year-2000));
+                
+                if (dataStr.length() > 0) dataStr.append(',');
+                dataStr.append(monNew);
+            }
+        }
         
         List<String> dataLines = new Vector<String>();
         dataLines.add(yearStr.toString());
         dataLines.add(dataStr.toString());
         
-        process("20060901Fix.html", hashRes);
-        process("20070901Fix.html", hashRes);
-        process("20080301Fix.html", hashRes);
-        process("20081001Fix.html", hashRes);
+        System.out.println(yearStr.toString());
+        System.out.println(dataStr.toString());
         
         yearStr = new StringBuilder();
         dataStr = new StringBuilder();
         StringBuilder dataStrDif = new StringBuilder();
         int diffTotal = 0;
         
-        keys = new Vector<Integer>(hashRes.keySet());
-        Collections.sort(keys);
-        for (Integer year : keys)
+        for (int year=2005;year<2009;year++)
         {
-            Hashtable<Integer, Integer> yearHash    = hashRes.get(year);
-            Hashtable<Integer, Integer> yearHashNew = hashNew.get(year);
+            int start = year == 2005 ? 7 : 1;
+            int end   = year == 2008 ? 11 : 13;
             
-            Vector<Integer> mKeys = new Vector<Integer>(yearHash.keySet());
-            Collections.sort(mKeys);
-            for (Integer mon : mKeys)
+            Hashtable<Integer, Integer> yearHashNew = hashNew.get(year);
+            Hashtable<Integer, Integer> yearHashRes = hashRes.get(year);
+            
+            for (int mon=start;mon<end;mon++)
             {
+                Integer monNew = yearHashNew != null ? yearHashNew.get(mon) : null;
+                if (monNew == null)
+                {
+                    monNew = 0;
+                }
+                Integer monRes = yearHashRes != null ? yearHashRes.get(mon) : null;
+                if (monRes == null)
+                {
+                    monRes = 0;
+                }
+                
                 if (yearStr.length() > 0) yearStr.append(',');
                 yearStr.append(mon+"/"+Integer.toString(year-2000));
                 
                 if (dataStr.length() > 0) dataStr.append(',');
-                dataStr.append(yearHash.get(mon));
+                dataStr.append(monRes);
                 
-                diffTotal += yearHashNew.get(mon) - yearHash.get(mon);
+                diffTotal += monNew - monRes;
+                System.out.println("> "+diffTotal+"  "+year+"  "+mon+" "+monNew+"  "+monRes);
                 if (dataStrDif.length() > 0) dataStrDif.append(',');
                 dataStrDif.append(diffTotal);
                 
-                System.out.println(year+"  "+mon+"  "+yearHash.get(mon));
+                //System.out.println(year+"  "+mon+"  "+monNew);
             }
         }
         System.out.println(dataStr.toString());
+        System.out.println(dataStrDif.toString());
         
         dataLines.add(dataStr.toString());
         dataLines.add(dataStrDif.toString());
         
-        createChart(dataLines);
-
+        createChart(dataLines, eng == null ? "All" : eng);
     }
     
     /**
@@ -138,21 +168,47 @@ public class BugParse
         }
     }
     
-    protected void createChart(final List<String> lines)
+    /**
+     * @param lines
+     */
+    protected void createChart(final List<String> lines, final String engineer)
     {
+        
+        int[] mins = new int[lines.size()-1];
+        for (int index=1;index<lines.size();index++)
+        {
+            String line = lines.get(index);
+            String[] values =  StringUtils.splitPreserveAllTokens(line, ",");
+            int inx = 0;
+            while (inx < values.length && values[inx].equals("0"))
+            {
+                inx++;
+            }
+            mins[index-1] = inx < values.length ? inx : Integer.MAX_VALUE;
+            System.err.println(mins[index-1]);
+        }
+        
+        int startInx = Integer.MAX_VALUE;
+        for (int min : mins)
+        {
+            startInx = Math.min(startInx, min);
+            System.out.println(min+"  "+startInx);
+        }
+        
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         
         String[] headers = StringUtils.split(lines.get(0), ",");
-        System.out.println(headers.length);
+        int len = headers.length - startInx;
+        System.out.println(headers.length+"  "+len);
         List<double[]> valArray = new Vector<double[]>();
         
         for (int i=1;i<lines.size();i++)
         {
             String[] values =  StringUtils.splitPreserveAllTokens(lines.get(i), ",");
-            double[] vals   = new double[headers.length];
+            double[] vals   = new double[len];
             int inx = 0;
             double prev = -1;
-            for (int j=0;j<headers.length;j++)
+            for (int j=startInx;j<headers.length;j++)
             {
                 if (StringUtils.isNotEmpty(values[j]))
                 {
@@ -161,13 +217,6 @@ public class BugParse
                 } else
                 {
                     vals[inx++] = 0.0;
-                }
-            }
-            for (int j=0;j<vals.length-1;j++)
-            {
-                if (vals[j] == 0.0 && vals[j+1] != 0.0 && vals[j-1] != 0.0)
-                {
-                    vals[j] = ((vals[j+1] - vals[j-1]) / 2.0) + vals[j-1];
                 }
             }
             valArray.add(vals);
@@ -184,14 +233,13 @@ public class BugParse
             dataset.addValue(vals[i], "Resolved", headers[i]);
         }
         
-        /*vals = valArray.get(2);
+        vals = valArray.get(2);
         for (int i=0;i<vals.length;i++)
         {
             dataset.addValue(vals[i], "Open", headers[i]);
-        }*/
+        }
         
-        JFreeChart chart;
-            chart = ChartFactory.createLineChart("Bugs", "Time", "Bugs", 
+        JFreeChart chart = ChartFactory.createLineChart("Bugs - "+engineer, "Time", "Bugs", 
                     dataset, PlotOrientation.VERTICAL, 
                     true, true, false);
         final CategoryPlot plot = (CategoryPlot) chart.getPlot();
@@ -214,51 +262,58 @@ public class BugParse
         frame.setVisible(true);
     }
     
+    /**
+     * @param hash
+     * @param year
+     * @param mon
+     */
     @SuppressWarnings("unchecked")
-    protected void process(final String fileName, final Hashtable<Integer, Hashtable<Integer, Integer>> hash)
+    protected void process(final Hashtable<Integer, Hashtable<Integer, Integer>> hash, final int year, final int mon)
+    {
+        Hashtable<Integer, Integer> yearHash = hash.get(year);
+        if (yearHash == null)
+        {
+            yearHash = new Hashtable<Integer, Integer>();
+            hash.put(year, yearHash);
+        }
+        
+        Hashtable<Integer, Integer> yearHashDif = hashDif.get(year);
+        if (yearHashDif == null)
+        {
+            yearHashDif = new Hashtable<Integer, Integer>();
+            hashDif.put(year, yearHashDif);
+        }
+        
+        Integer monTotal = yearHash.get(mon);
+        int tot = monTotal == null ? 0 : monTotal;
+        tot++;
+        yearHash.put(mon, tot);
+        yearHashDif.put(mon, 0);
+    }
+    
+    /**
+     * @param fileName
+     */
+    @SuppressWarnings("unchecked")
+    protected void parse(final String fileName)
     {
         try
         {
             List<String> lines = FileUtils.readLines(new File("/Users/rod/Downloads/"+fileName));
-            for (String line : lines)
+            for (int i=0;i<lines.size();i++)
             {
-                int sInx = line.indexOf("<td><nobr>");
-                if (sInx > -1)
+                String line = lines.get(i);
+                if (StringUtils.contains(line, "show_bug.cgi?id"))
                 {
-                    sInx += 10;
-                    int eInx = line.indexOf("</nobr>", sInx);
-                    if (eInx > -1)
-                    {
-                        String token = line.substring(sInx, eInx);
-                        if (token.length() == 10 && token.charAt(4) == '-' && token.charAt(7) == '-')
-                        {
-                            int year = Integer.parseInt(token.substring(0, 4));
-                            int mon  = Integer.parseInt(token.substring(5, 7));
-                            
-                            Hashtable<Integer, Integer> yearHash = hash.get(year);
-                            if (yearHash == null)
-                            {
-                                yearHash = new Hashtable<Integer, Integer>();
-                                hash.put(year, yearHash);
-                            }
-                            
-                            Hashtable<Integer, Integer> yearHashDif = hashDif.get(year);
-                            if (yearHashDif == null)
-                            {
-                                yearHashDif = new Hashtable<Integer, Integer>();
-                                hashDif.put(year, yearHashDif);
-                            }
-                            
-                            Integer monTotal = yearHash.get(mon);
-                            if (monTotal == null)
-                            {
-                                monTotal = 0;
-                            }
-                            monTotal++;
-                            yearHash.put(mon, monTotal);
-                            yearHashDif.put(mon, 0);
-                        }
-                    }
+                    String line2 = lines.get(++i);
+                    
+                    String[] tokens1 = StringUtils.split(line);
+                    String[] tokens2 = StringUtils.split(line2);
+                    
+                    int number = Integer.parseInt(tokens1[0]);
+                    Bug bug = new Bug(number, tokens1[2], tokens1[3], tokens2[0], tokens1[4], tokens1[5]);
+                    bug.setClosed(!(tokens2[1].equals("ASSI") || tokens2[1].equals("NEW")));
+                    bugHash.put(number, bug);
                 }
             }
             
@@ -268,12 +323,127 @@ public class BugParse
         }
     }
 
+
     /**
      * @param args
      */
     public static void main(String[] args)
     {
         new BugParse();
+    }
+    
+    class Bug 
+    {
+        protected int    number;
+        protected String openDate;
+        protected String modDate;
+        protected String engineer;
+        protected String severity;
+        protected String priority;
+        protected boolean isClosed;
+        
+        /**
+         * @param openDate
+         * @param modDate
+         * @param engineer
+         * @param severity
+         */
+        public Bug(int number, String openDate, String modDate, String engineer, String severity, String priority)
+        {
+            super();
+            this.number = number;
+            this.openDate = openDate;
+            this.modDate = modDate;
+            this.engineer = engineer;
+            this.severity = severity;
+            this.priority = priority;
+        }
+        
+        protected int getOpenYear()
+        {
+            //System.out.println("["+openDate+"]");
+            if (openDate.length() != 10) openDate = "2008-10-10";
+            return Integer.parseInt(openDate.substring(0, 4));
+        }
+        
+        protected int getOpenMon()
+        {
+            return Integer.parseInt(openDate.substring(5, 7));
+        }
+        
+        protected int getModYear()
+        {
+            if (modDate.length() != 10) modDate = "2008-10-10";
+            return Integer.parseInt(modDate.substring(0, 4));
+        }
+        
+        protected int getModMon()
+        {
+            return Integer.parseInt(modDate.substring(5, 7));
+        }
+        
+        /**
+         * @return the number
+         */
+        public int getNumber()
+        {
+            return number;
+        }
+
+        /**
+         * @return the openDate
+         */
+        public String getOpenDate()
+        {
+            return openDate;
+        }
+        /**
+         * @return the modDate
+         */
+        public String getModDate()
+        {
+            return modDate;
+        }
+        /**
+         * @return the engineer
+         */
+        public String getEngineer()
+        {
+            return engineer;
+        }
+        /**
+         * @return the severity
+         */
+        public String getSeverity()
+        {
+            return severity;
+        }
+
+        /**
+         * @return the isClosed
+         */
+        public boolean isClosed()
+        {
+            return isClosed;
+        }
+
+        /**
+         * @param isClosed the isClosed to set
+         */
+        public void setClosed(boolean isClosed)
+        {
+            this.isClosed = isClosed;
+        }
+
+        /**
+         * @return the priority
+         */
+        public String getPriority()
+        {
+            return priority;
+        }
+        
+        
     }
 
 }
