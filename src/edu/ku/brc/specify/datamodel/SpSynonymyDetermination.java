eaab041cc555afbf7ec738fa14a352e0f12fffdb
Tim Noble <timo@ku.edu>
2008-10-23 15:53:46 +0000

    new tables for taxon synonymization
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5223 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/datamodel/SpSynonymyDetermination.java
@@ -0,0 +1,182 @@
+/*
+     * Copyright (C) 2008  The University of Kansas
+     *
+     * [INSERT KU-APPROVED LICENSE TEXT HERE]
+     *
+     */
+/**
+ * 
+ */
+package edu.ku.brc.specify.datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ * @author timbo
+ *
+ * @code_status Alpha
+ *
+ */
+@Entity
+@org.hibernate.annotations.Entity(dynamicInsert=true, dynamicUpdate=true)
+@org.hibernate.annotations.Proxy(lazy = false)
+@Table(name = "spsynonymydetermination")
+public class SpSynonymyDetermination extends DataModelObjBase
+{
+    protected Integer         spSynonymyDeterminationId;
+    protected SpTaxonSynonymy taxonSynonymy;
+    protected Determination   oldDetermination;
+    protected Determination   newDetermination;
+    
+    /**
+     * 
+     */
+    public SpSynonymyDetermination()
+    {
+        //nothing.
+    }
+    
+    
+    
+    /**
+     * @return the spSynonymyDeterminationId
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "SpSynonymyDeterminationID", unique = false, nullable = false, insertable = true, updatable = true)
+    public Integer getSpSynonymyDeterminationId()
+    {
+        return spSynonymyDeterminationId;
+    }
+
+
+
+    /**
+     * @param spSynonymyDeterminationId the spSynonymyDeterminationId to set
+     */
+    public void setSpSynonymyDeterminationId(Integer spSynonymyDeterminationId)
+    {
+        this.spSynonymyDeterminationId = spSynonymyDeterminationId;
+    }
+
+
+
+    /**
+     * @return the taxonSynonymy
+     */
+     @ManyToOne
+     @JoinColumn(name = "TaxonSynonymyId", unique = false, nullable = false, insertable = true, updatable = true)
+     public SpTaxonSynonymy getTaxonSynonymy()
+    {
+        return taxonSynonymy;
+    }
+
+
+
+    /**
+     * @param taxonSynonymy the taxonSynonymy to set
+     */
+    public void setTaxonSynonymy(SpTaxonSynonymy taxonSynonymy)
+    {
+        this.taxonSynonymy = taxonSynonymy;
+    }
+
+
+
+    /**
+     * @return the oldDetermination
+     */
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "OldDeterminationID", unique = false, nullable = true, insertable = true, updatable = true)
+    public Determination getOldDetermination()
+    {
+        return oldDetermination;
+    }
+
+
+
+    /**
+     * @param oldDetermination the oldDetermination to set
+     */
+    public void setOldDetermination(Determination oldDetermination)
+    {
+        this.oldDetermination = oldDetermination;
+    }
+
+
+
+    /**
+     * @return the newDetermination
+     */
+    @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "NewDeterminationID", unique = false, nullable = true, insertable = true, updatable = true)
+    public Determination getNewDetermination()
+    {
+        return newDetermination;
+    }
+
+
+
+    /**
+     * @param newDetermination the newDetermination to set
+     */
+    public void setNewDetermination(Determination newDetermination)
+    {
+        this.newDetermination = newDetermination;
+    }
+
+
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getDataClass()
+     */
+    @Override
+    @Transient
+    public Class<?> getDataClass()
+    {
+        return SpSynonymyDetermination.class;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getId()
+     */
+    @Override
+    @Transient
+    public Integer getId()
+    {
+        return spSynonymyDeterminationId;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getTableId()
+     */
+    @Override
+    @Transient
+    public int getTableId()
+    {
+        return 528;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#initialize()
+     */
+    @Override
+    public void initialize()
+    {
+        super.init();
+        
+        spSynonymyDeterminationId = null;
+        taxonSynonymy = null;
+        oldDetermination = null;
+        newDetermination = null;
+    }
+
+}
---
+++ src/edu/ku/brc/specify/datamodel/SpTaxonSynonymy.java
@@ -0,0 +1,246 @@
+/*
+     * Copyright (C) 2008  The University of Kansas
+     *
+     * [INSERT KU-APPROVED LICENSE TEXT HERE]
+     *
+     */
+/**
+ * 
+ */
+package edu.ku.brc.specify.datamodel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+/**
+ * @author timbo
+ *
+ * @code_status Alpha
+ *
+ */
+@Entity
+@org.hibernate.annotations.Entity(dynamicInsert=true, dynamicUpdate=true)
+@org.hibernate.annotations.Proxy(lazy = false)
+@Table(name = "sptaxonsynonymy")
+//@org.hibernate.annotations.Table(appliesTo="sptaxonsynonymy", indexes =
+//    {   @Index (name="SpSynonymyAccepted", columnNames={"AcceptedId"}),
+//        @Index (name="SpSynonymyNotAccepted", columnNames={"NotAcceptedId"})
+//    })
+public class SpTaxonSynonymy extends DataModelObjBase
+{
+
+    protected Integer                       spTaxonSynonymyId;
+    protected TaxonTreeDef                  taxonTreeDef;
+    protected Taxon                         accepted;
+    protected Taxon                         notAccepted;
+    protected Agent                         synonymizer;
+    protected Set<SpSynonymyDetermination> determinations;
+    
+    
+    /**
+     * 
+     */
+    public SpTaxonSynonymy()
+    {
+        //nothing
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#initialize()
+     */
+    @Override
+    public void initialize()
+    {
+        super.init();
+        
+        spTaxonSynonymyId = null;
+        taxonTreeDef = null;
+        accepted = null;
+        notAccepted = null;
+        synonymizer = null;
+        determinations = new HashSet<SpSynonymyDetermination>();
+    }
+
+    /**
+     * @return the spSynonymyId
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "SpTaxonSynonymyID", unique = false, nullable = false, insertable = true, updatable = true)
+    public Integer getSpTaxonSynonymyId()
+    {
+        return spTaxonSynonymyId;
+    }
+
+
+    
+    /**
+     * @param spTaxonSynonymyId the spTaxonSynonymyId to set
+     */
+    public void setSpTaxonSynonymyId(Integer spTaxonSynonymyId)
+    {
+        this.spTaxonSynonymyId = spTaxonSynonymyId;
+    }
+
+
+    /**
+     * @return the taxonTreeDef
+     */
+    @ManyToOne
+    @JoinColumn(name = "TaxonTreeDefID", nullable = false)
+    public TaxonTreeDef getTaxonTreeDef()
+    {
+        return taxonTreeDef;
+    }
+
+
+    /**
+     * @param taxonTreeDef the taxonTreeDef to set
+     */
+    public void setTaxonTreeDef(TaxonTreeDef taxonTreeDef)
+    {
+        this.taxonTreeDef = taxonTreeDef;
+    }
+
+
+    /**
+     * @return the accepted
+     */
+    @ManyToOne
+    @JoinColumn(name = "AcceptedID")
+    public Taxon getAccepted()
+    {
+        return accepted;
+    }
+
+
+    /**
+     * @param accepted the accepted to set
+     */
+    public void setAccepted(Taxon accepted)
+    {
+        this.accepted = accepted;
+    }
+
+
+    /**
+     * @return the notAccepted
+     */
+    @ManyToOne
+    @JoinColumn(name = "NotAcceptedID")
+    public Taxon getNotAccepted()
+    {
+        return notAccepted;
+    }
+
+
+    /**
+     * @param notAccepted the notAccepted to set
+     */
+    public void setNotAccepted(Taxon notAccepted)
+    {
+        this.notAccepted = notAccepted;
+    }
+
+
+    /**
+     * @return the synonymizer
+     */
+    @ManyToOne
+    @JoinColumn(name = "SynonymizerID")
+    public Agent getSynonymizer()
+    {
+        return synonymizer;
+    }
+
+
+    /**
+     * @param synonymizer the synonymizer to set
+     */
+    public void setSynonymizer(Agent synonymizer)
+    {
+        this.synonymizer = synonymizer;
+    }
+
+
+    /**
+     * @return the determinations
+     */
+    @OneToMany(mappedBy = "taxonSynonymy")
+    @org.hibernate.annotations.Cascade( { org.hibernate.annotations.CascadeType.ALL, org.hibernate.annotations.CascadeType.DELETE_ORPHAN })
+    public Set<SpSynonymyDetermination> getDeterminations()
+    {
+        return determinations;
+    }
+
+    public void removeDetermination(final SpSynonymyDetermination toRemove)
+    {
+        SpSynonymyDetermination toRemoveObj = null;
+        for (SpSynonymyDetermination det: determinations)
+        {
+            if (det.getId().equals(toRemove.getId()))
+            {
+                toRemoveObj = det;
+                break;
+            }
+        }
+        if (toRemoveObj != null)
+        {
+            determinations.remove(toRemoveObj);
+        }
+    }
+
+    /**
+     * @param determinations the determinations to set
+     */
+    public void setDeterminations(Set<SpSynonymyDetermination> determinations)
+    {
+        this.determinations = determinations;
+    }
+
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getDataClass()
+     */
+    @Override
+    @Transient
+    public Class<?> getDataClass()
+    {
+        return SpTaxonSynonymy.class;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getId()
+     */
+    @Override
+    @Transient
+    public Integer getId()
+    {
+        return spTaxonSynonymyId;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.DataModelObjBase#getTableId()
+     */
+    @Override
+    @Transient
+    public int getTableId()
+    {
+        return 527;
+    }
+ 
+}
