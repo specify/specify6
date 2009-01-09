0b1a9250457d5f87cee350cf4137b4b8a8abbdf8
Rod Spears <rods@ku.edu>
2009-01-09 20:01:44 +0000

    Bug 6305 need to manually set the validator
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5570 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/prefs/MySQLPrefs.java
+++ src/edu/ku/brc/specify/prefs/MySQLPrefs.java
@@ -46,9 +46,11 @@ import edu.ku.brc.af.prefs.PrefsPanelMgrIFace;
 import edu.ku.brc.af.prefs.PrefsSavable;
 import edu.ku.brc.af.tasks.BackupTask;
 import edu.ku.brc.af.ui.forms.validation.FormValidator;
+import edu.ku.brc.af.ui.forms.validation.UIValidator;
 import edu.ku.brc.af.ui.forms.validation.ValBrowseBtnPanel;
 import edu.ku.brc.af.ui.forms.validation.ValTextField;
 import edu.ku.brc.specify.datamodel.Institution;
+import edu.ku.brc.ui.DocumentAdaptor;
 import edu.ku.brc.ui.UIRegistry;
 
 /**
@@ -69,15 +71,15 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     private final String MYSQLDUMP_LOC = "mysqldump.location";
     private final String MYSQLBCK_LOC  = "backup.location";
     
-    private ValBrowseBtnPanel mysqlLocBP     = new ValBrowseBtnPanel(new ValTextField(), true, true);
-    private ValBrowseBtnPanel mysqlDumpLocBP = new ValBrowseBtnPanel(new ValTextField(), true, true);
+    private ValBrowseBtnPanel mysqlLocBP     = new ValBrowseBtnPanel(new ValTextField(), false, true);
+    private ValBrowseBtnPanel mysqlDumpLocBP = new ValBrowseBtnPanel(new ValTextField(), false, true);
     private ValBrowseBtnPanel backupLocBP    = new ValBrowseBtnPanel(new ValTextField(), true, true);
     
     private JButton           backupBtn;
     private JButton           restoreBtn;
     private boolean           doShowRestore;
 
-    private FormValidator     validator     = new FormValidator(null);
+    private FormValidator        validator     = new FormValidator(null);
     protected PrefsPanelMgrIFace mgr = null;
     protected String             name;
     protected String             title;
@@ -93,6 +95,12 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     public MySQLPrefs()
     {
         this(false);
+        
+        validator.setName("MySQL Validator");
+        validator.setNewObj(true);
+        validator.hookupTextField(mysqlLocBP.getTextField(),     "1", false, UIValidator.Type.Changed, null, true);
+        validator.hookupTextField(mysqlDumpLocBP.getTextField(), "2", false, UIValidator.Type.Changed, null, true);
+        validator.hookupTextField(backupLocBP.getTextField(),    "3", false, UIValidator.Type.Changed, null, true);
     }
     
     /**
@@ -147,13 +155,13 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
         pb.addSeparator(UIRegistry.getResourceString("BCKRSTR_OPTIONS"), cc.xyw(1, 1, 5));
         
         pb.add(createI18NFormLabel("MYS_DUMP_PATH"), cc.xy(1, 3));
-        pb.add(mysqlDumpLocBP,                   cc.xyw(3, 3, 3));
+        pb.add(mysqlDumpLocBP,                       cc.xyw(3, 3, 3));
         
         pb.add(createI18NFormLabel("MYS_RESTR_PATH"), cc.xy(1, 5));
-        pb.add(mysqlLocBP,                        cc.xyw(3, 5, 3));
+        pb.add(mysqlLocBP,                            cc.xyw(3, 5, 3));
         
         pb.add(createI18NFormLabel("MYS_BCK_PATH"), cc.xy(1, 7));
-        pb.add(backupLocBP,                     cc.xyw(3, 7, 3));
+        pb.add(backupLocBP,                         cc.xyw(3, 7, 3));
         
         pb.add(btnPB.getPanel(), cc.xyw(1, 9, 5));
         
@@ -177,19 +185,16 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
         mysqlDumpLocBP.setValue(mysqlDumpLoc, mysqlDumpLoc);
         backupLocBP.setValue(backupLoc, backupLoc);
         
-        DocumentListener dl = new DocumentListener()
+        DocumentListener dl = new DocumentAdaptor()
         {
-            protected void changed()
+            @Override
+            protected void changed(final DocumentEvent e)
             {
                 validator.setHasChanged(true);
+                validator.validateRoot();
+                //form.getUIComponent().validate();
                 updateEnableUI();
             }
-            @Override
-            public void changedUpdate(DocumentEvent e) { changed(); }
-            @Override
-            public void insertUpdate(DocumentEvent e) { changed(); }
-            @Override
-            public void removeUpdate(DocumentEvent e) { changed(); }
         };
         mysqlLocBP.getTextField().getDocument().addDocumentListener(dl);
         mysqlDumpLocBP.getTextField().getDocument().addDocumentListener(dl);
@@ -286,7 +291,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#getChangedFields(java.util.Properties)
      */
     @Override
-    public void getChangedFields(Properties changeHash)
+    public void getChangedFields(final Properties changeHash)
     {
         
     }
@@ -305,7 +310,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     /* (non-Javadoc)
      * @see java.awt.Component#setName(java.lang.String)
      */
-    public void setName(String name)
+    public void setName(final String name)
     {
         this.name = name;
     }
@@ -321,7 +326,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     /* (non-Javadoc)
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#setPermissions(edu.ku.brc.af.core.PermissionIFace)
      */
-    public void setPermissions(PermissionIFace permissions)
+    public void setPermissions(final PermissionIFace permissions)
     {
         this.permissions = permissions;
     }
@@ -330,7 +335,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#setPrefsPanelMgr(edu.ku.brc.af.prefs.PrefsPanelMgrIFace)
      */
     @Override
-    public void setPrefsPanelMgr(PrefsPanelMgrIFace mgrArg)
+    public void setPrefsPanelMgr(final PrefsPanelMgrIFace mgrArg)
     {
         this.mgr = mgrArg;
     }
@@ -346,7 +351,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     /* (non-Javadoc)
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#setHelpContext(java.lang.String)
      */
-    public void setHelpContext(String context)
+    public void setHelpContext(final String context)
     {
         hContext = context;
     }
@@ -355,7 +360,7 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#setShadeColor(java.awt.Color)
      */
     @Override
-    public void setShadeColor(Color color)
+    public void setShadeColor(final Color color)
     {
         shadeColor = color;
     }
@@ -375,14 +380,16 @@ public class MySQLPrefs extends JPanel implements PrefsSavable, PrefsPanelIFace
     @Override
     public boolean isFormValid()
     {
-        return false;
+        return StringUtils.isNotEmpty(mysqlLocBP.getTextField().getText()) &&
+               StringUtils.isNotEmpty(mysqlDumpLocBP.getTextField().getText()) &&
+               StringUtils.isNotEmpty(backupLocBP.getTextField().getText());
     }
 
     /* (non-Javadoc)
      * @see edu.ku.brc.af.prefs.PrefsPanelIFace#setTitle(java.lang.String)
      */
     @Override
-    public void setTitle(String titleArg)
+    public void setTitle(final String titleArg)
     {
         this.title = titleArg;
     }
