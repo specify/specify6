29dc0aa4e41e4d8b6bddb98bbe16eea8213ae1c7
Ricardo Scachetti Pereira <scachett@gmail.com>
2009-01-19 23:06:28 +0000

    Replaced tabs with spaces.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5690 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/security/AdminInfoSubPanelWrapper.java
+++ src/edu/ku/brc/specify/tasks/subpane/security/AdminInfoSubPanelWrapper.java
@@ -38,126 +38,126 @@ import edu.ku.brc.ui.UIRegistry;
  */
 public class AdminInfoSubPanelWrapper
 {
-	private JPanel                      displayPanel;
-	private List<PermissionPanelEditor> permissionEditors; 
-	
-	private SpPrincipal                 principal           = null;
-	private SpPrincipal                 overrulingPrincipal = null;
+    private JPanel                      displayPanel;
+    private List<PermissionPanelEditor> permissionEditors; 
+    
+    private SpPrincipal                 principal           = null;
+    private SpPrincipal                 overrulingPrincipal = null;
 
-	/**
-	 * Constructor taking only a JPanel as parameter
-	 * 
-	 * @param displayPanel
-	 */
-	public AdminInfoSubPanelWrapper(final JPanel displayPanel)
-	{
-		this.displayPanel = displayPanel;
-		permissionEditors = new ArrayList<PermissionPanelEditor>();
-	}
+    /**
+     * Constructor taking only a JPanel as parameter
+     * 
+     * @param displayPanel
+     */
+    public AdminInfoSubPanelWrapper(final JPanel displayPanel)
+    {
+        this.displayPanel = displayPanel;
+        permissionEditors = new ArrayList<PermissionPanelEditor>();
+    }
 
-	/**
-	 * 
-	 */
-	public void clearPermissionEditors()
-	{
-		permissionEditors.clear();
-	}
-	
-	/**
-	 * @param permissionEditor
-	 */
-	public void addPermissionEditor(PermissionPanelEditor permissionEditor)
-	{
-		permissionEditors.add(permissionEditor);
-	}
-	
-	/**
-	 * @param permissionEditor
-	 */
-	public void removePermissionEditor(PermissionPanelEditor permissionEditor)
-	{
-		permissionEditors.remove(permissionEditor);
-	}
-	
-	/**
-	 * @return
-	 */
-	public JPanel getDisplayPanel()
-	{
-		return displayPanel;
-	}
-	
-	/**
+    /**
+     * 
+     */
+    public void clearPermissionEditors()
+    {
+        permissionEditors.clear();
+    }
+    
+    /**
+     * @param permissionEditor
+     */
+    public void addPermissionEditor(PermissionPanelEditor permissionEditor)
+    {
+        permissionEditors.add(permissionEditor);
+    }
+    
+    /**
+     * @param permissionEditor
+     */
+    public void removePermissionEditor(PermissionPanelEditor permissionEditor)
+    {
+        permissionEditors.remove(permissionEditor);
+    }
+    
+    /**
+     * @return
+     */
+    public JPanel getDisplayPanel()
+    {
+        return displayPanel;
+    }
+    
+    /**
      * @return the permissionEditors
      */
     public List<PermissionPanelEditor> getPermissionEditors()
     {
         return permissionEditors;
     }
-	
-	/**
-	 * Set form data based on a given persistent object
-	 * If first object is a SpecifyUser, secondObject is the group (GroupPrincipal) a user belongs to
-	 * @param dataObj
-	 * @param secondObject
-	 * @return whether new data was set (usually from setting defaults)
-	 */
-	public boolean setData(final Object dataObj, 
-	                       final Object secondObject)
-	{
-	    boolean hasChanged = false;
-		if (displayPanel instanceof ViewBasedDisplayPanel)
-		{
+    
+    /**
+     * Set form data based on a given persistent object
+     * If first object is a SpecifyUser, secondObject is the group (GroupPrincipal) a user belongs to
+     * @param dataObj
+     * @param secondObject
+     * @return whether new data was set (usually from setting defaults)
+     */
+    public boolean setData(final Object dataObj, 
+                           final Object secondObject)
+    {
+        boolean hasChanged = false;
+        if (displayPanel instanceof ViewBasedDisplayPanel)
+        {
             ViewBasedDisplayPanel panel = (ViewBasedDisplayPanel) displayPanel;
             panel.setData(null);
             panel.setData(dataObj);
-			
+            
             SpecifyUser user = null;
             
-			// set permissions table if appropriate according to principal (user or usergroup)
-			SpPrincipal firstPrincipal = null;
-			if (dataObj instanceof SpecifyUser)
-			{
-			    user      = (SpecifyUser) dataObj;
-				firstPrincipal = UserPrincipalHibernateService.getUserPrincipalBySpecifyUser(user);
-				
-			} else if (dataObj instanceof SpPrincipal)
-			{
-				firstPrincipal = (SpPrincipal) dataObj;
-			}
+            // set permissions table if appropriate according to principal (user or usergroup)
+            SpPrincipal firstPrincipal = null;
+            if (dataObj instanceof SpecifyUser)
+            {
+                user      = (SpecifyUser) dataObj;
+                firstPrincipal = UserPrincipalHibernateService.getUserPrincipalBySpecifyUser(user);
+                
+            } else if (dataObj instanceof SpPrincipal)
+            {
+                firstPrincipal = (SpPrincipal) dataObj;
+            }
 
-			// get user principal
-			SpPrincipal secondPrincipal = null;
-			if (secondObject instanceof SpecifyUser)
-			{
-				user            = (SpecifyUser) secondObject;
-				secondPrincipal = UserPrincipalHibernateService.getUserPrincipalBySpecifyUser(user);
-			}
+            // get user principal
+            SpPrincipal secondPrincipal = null;
+            if (secondObject instanceof SpecifyUser)
+            {
+                user            = (SpecifyUser) secondObject;
+                secondPrincipal = UserPrincipalHibernateService.getUserPrincipalBySpecifyUser(user);
+            }
 
-			if (firstPrincipal != null && permissionEditors.size() > 0)
-			{
-			    String userType = null;
-	            DataProviderSessionIFace session = null;
-		        try
-		        {
-		            session  = DataProviderFactory.getInstance().createSession();
-		            session.attach(firstPrincipal);
-		            userType = firstPrincipal.getPermissions().size() == 0 ? (user != null ? user.getUserType() : null) : null;
-		            
-		        } catch (Exception ex)
-		        {
-		            edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
-		            edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(AdminInfoSubPanelWrapper.class, ex);
-		            ex.printStackTrace();
-		            session.rollback();
-		            
-		        } finally
-		        {
-		            if (session != null)
-		            {
-		                session.close();
-		            }
-		        }
+            if (firstPrincipal != null && permissionEditors.size() > 0)
+            {
+                String userType = null;
+                DataProviderSessionIFace session = null;
+                try
+                {
+                    session  = DataProviderFactory.getInstance().createSession();
+                    session.attach(firstPrincipal);
+                    userType = firstPrincipal.getPermissions().size() == 0 ? (user != null ? user.getUserType() : null) : null;
+                    
+                } catch (Exception ex)
+                {
+                    edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                    edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(AdminInfoSubPanelWrapper.class, ex);
+                    ex.printStackTrace();
+                    session.rollback();
+                    
+                } finally
+                {
+                    if (session != null)
+                    {
+                        session.close();
+                    }
+                }
                 
                 if (userType != null)
                 {
@@ -189,14 +189,14 @@ public class AdminInfoSubPanelWrapper
                 principal           = firstPrincipal;
                 overrulingPrincipal = secondPrincipal;
 
-				for (PermissionPanelEditor editor : permissionEditors)
-				{
-					editor.updateData(firstPrincipal, secondPrincipal, existingPerms, overrulingPerms, userType);
-				}
-			}
-		}
-		return hasChanged;
-	}
+                for (PermissionPanelEditor editor : permissionEditors)
+                {
+                    editor.updateData(firstPrincipal, secondPrincipal, existingPerms, overrulingPerms, userType);
+                }
+            }
+        }
+        return hasChanged;
+    }
 
     /**
      * @param session the current session
@@ -234,20 +234,20 @@ public class AdminInfoSubPanelWrapper
         }
         session.saveOrUpdate(principal);
     }
-	
-	/**
-	 * Returns the MultiView associated with a ViewBasedDisplayPanel, or just return null if
-	 * wrapped panel is just a regular JPanel
-	 * @return the forms MultiView
-	 */
-	public MultiView getMultiView()
-	{
-		if (displayPanel instanceof ViewBasedDisplayPanel)
-		{
-			ViewBasedDisplayPanel panel = (ViewBasedDisplayPanel) displayPanel;
-    		return panel.getMultiView();
-		}
-		// else
-		return null;
-	}
+    
+    /**
+     * Returns the MultiView associated with a ViewBasedDisplayPanel, or just return null if
+     * wrapped panel is just a regular JPanel
+     * @return the forms MultiView
+     */
+    public MultiView getMultiView()
+    {
+        if (displayPanel instanceof ViewBasedDisplayPanel)
+        {
+            ViewBasedDisplayPanel panel = (ViewBasedDisplayPanel) displayPanel;
+            return panel.getMultiView();
+        }
+        // else
+        return null;
+    }
 }
