510d007e30e3cd08f13242af51c077c1d2acd45d
Rod Spears <rods@ku.edu>
2009-01-23 03:54:53 +0000

    Misspelling of help token
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@5762 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/af/core/expresssearch/ExpressSearchConfigDlg.java
+++ src/edu/ku/brc/af/core/expresssearch/ExpressSearchConfigDlg.java
@@ -84,7 +84,7 @@ public class ExpressSearchConfigDlg extends CustomDialog
     public static String ASCENDING  = null;
     public static String DESCENDING = null;
     
-    protected static final String[] TAB_HELP_CONTEXT = {"ESConfigSerachFields", "ESConfigResultsOrdering", "ESConfigRelatedTables"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    protected static final String[] TAB_HELP_CONTEXT = {"ESConfigSearchFields", "ESConfigResultsOrdering", "ESConfigRelatedTables"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     
     protected JTabbedPane                    tabbedPane;
     protected JList                          tableList;
