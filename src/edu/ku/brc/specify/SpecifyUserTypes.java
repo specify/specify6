2e3c71f3721366ec110d20d4a764836e9c1cc956
Rod Spears <rods@ku.edu>
2009-02-16 17:04:05 +0000

    Adding new UserType class
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@6095 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/SpecifyUserTypes.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2009  The University of Kansas
+ *
+ * [INSERT KU-APPROVED LICENSE TEXT HERE]
+ *
+ */
+package edu.ku.brc.specify;
+
+/**
+ * Implements an enum for describing user types.
+ * 
+ * @author rod
+ *
+ * @code_status Beta
+ *
+ * Feb 16, 2009
+ *
+ */
+public class SpecifyUserTypes
+{
+
+    public enum UserType 
+    {
+        Manager(0, "Manager"),
+        FullAccess(1, "FullAccess"),
+        LimitedAccess(2, "LimitedAccess"),
+        Guest(3, "Guest");
+
+        UserType(final int id, final String name)
+        {
+            this.id   = id;
+            this.name = name;
+        }
+
+        private int    id;
+        private String name;
+
+        public Integer getId()          { return id; }
+        public String  getName()        { return name; }
+        public String  getDirName()     { return name.toLowerCase(); }
+        
+        @Override
+        public String  toString()       { return name; }
+    }
+
+}
