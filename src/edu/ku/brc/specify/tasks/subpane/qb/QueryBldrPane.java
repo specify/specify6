cd482cbb706c35df8d18a7e0cb150e7cecb8753a
Tim Noble <timo@ku.edu>
2009-04-06 19:34:27 +0000

    query names are screened for invalid chars
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@6635 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/qb/QueryBldrPane.java
+++ src/edu/ku/brc/specify/tasks/subpane/qb/QueryBldrPane.java
@@ -22,6 +22,7 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -2098,6 +2099,11 @@ public class QueryBldrPane extends BaseSubPane implements QueryFieldPanelContain
                     {
                         UIRegistry.getStatusBar().setErrorMessage(
                                 getResourceString("QB_ENTER_A_NAME"));
+                    }
+                    else if (!UIHelper.isValidNameForDB(newQueryName))
+                    {
+                        UIRegistry.displayLocalizedStatusBarError("INVALID_CHARS_NAME");
+                        Toolkit.getDefaultToolkit().beep();
                     }
                     else
                     {
