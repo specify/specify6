c99e1d93409aab32e661e987e6cf99ce68f6a839
Rod Spears <rods@ku.edu>
2009-04-10 20:43:12 +0000

    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@6764 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- help/SpecifyHelp.hs
+++ help/SpecifyHelp.hs
@@ -1,52 +1,40 @@
-<?xml version='1.0' encoding='ISO-8859-1'  ?>
-<!DOCTYPE helpset
-  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
-         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
 <helpset version="2.0">
-
-<!-- title -->
-<title>Specify Help</title>
-
-<!-- maps -->
-<maps>
-    <homeID>main</homeID>
-    <mapref location="SpecifyHelp.jhm" />
-</maps>
-
-<!-- views -->
-<view mergetype="javax.help.UniteAppendMerge">
-    <name>TOC</name>
-    <label>Table Of Contents</label>
-    <type>javax.help.TOCView</type>
-    <data>SpecifyHelpTOC.xml</data>
-</view>
-
-<view mergetype="javax.help.SortMerge">
-    <name>Index</name>
-    <label>Index</label>
-    <type>javax.help.IndexView</type>
-    <data>SpecifyHelpIndex.xml</data>
-</view>
-
-<view>
-    <name>Search</name>
-    <label>Search</label>
-    <type>javax.help.SearchView</type>
-    <data engine="com.sun.java.help.search.DefaultSearchEngine">
-     JavaHelpSearch</data>
-</view>
-
-<view mergetype="javax.help.SortMerge">
-    <name>glossary</name>
-    <label>Glossary</label>
-    <type>javax.help.GlossaryView</type>
-    <data>SpecifyHelpGlossary.xml</data>
-</view>
-
-<view>
-    <name>favorites</name>
-    <label>Favorites</label>
-    <type>javax.help.FavoritesView</type>
-</view>
-
+	<title>Specify Help</title>
+	<maps>
+		<homeID></homeID>
+		<mapref location="SpecifyHelp.jhm"/>
+	</maps>
+	<view mergetype="javax.help.UniteAppendMerge">
+		<name>TOC</name>
+		<label>Table Of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>SpecifyHelpTOC.xml</data>
+	</view>
+	<view mergetype="javax.help.SortMerge">
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>SpecifyHelpIndex.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search</label>
+		<type>javax.help.SearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+	</view>
+	<view mergetype="javax.help.SortMerge">
+		<name>glossary</name>
+		<label>Glossary</label>
+		<type>javax.help.GlossaryView</type>
+		<data>SpecifyHelpGlossary.xml</data>
+	</view>
+	<view mergetype="javax.help.NoMerge">
+		<name>favorites</name>
+		<label>Favorites</label>
+		<type>javax.help.FavoritesView</type>
+	</view>
 </helpset>
