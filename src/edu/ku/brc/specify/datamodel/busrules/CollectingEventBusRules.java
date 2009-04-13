d175e9f231152de40e41ddea1af1ca8fcb844376
Tim Noble <timo@ku.edu>
2009-04-13 18:28:07 +0000

    restored CollectingEventBusRules class, with updated GPL
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@6794 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/busrules/CollectingEventBusRules.java
+++ src/edu/ku/brc/specify/datamodel/busrules/CollectingEventBusRules.java
@@ -17,3 +17,115 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+package edu.ku.brc.specify.datamodel.busrules;
+
+import javax.swing.JButton;
+
+import edu.ku.brc.af.core.AppContextMgr;
+import edu.ku.brc.af.ui.forms.BusinessRulesOkDeleteIFace;
+import edu.ku.brc.af.ui.forms.Viewable;
+import edu.ku.brc.dbsupport.DataProviderSessionIFace;
+import edu.ku.brc.specify.datamodel.CollectingEvent;
+import edu.ku.brc.specify.datamodel.Collection;
+import edu.ku.brc.specify.datamodel.Locality;
+
+/**
+ * @author rod 
+ *
+ * (original author was JDS)
+ *
+ * @code_status Alpha
+ *
+ * Jan 10, 2008
+ *
+ */
+public class CollectingEventBusRules extends AttachmentOwnerBaseBusRules
+{
+    /**
+     * 
+     */
+    public CollectingEventBusRules()
+    {
+        super(CollectingEvent.class);
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.af.ui.forms.BaseBusRules#initialize(edu.ku.brc.af.ui.forms.Viewable)
+     */
+    @Override
+    public void initialize(Viewable viewableArg)
+    {
+        super.initialize(viewableArg);
+        
+        if (AppContextMgr.getInstance().getClassObject(Collection.class).getIsEmbeddedCollectingEvent())
+        {
+            JButton newBtn = getNewBtn();
+            if (newBtn != null)
+            {
+                newBtn.setVisible(false);
+            }
+            JButton delBtn = getDelBtn();
+            if (delBtn != null)
+            {
+                delBtn.setVisible(false);
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.busrules.BaseBusRules#addChildrenToNewDataObjects(java.lang.Object)
+     */
+    @Override
+    public void addChildrenToNewDataObjects(final Object newDataObj)
+    {
+        super.addChildrenToNewDataObjects(newDataObj);
+        
+        if (false)
+        {
+            CollectingEvent ce = (CollectingEvent)newDataObj;
+            
+            if (ce.getLocality() == null)
+            {
+                Locality locality = new Locality();
+                locality.initialize();
+                
+                ce.addReference(locality, "locality");
+            }
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.busrules.BaseBusRules#okToDelete(java.lang.Object, edu.ku.brc.dbsupport.DataProviderSessionIFace, edu.ku.brc.ui.forms.BusinessRulesOkDeleteIFace)
+     */
+    @Override
+    public void okToDelete(final Object                     dataObj,
+                           final DataProviderSessionIFace   session,
+                           final BusinessRulesOkDeleteIFace deletable)
+    {
+        reasonList.clear();
+        
+        boolean isOK = false;
+        if (deletable != null)
+        {
+            CollectingEvent ce = (CollectingEvent)dataObj;
+            
+            Integer id = ce.getId();
+            if (id == null)
+            {
+                isOK = true;
+                
+            } else
+            {
+                Collection collection = AppContextMgr.getInstance().getClassObject(Collection.class);
+                int        count      = collection.getIsEmbeddedCollectingEvent() ? 1 : 0;
+                isOK = okToDelete(count, new String[] {"collectionobject", "CollectingEventID"}, ce.getId());
+            }
+            deletable.doDeleteDataObj(dataObj, session, isOK);
+            
+        } else
+        {
+            super.okToDelete(dataObj, session, deletable);
+        }
+    }
+
+}
