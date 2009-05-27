c3a41d243e8e50245a72f58bfe738f5366bf72c6
Tim Noble <timo@ku.edu>
2009-05-27 16:18:10 +0000

    removed concurrent form access
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@7156 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/busrules/BaseTreeBusRules.java
+++ src/edu/ku/brc/specify/datamodel/busrules/BaseTreeBusRules.java
@@ -86,7 +86,7 @@ public abstract class BaseTreeBusRules<T extends Treeable<T,D,I>,
                                        I extends TreeDefItemIface<T,D,I>>
                                        extends BaseBusRules
 {
-    public static final boolean ALLOW_CONCURRENT_FORM_ACCESS = true;
+    public static final boolean ALLOW_CONCURRENT_FORM_ACCESS = false;
 	
 	private static final Logger log = Logger.getLogger(BaseTreeBusRules.class);
     
