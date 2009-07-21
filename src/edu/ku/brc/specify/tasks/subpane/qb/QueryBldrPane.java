0898ba926d0eb0b874433f0632fb85853999a9ff
Tim Noble <timo@ku.edu>
2009-07-21 21:04:03 +0000

    skeleton for standalone schema export
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@7444 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/qb/QueryBldrPane.java
+++ src/edu/ku/brc/specify/tasks/subpane/qb/QueryBldrPane.java
@@ -50,7 +50,10 @@ import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicLong;
@@ -80,6 +83,7 @@ import javax.swing.ListModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.MouseInputAdapter;
@@ -95,10 +99,15 @@ import org.apache.log4j.Logger;
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.looks.plastic.PlasticLookAndFeel;
+import com.jgoodies.looks.plastic.theme.ExperienceBlue;
 
+import edu.ku.brc.af.auth.SecurityMgr;
+import edu.ku.brc.af.auth.UserAndMasterPasswordMgr;
 import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.core.ContextMgr;
 import edu.ku.brc.af.core.NavBoxLayoutManager;
+import edu.ku.brc.af.core.SchemaI18NService;
 import edu.ku.brc.af.core.SubPaneIFace;
 import edu.ku.brc.af.core.SubPaneMgr;
 import edu.ku.brc.af.core.Taskable;
@@ -109,18 +118,27 @@ import edu.ku.brc.af.core.db.DBTableIdMgr;
 import edu.ku.brc.af.core.db.DBTableInfo;
 import edu.ku.brc.af.core.db.DBRelationshipInfo.RelationshipType;
 import edu.ku.brc.af.core.expresssearch.QueryAdjusterForDomain;
+import edu.ku.brc.af.prefs.AppPreferences;
 import edu.ku.brc.af.tasks.subpane.BaseSubPane;
+import edu.ku.brc.af.ui.db.DatabaseLoginPanel;
 import edu.ku.brc.af.ui.db.ERTICaptionInfo;
 import edu.ku.brc.af.ui.db.ERTICaptionInfo.ColInfo;
 import edu.ku.brc.af.ui.forms.formatters.DataObjDataField;
 import edu.ku.brc.af.ui.forms.formatters.DataObjDataFieldFormatIFace;
 import edu.ku.brc.af.ui.forms.formatters.UIFieldFormatterIFace;
 import edu.ku.brc.af.ui.forms.formatters.UIFieldFormatterMgr;
+import edu.ku.brc.af.ui.weblink.WebLinkMgr;
+import edu.ku.brc.dbsupport.CustomQueryFactory;
+import edu.ku.brc.dbsupport.DBMSUserMgr;
 import edu.ku.brc.dbsupport.DataProviderFactory;
 import edu.ku.brc.dbsupport.DataProviderSessionIFace;
+import edu.ku.brc.dbsupport.HibernateUtil;
 import edu.ku.brc.dbsupport.RecordSetIFace;
 import edu.ku.brc.dbsupport.RecordSetItemIFace;
+import edu.ku.brc.dbsupport.SchemaUpdateService;
 import edu.ku.brc.helpers.SwingWorker;
+import edu.ku.brc.helpers.XMLHelper;
+import edu.ku.brc.specify.Specify;
 import edu.ku.brc.specify.conversion.BasicSQLUtils;
 import edu.ku.brc.specify.datamodel.DataModelObjBase;
 import edu.ku.brc.specify.datamodel.SpExportSchema;
@@ -140,6 +158,7 @@ import edu.ku.brc.specify.tasks.ReportsBaseTask;
 import edu.ku.brc.specify.tasks.subpane.ExpressSearchResultsPaneIFace;
 import edu.ku.brc.specify.tasks.subpane.JasperCompilerRunnable;
 import edu.ku.brc.specify.tasks.subpane.wb.WorkbenchJRDataSource;
+import edu.ku.brc.specify.tools.ireportspecify.MainFrameSpecify;
 import edu.ku.brc.ui.CommandAction;
 import edu.ku.brc.ui.CommandDispatcher;
 import edu.ku.brc.ui.CommandListener;
@@ -4467,7 +4486,271 @@ public class QueryBldrPane extends BaseSubPane implements QueryFieldPanelContain
     		return isRel;
     	}
     }
+
+    public static void main(String[] args)
+    {
+        log.debug("********* Current ["+(new File(".").getAbsolutePath())+"]"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        // This is for Windows and Exe4J, turn the args into System Properties
+        
+        UIRegistry.setEmbeddedDBDir(UIRegistry.getDefaultEmbeddedDBPath()); // on the local machine
+        
+        for (String s : args)
+        {
+            String[] pairs = s.split("="); //$NON-NLS-1$
+            if (pairs.length == 2)
+            {
+                if (pairs[0].startsWith("-D")) //$NON-NLS-1$
+                {
+                    System.setProperty(pairs[0].substring(2, pairs[0].length()), pairs[1]);
+                } 
+            } else
+            {
+                String symbol = pairs[0].substring(2, pairs[0].length());
+                System.setProperty(symbol, symbol);
+            }
+        }
+        
+        // Now check the System Properties
+        String appDir = System.getProperty("appdir");
+        if (StringUtils.isNotEmpty(appDir))
+        {
+            UIRegistry.setDefaultWorkingPath(appDir);
+        }
+        
+        String appdatadir = System.getProperty("appdatadir");
+        if (StringUtils.isNotEmpty(appdatadir))
+        {
+            UIRegistry.setBaseAppDataDir(appdatadir);
+        }
+        
+        String embeddeddbdir = System.getProperty("embeddeddbdir");
+        if (StringUtils.isNotEmpty(embeddeddbdir))
+        {
+            UIRegistry.setEmbeddedDBDir(embeddeddbdir);
+        }
+        
+        String mobile = System.getProperty("mobile");
+        if (StringUtils.isNotEmpty(mobile))
+        {
+            UIRegistry.setEmbeddedDBDir(UIRegistry.getMobileEmbeddedDBPath());
+        }
+
+        // Set App Name, MUST be done very first thing!
+        UIRegistry.setAppName("SchemaExporter");  //$NON-NLS-1$
+        //UIRegistry.setAppName("Specify");  //$NON-NLS-1$
+        
+        // Then set this
+        IconManager.setApplicationClass(Specify.class);
+        IconManager.loadIcons(XMLHelper.getConfigDir("icons_datamodel.xml")); //$NON-NLS-1$
+        IconManager.loadIcons(XMLHelper.getConfigDir("icons_plugins.xml")); //$NON-NLS-1$
+        IconManager.loadIcons(XMLHelper.getConfigDir("icons_disciplines.xml")); //$NON-NLS-1$
+
+        
+        
+        System.setProperty(AppContextMgr.factoryName,                   "edu.ku.brc.specify.config.SpecifyAppContextMgr");      // Needed by AppContextMgr //$NON-NLS-1$
+        System.setProperty(AppPreferences.factoryName,                  "edu.ku.brc.specify.config.AppPrefsDBIOIImpl");         // Needed by AppReferences //$NON-NLS-1$
+        System.setProperty("edu.ku.brc.ui.ViewBasedDialogFactoryIFace", "edu.ku.brc.specify.ui.DBObjDialogFactory");            // Needed By UIRegistry //$NON-NLS-1$ //$NON-NLS-2$
+        System.setProperty("edu.ku.brc.ui.forms.DraggableRecordIdentifierFactory", "edu.ku.brc.specify.ui.SpecifyDraggableRecordIdentiferFactory"); // Needed By the Form System //$NON-NLS-1$ //$NON-NLS-2$
+        System.setProperty("edu.ku.brc.dbsupport.AuditInterceptor",     "edu.ku.brc.specify.dbsupport.AuditInterceptor");       // Needed By the Form System for updating Lucene and logging transactions //$NON-NLS-1$ //$NON-NLS-2$
+        System.setProperty("edu.ku.brc.dbsupport.DataProvider",         "edu.ku.brc.specify.dbsupport.HibernateDataProvider");  // Needed By the Form System and any Data Get/Set //$NON-NLS-1$ //$NON-NLS-2$
+        System.setProperty("edu.ku.brc.ui.db.PickListDBAdapterFactory", "edu.ku.brc.specify.ui.db.PickListDBAdapterFactory");   // Needed By the Auto Cosmplete UI //$NON-NLS-1$ //$NON-NLS-2$
+        System.setProperty(CustomQueryFactory.factoryName,              "edu.ku.brc.specify.dbsupport.SpecifyCustomQueryFactory"); //$NON-NLS-1$
+        System.setProperty(UIFieldFormatterMgr.factoryName,             "edu.ku.brc.specify.ui.SpecifyUIFieldFormatterMgr");           // Needed for CatalogNumberign //$NON-NLS-1$
+        System.setProperty(QueryAdjusterForDomain.factoryName,          "edu.ku.brc.specify.dbsupport.SpecifyQueryAdjusterForDomain"); // Needed for ExpressSearch //$NON-NLS-1$
+        System.setProperty(SchemaI18NService.factoryName,               "edu.ku.brc.specify.config.SpecifySchemaI18NService");         // Needed for Localization and Schema //$NON-NLS-1$
+        System.setProperty(WebLinkMgr.factoryName,                      "edu.ku.brc.specify.config.SpecifyWebLinkMgr");                // Needed for WebLnkButton //$NON-NLS-1$
+        System.setProperty(SecurityMgr.factoryName,                     "edu.ku.brc.af.auth.specify.SpecifySecurityMgr");              // Needed for Tree Field Names //$NON-NLS-1$
+        System.setProperty(DBMSUserMgr.factoryName,                     "edu.ku.brc.dbsupport.MySQLDMBSUserMgr");
+        System.setProperty(SchemaUpdateService.factoryName,             "edu.ku.brc.specify.dbsupport.SpecifySchemaUpdateService");   // needed for updating the schema
+        
+        final AppPreferences localPrefs = AppPreferences.getLocalPrefs();
+        localPrefs.setDirPath(UIRegistry.getAppDataDir());
+        adjustLocaleFromPrefs();
+    	final String iRepPrefDir = localPrefs.getDirPath(); 
+        int mark = iRepPrefDir.lastIndexOf(UIRegistry.getAppName(), iRepPrefDir.length());
+        final String SpPrefDir = iRepPrefDir.substring(0, mark) + "Specify";
+        HibernateUtil.setListener("post-commit-update", new edu.ku.brc.specify.dbsupport.PostUpdateEventListener()); //$NON-NLS-1$
+        HibernateUtil.setListener("post-commit-insert", new edu.ku.brc.specify.dbsupport.PostInsertEventListener()); //$NON-NLS-1$
+        HibernateUtil.setListener("post-commit-delete", new edu.ku.brc.specify.dbsupport.PostDeleteEventListener()); //$NON-NLS-1$
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @SuppressWarnings("synthetic-access") //$NON-NLS-1$
+          public void run()
+            {
+                
+                try
+                {
+                    UIHelper.OSTYPE osType = UIHelper.getOSType();
+                    if (osType == UIHelper.OSTYPE.Windows )
+                    {
+                        UIManager.setLookAndFeel(new PlasticLookAndFeel());
+                        PlasticLookAndFeel.setPlasticTheme(new ExperienceBlue());
+                        
+                    } else if (osType == UIHelper.OSTYPE.Linux )
+                    {
+                        UIManager.setLookAndFeel(new PlasticLookAndFeel());
+                    }
+                }
+                catch (Exception e)
+                {
+                    edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                    edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(MainFrameSpecify.class, e);
+                    log.error("Can't change L&F: ", e); //$NON-NLS-1$
+                }
+                
+                DatabaseLoginPanel.MasterPasswordProviderIFace usrPwdProvider = new DatabaseLoginPanel.MasterPasswordProviderIFace()
+                {
+                    @Override
+                    public boolean hasMasterUserAndPwdInfo(final String username, final String password)
+                    {
+                        if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password))
+                        {
+                            UserAndMasterPasswordMgr.getInstance().setUsersUserName(username);
+                            UserAndMasterPasswordMgr.getInstance().setUsersPassword(password);
+                            boolean result = false;
+                            try
+                            {
+                            	try
+                            	{
+                            		AppPreferences.getLocalPrefs().flush();
+                            		AppPreferences.getLocalPrefs().setDirPath(SpPrefDir);
+                            		AppPreferences.getLocalPrefs().setProperties(null);
+                            		result = UserAndMasterPasswordMgr.getInstance().hasMasterUsernameAndPassword();
+                            	}
+                            	finally
+                            	{
+                            		AppPreferences.getLocalPrefs().flush();
+                            		AppPreferences.getLocalPrefs().setDirPath(iRepPrefDir);
+                            		AppPreferences.getLocalPrefs().setProperties(null);
+                            	}
+                            } catch (Exception e)
+                            {
+                            	edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                            	edu.ku.brc.exceptions.ExceptionTracker.getInstance()
+    								.capture(MainFrameSpecify.class, e);
+                            	result = false;
+                            }
+                            return result;
+                        }
+                        return false;
+                    }
+                    
+                    @Override
+                    public Pair<String, String> getUserNamePassword(final String username, final String password)
+                    {
+                        UserAndMasterPasswordMgr.getInstance().setUsersUserName(username);
+                        UserAndMasterPasswordMgr.getInstance().setUsersPassword(password);
+                        Pair<String, String> result = null;
+                        try
+                        {
+                        	try
+                        	{
+                        		AppPreferences.getLocalPrefs().flush();
+                        		AppPreferences.getLocalPrefs().setDirPath(SpPrefDir);
+                        		AppPreferences.getLocalPrefs().setProperties(null);
+                        		result = UserAndMasterPasswordMgr.getInstance().getUserNamePasswordForDB();
+                        	}
+                        	finally
+                        	{
+                        		AppPreferences.getLocalPrefs().flush();
+                        		AppPreferences.getLocalPrefs().setDirPath(iRepPrefDir);
+                        		AppPreferences.getLocalPrefs().setProperties(null);
+                        	}
+                        } catch (Exception e)
+                        {
+                        	edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                        	edu.ku.brc.exceptions.ExceptionTracker.getInstance()
+								.capture(MainFrameSpecify.class, e);
+                        	result = null;
+                        }
+                        return result;
+                    }
+                    @Override
+                    public boolean editMasterInfo(final String username, final boolean askFroCredentials)
+                    {
+                        boolean result = false;
+                    	try
+                        {
+                        	try
+                        	{
+                        		AppPreferences.getLocalPrefs().flush();
+                        		AppPreferences.getLocalPrefs()
+									.setDirPath(SpPrefDir);
+                        		AppPreferences.getLocalPrefs().setProperties(null);
+                        		result =  UserAndMasterPasswordMgr
+									.getInstance()
+									.editMasterInfo(username, askFroCredentials);
+                        	} finally
+                        	{
+                        		AppPreferences.getLocalPrefs().flush();
+                        		AppPreferences.getLocalPrefs().setDirPath(
+									iRepPrefDir);
+                        		AppPreferences.getLocalPrefs().setProperties(null);
+                        	}
+                        } catch (Exception e)
+                        {
+                        	edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                        	edu.ku.brc.exceptions.ExceptionTracker.getInstance()
+								.capture(MainFrameSpecify.class, e);
+                        	result = false;
+                        }
+                    	return result;
+                   }
+                };
+                String nameAndTitle = "Schema Exporter"; // I18N
+                UIRegistry.setRelease(true);
+                UIHelper.doLogin(usrPwdProvider, false, false, new SchemaExportLauncher(), "Specify", nameAndTitle, nameAndTitle, "SpecifyWhite32"); // true
+																																	// means
+																																	// do
+																																	// auto
+																																	// login
+																																	// if
+																																	// it
+																																	// can,
+																																	// second
+																																	// bool
+																																	// means
+																																	// use
+																																	// dialog
+																																	// instead
+																																	// of
+																																	// frame
+                
+                localPrefs.load();
+                
+            }
+        });
+
+       
+    }
     
+    protected static void adjustLocaleFromPrefs()
+    {
+        String language = AppPreferences.getLocalPrefs().get("locale.lang", null); //$NON-NLS-1$
+        if (language != null)
+        {
+            String country  = AppPreferences.getLocalPrefs().get("locale.country", null); //$NON-NLS-1$
+            String variant  = AppPreferences.getLocalPrefs().get("locale.var",     null); //$NON-NLS-1$
+            
+            Locale prefLocale = new Locale(language, country, variant);
+            
+            Locale.setDefault(prefLocale);
+            UIRegistry.setResourceLocale(prefLocale);
+        }
+        
+        try
+        {
+            ResourceBundle.getBundle("resources", Locale.getDefault()); //$NON-NLS-1$
+            
+        } catch (MissingResourceException ex)
+        {
+            edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+            edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(MainFrameSpecify.class, ex);
+            Locale.setDefault(Locale.ENGLISH);
+            UIRegistry.setResourceLocale(Locale.ENGLISH);
+        }
+        
+    }
 }
 
 
---
+++ src/edu/ku/brc/specify/tasks/subpane/qb/SchemaExportLauncher.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package edu.ku.brc.specify.tasks.subpane.qb;
+
+import static edu.ku.brc.ui.UIRegistry.getResourceString;
+
+import java.awt.Window;
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import edu.ku.brc.af.auth.SecurityMgr;
+import edu.ku.brc.af.core.AppContextMgr;
+import edu.ku.brc.af.core.PermissionIFace;
+import edu.ku.brc.af.core.SchemaI18NService;
+import edu.ku.brc.af.core.db.DBTableIdMgr;
+import edu.ku.brc.af.prefs.AppPreferences;
+import edu.ku.brc.af.ui.db.DatabaseLoginListener;
+import edu.ku.brc.specify.config.SpecifyAppPrefs;
+import edu.ku.brc.specify.datamodel.Collection;
+import edu.ku.brc.specify.datamodel.Discipline;
+import edu.ku.brc.specify.datamodel.SpLocaleContainer;
+import edu.ku.brc.specify.tools.ireportspecify.IReportLauncher;
+import edu.ku.brc.ui.UIRegistry;
+
+/**
+ * @author timo
+ *
+ */
+public class SchemaExportLauncher implements DatabaseLoginListener
+{
+    /* (non-Javadoc)
+     * @see edu.ku.brc.ui.db.DatabaseLoginListener#cancelled()
+     */
+    public void cancelled()
+    {
+        System.exit(0);
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.ui.db.DatabaseLoginListener#loggedIn(java.awt.Window, java.lang.String, java.lang.String)
+     */
+    public void loggedIn(Window window, String databaseName, String userName)
+    {
+        //System.out.println("Yup. You logged in.");
+
+        //snatched from Specify.restartApp...
+        AppPreferences.shutdownRemotePrefs();
+        
+        if (window != null)
+        {
+            window.setVisible(false);
+        }
+        
+        //moved here because context needs to be set before loading prefs, we need to know the SpecifyUser
+        AppContextMgr.CONTEXT_STATUS status = AppContextMgr.getInstance().setContext(databaseName, userName, true);
+       // AppContextMgr.getInstance().
+        SpecifyAppPrefs.initialPrefs();
+        
+        if (status == AppContextMgr.CONTEXT_STATUS.OK)
+        {
+            if (AppContextMgr.getInstance().getClassObject(Discipline.class) == null)
+            {
+                return;
+            }
+            
+            int disciplineeId = AppContextMgr.getInstance().getClassObject(Discipline.class).getDisciplineId();
+            SchemaI18NService.getInstance().loadWithLocale(SpLocaleContainer.CORE_SCHEMA, disciplineeId, DBTableIdMgr.getInstance(), Locale.getDefault());
+        } else if (status == AppContextMgr.CONTEXT_STATUS.Error)
+        {
+            if (AppContextMgr.getInstance().getClassObject(Collection.class) == null)
+            {
+                
+                // TODO This is really bad because there is a Database Login with no Specify login
+                JOptionPane.showMessageDialog(null, 
+                                              getResourceString("Specify.LOGIN_USER_MISMATCH"),  //$NON-NLS-1$
+                                              getResourceString("Specify.LOGIN_USER_MISMATCH_TITLE"),  //$NON-NLS-1$
+                                              JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            }
+        
+        }
+        //...end specify.restartApp snatch
+        
+        boolean canOpen = true;
+        if (AppContextMgr.isSecurityOn())
+        {
+            PermissionIFace permissions = SecurityMgr.getInstance().getPermission("Task.Reports");
+            canOpen = permissions.canView();
+        }
+        if (canOpen)
+        {
+            //openIReportEditor();
+        	openSchemaExporter();
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, getResourceString("IReportLauncher.PERMISSION_DENIED"),
+                        getResourceString("IReportLauncher.PERMISSION_DENIED_TITLE"),
+                        JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+    }
+
+    
+    /**
+     * 
+     */
+    protected void openSchemaExporter() 
+    {
+       SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                	UIRegistry.displayInfoMsgDlgLocalized("SOON_VERRY_SOON");
+                	System.exit(0);
+                }
+                catch (Exception e)
+                {
+                    edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                    edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(IReportLauncher.class, e);
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
