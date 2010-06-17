e26db0a5de1052c41380976a7e1d00b58530f29e
Rod Spears <rods@ku.edu>
2010-06-17 13:37:14 +0000

    Updating HostTaxonID PLugin with better error checking
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@8668 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/plugins/HostTaxonPlugin.java
+++ src/edu/ku/brc/specify/plugins/HostTaxonPlugin.java
@@ -19,16 +19,20 @@
 */
 package edu.ku.brc.specify.plugins;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang.StringUtils;
 
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
@@ -51,6 +55,7 @@ import edu.ku.brc.specify.datamodel.Discipline;
 import edu.ku.brc.specify.datamodel.Taxon;
 import edu.ku.brc.specify.datamodel.TaxonTreeDef;
 import edu.ku.brc.ui.UIHelper;
+import edu.ku.brc.ui.UIRegistry;
 
 /**
  * @author rod
@@ -74,7 +79,7 @@ public class HostTaxonPlugin extends UIPluginBase
     protected JTextField             text         = null;
     protected String                 relName       = null;
     protected Integer                hostCollId    = null;
-    protected Discipline             discipline    = null;
+    protected Discipline             rightsideDiscipline = null;
     protected TaxonTreeDef           taxonTreeDef  = null;
     protected Taxon                  taxon         = null;
     
@@ -153,13 +158,17 @@ public class HostTaxonPlugin extends UIPluginBase
     {
         super.initialize(propertiesArg, isViewModeArg);
         
+        CellConstraints cc = new CellConstraints();
+
+        String errMsg = null;
+        
         relName = propertiesArg.getProperty("relname", null);
-        if (relName != null)
+        if (StringUtils.isNotEmpty(relName))
         {
             Collection curCollection = AppContextMgr.getInstance().getClassObject(Collection.class);
             
             String sql = String.format("SELECT RightSideCollectionID FROM collectionreltype WHERE Name = \"%s\" AND LeftSideCollectionID = %d", relName,  curCollection.getId());
-            System.err.println(sql);
+            //System.err.println(sql);
             hostCollId = BasicSQLUtils.getCount(sql);
             
             if (hostCollId != null)
@@ -167,15 +176,27 @@ public class HostTaxonPlugin extends UIPluginBase
                 DataProviderSessionIFace session = null;
                 try
                 {
-                    session    = DataProviderFactory.getInstance().createSession();
+                    session             = DataProviderFactory.getInstance().createSession();
                     Collection rightCol = session.get(Collection.class, hostCollId);
-                    //discipline = session.get(Discipline.class, rightCol.getDiscipline().getId());
-                    discipline   = rightCol.getDiscipline();
-                    taxonTreeDef = discipline.getTaxonTreeDef();
+                    if (rightCol != null)
+                    {
+                        rightsideDiscipline = rightCol.getDiscipline();
+                        if (rightsideDiscipline != null)
+                        {
+                            taxonTreeDef = rightsideDiscipline.getTaxonTreeDef();
+                        } else
+                        {
+                            errMsg = UIRegistry.getLocalizedMessage("HostTaxonPlugin.ERR_MSG_RSD", relName);
+                        }
+                    } else
+                    {
+                        errMsg = UIRegistry.getLocalizedMessage("HostTaxonPlugin.ERR_MSG_RSC", relName);
+                    }
                     
                 } catch (Exception ex)
                 {
                     ex.printStackTrace();
+                    errMsg = UIRegistry.getLocalizedMessage("HostTaxonPlugin.ERR");
                     
                 } finally
                 {
@@ -184,46 +205,67 @@ public class HostTaxonPlugin extends UIPluginBase
                         session.close();
                     }
                 }
-            }
-        }
-        
-        if (discipline != null)
-        {
-            CellConstraints cc = new CellConstraints();
-            
-            if (isViewMode)
-            {
-                PanelBuilder pb = new PanelBuilder(new FormLayout("f:p:g",  "p"), this);
-                text = UIHelper.createTextField("");
-                ViewFactory.changeTextFieldUIForDisplay(text, false);
-                pb.add(text, cc.xy(1, 1));
-                
             } else
             {
-                PanelBuilder pb = new PanelBuilder(new FormLayout("f:p:g",  "f:p:g"), this);
-                
-                int btnOpts = ValComboBoxFromQuery.CREATE_EDIT_BTN | ValComboBoxFromQuery.CREATE_NEW_BTN | ValComboBoxFromQuery.CREATE_SEARCH_BTN;
-                cbx = new ValComboBoxFromQuery(DBTableIdMgr.getInstance().getInfoById(Taxon.getClassTableId()),
-                                        "fullName",
-                                        "fullName",
-                                        "fullName",
-                                        "%s",
-                                        null,
-                                        null,
-                                        "",
-                                        null, // helpContext
-                                        btnOpts);
-                pb.add(cbx, cc.xy(1, 1));
-                
-                adjustSQLTemplate();
-                
-                cbx.addListSelectionListener(new ListSelectionListener() {
-                    public void valueChanged(ListSelectionEvent e)
-                    {
-                        itemSelected();
-                    }
-                });
+                errMsg = UIRegistry.getLocalizedMessage("HostTaxonPlugin.ERR_BAD_NM", relName, curCollection.getCollectionName());
             }
+        
+            if (hostCollId != null && rightsideDiscipline != null)
+            {
+                if (isViewMode)
+                {
+                    PanelBuilder pb = new PanelBuilder(new FormLayout("f:p:g",  "p"), this);
+                    text = UIHelper.createTextField("");
+                    ViewFactory.changeTextFieldUIForDisplay(text, false);
+                    pb.add(text, cc.xy(1, 1));
+                    
+                } else
+                {
+                    PanelBuilder pb = new PanelBuilder(new FormLayout("f:p:g",  "f:p:g"), this);
+                    
+                    int btnOpts = ValComboBoxFromQuery.CREATE_EDIT_BTN | ValComboBoxFromQuery.CREATE_NEW_BTN | ValComboBoxFromQuery.CREATE_SEARCH_BTN;
+                    cbx = new ValComboBoxFromQuery(DBTableIdMgr.getInstance().getInfoById(Taxon.getClassTableId()),
+                                            "fullName",
+                                            "fullName",
+                                            "fullName",
+                                            "%s",
+                                            null,
+                                            null,
+                                            "",
+                                            null, // helpContext
+                                            btnOpts);
+                    pb.add(cbx, cc.xy(1, 1));
+                    
+                    adjustSQLTemplate();
+                    
+                    cbx.addListSelectionListener(new ListSelectionListener() {
+                        public void valueChanged(ListSelectionEvent e)
+                        {
+                            itemSelected();
+                        }
+                    });
+                }
+            }
+        } else
+        {
+            errMsg = UIRegistry.getLocalizedMessage("HostTaxonPlugin.ERR_MSG_REL_EMPTY");
+        }
+        
+        if (errMsg != null)
+        {
+            PanelBuilder pb = new PanelBuilder(new FormLayout("f:p:g",  "p"), this);
+            JButton btn = UIHelper.createI18NButton("HostTaxonPlugin.ERR_CLICK");
+            pb.add(btn, cc.xy(1, 1));
+            
+            final String dlgErrMsg = errMsg;
+            btn.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    UIRegistry.showError(dlgErrMsg);
+                }
+            });
         }
     }
     
@@ -266,9 +308,13 @@ public class HostTaxonPlugin extends UIPluginBase
             if (text != null)
             {
                 text.setText("");
-            } else
+                
+            } else if (cbx != null)
             {
                 cbx.setValue(null, null);
+            } else
+            {
+                System.err.println("cbx and text field were null");
             }
         }
         
