d2bf88847d2781f1b73628677d6c464aa6647792
Tim Noble <timo@ku.edu>
2011-04-15 19:38:14 +0000

    class hopefully useful for checking uniqueness of entries in a dataset column
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@9458 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

---
+++ src/edu/ku/brc/specify/tasks/subpane/wb/wbuploader/UniquenessChecker.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package edu.ku.brc.specify.tasks.subpane.wb.wbuploader;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.TreeMap;
+import java.util.Vector;
+
+/**
+ * @author timo
+ *
+ */
+public class UniquenessChecker
+{
+
+	protected TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+	
+	public UniquenessChecker()
+	{
+		
+	}
+	
+	public void clear()
+	{
+		map.clear();
+	}
+	
+	public Vector<Integer> setValue(int row, String value, boolean check)
+	{
+		map.put(row, value);
+		
+		if (check)
+		{
+			return checkValues(row, row);
+		}
+		
+		return null;
+	}
+
+	public boolean checkValue(int row, Vector<String> sortedValues) {
+		String value = map.get(row);
+		int idx = Collections.binarySearch(sortedValues, value);
+		int currIdx = idx;
+		//can actually quit once a second occurrence is found??
+		while (--currIdx >= 0 && sortedValues.get(currIdx).equals(value))
+			return true;
+		currIdx = idx + 1;
+		while (currIdx < sortedValues.size() && sortedValues.get(currIdx++).equals(value))
+			return true;
+		return false;
+	}
+	
+	public Vector<Integer> checkValues(int[] rows)
+	{
+		return checkValues(-1, -1, rows);
+	}
+	
+	public Vector<Integer> checkValues(int startRow, int endRow)
+	{
+		return checkValues(startRow, endRow, null);
+	}
+	
+	public Vector<Integer> checkValues(int startRow, int endRow, int[] rows)
+	{
+		Vector<Integer> result = new Vector<Integer>();
+		Collection<String> valuesColl = map.values();
+		Vector<String> values = new Vector<String>(valuesColl);
+		Collections.sort(values);
+		if (rows == null)
+		{
+			for (int row = startRow; row <= endRow; row++)
+			{
+				if (checkValue(row, values))
+				{
+					result.add(row);
+				}
+			}
+		} else 
+		{
+			for (Integer row : rows)
+			{
+				if (checkValue(row, values))
+				{
+					result.add(row);
+				}
+			}
+			
+		}
+		return result;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		UniquenessChecker uc = new UniquenessChecker();
+		for (int i = 0; i < 2000000; i++)
+		{
+			uc.setValue(i, String.valueOf(i), false);
+		}
+		System.out.println(uc.setValue(50, "4", true));
+		System.out.println(uc.setValue(500, "4", true));
+		System.out.println(uc.setValue(5000, "4", true));
+		System.out.println(uc.setValue(50000, "4", true));
+		System.out.println(uc.setValue(500000, "4", true));
+		System.out.println(uc.setValue(123, "bash", true));
+		Vector<Integer> duped = uc.checkValues(0, 1999999);
+		for (Integer dupe : duped)
+		{
+			System.out.println(dupe);
+		}
+		int[] checkees = {4, 50, 500, 5000, 50000, 500000, 666, 52, 37, 99};
+		duped = uc.checkValues(checkees);
+		System.out.println();
+		for (Integer dupe : duped)
+		{
+			System.out.println(dupe);
+		}		
+	}
+
+}
