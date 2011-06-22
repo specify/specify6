24b75909c1da2e0d860144fabc1f8f15654d4e71
Rod Spears <rods@ku.edu>
2011-06-22 19:16:43 +0000

    Bug 8439 - fixes null ptr exception
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@9753 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/plugins/LocalityWorldWindPlugin.java
+++ src/edu/ku/brc/specify/plugins/LocalityWorldWindPlugin.java
@@ -197,16 +197,18 @@ public class LocalityWorldWindPlugin extends LocalityGoogleEarthPlugin implement
         
         worldWindDlg.setVisible(true);
         
-        if (!worldWindDlg.isCancelled() && locality != null && latLonPlugin != null)
+        if (!worldWindDlg.isCancelled() && locality != null && latLonPlugin != null && latLonPnt != null)
         {
             FORMAT defaultFormat = convertIntToFORMAT(locality.getOriginalLatLongUnit());
-            
-            String latStr = ensureFormattedString(new BigDecimal(latLonPnt.getLatitude()),  locality.getLat2text(),  defaultFormat, LATLON.Latitude);
-            String lonStr = ensureFormattedString(new BigDecimal(latLonPnt.getLongitude()), locality.getLong2text(), defaultFormat, LATLON.Longitude);
-
-            latLonPlugin.setLatLon(latStr, lonStr, null, null);
-            
-            latLon = new Pair<BigDecimal, BigDecimal>(new BigDecimal(latLonPnt.getLatitude()), new BigDecimal(latLonPnt.getLongitude()));
+            if (defaultFormat != null)
+            {
+                String latStr = ensureFormattedString(new BigDecimal(latLonPnt.getLatitude()),  locality.getLat2text(),  defaultFormat, LATLON.Latitude);
+                String lonStr = ensureFormattedString(new BigDecimal(latLonPnt.getLongitude()), locality.getLong2text(), defaultFormat, LATLON.Longitude);
+    
+                latLonPlugin.setLatLon(latStr, lonStr, null, null);
+                
+                latLon = new Pair<BigDecimal, BigDecimal>(new BigDecimal(latLonPnt.getLatitude()), new BigDecimal(latLonPnt.getLongitude()));
+            }
         }
     }
     
