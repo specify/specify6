0a2873a26dfb3291cd293abddb650fe96cd08dc5
Tim Noble <timo@ku.edu>
2013-04-12 15:18:37 +0000

    DB create Wizard now only imports the latest darwincore.xsd file
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@10938 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/config/init/DataBuilder.java
+++ src/edu/ku/brc/specify/config/init/DataBuilder.java
@@ -3324,8 +3324,7 @@ public class DataBuilder
 //            localSession.flush();
             
             //ExportMappingTask imports don't update the schemaLocale tables
-            ExportMappingTask.importSchemaDefinition(new File(XMLHelper.getConfigDirPath("darwinCoreVertNet.xsd")), "VertNetDarwinCore", "1.21");
-            ExportMappingTask.importSchemaDefinition(new File(XMLHelper.getConfigDirPath("tdwg_dw_core.xsd")), "DarwinCoreTDWG", "1.4");
+            ExportMappingTask.importSchemaDefinition(new File(XMLHelper.getConfigDirPath("tdwg_dwcterms.xsd")), "DarwinCoreTDWG", "1.4");
             
         } catch (Exception ex)
         {
