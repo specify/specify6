6609a790e62e74c1038aa54ceaf9b729dc129db1
Rod Spears <rods@ku.edu>
2013-06-05 03:15:38 +0000

    Bug 9293 - Fixes Permit attachments not getting deleted (cascade rules)
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@11078 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/PermitAttachment.java
+++ src/edu/ku/brc/specify/datamodel/PermitAttachment.java
@@ -107,7 +107,7 @@ public class PermitAttachment extends DataModelObjBase implements ObjectAttachme
     }
 
     @ManyToOne(cascade = {}, fetch = FetchType.EAGER)
-    @Cascade( {CascadeType.SAVE_UPDATE, CascadeType.MERGE, CascadeType.LOCK} )
+    @Cascade( { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     @JoinColumn(name = "AttachmentID", nullable = false)
     @OrderBy("ordinal ASC")
     public Attachment getAttachment()
