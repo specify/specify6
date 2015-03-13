36d98c3152713ee8a7844f82da025027c932fc35
Rod Spears <rods@ku.edu>
2015-03-13 02:43:45 +0000

    Another try at fixing Bug 10061, hopefully won't create any regressions
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@11914 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/ui/Java2sAutoTextField.java
+++ src/edu/ku/brc/ui/Java2sAutoTextField.java
@@ -186,7 +186,7 @@ public class Java2sAutoTextField extends JTextField
         public void replace(int i, int j, String s, AttributeSet attributeset)
                 throws BadLocationException
         {
-            keyType = j == 0 ? INSERT : REMOVE;
+            keyType = INSERT;
             super.remove(i, j);
             insertString(i, s, attributeset);
         }
