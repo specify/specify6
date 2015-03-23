9a2ec262e7b95f003a9e32ff10868850ce205101
Tim Noble <timo@ku.edu>
2015-03-23 20:30:09 +0000

    changed copyright year to 2015 in Help | About dlg. #10079
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@11921 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/Specify.java
+++ src/edu/ku/brc/specify/Specify.java
@@ -2222,7 +2222,7 @@ public class Specify extends JPanel implements DatabaseLoginListener, CommandLis
         "<p>The Specify Software Project is "+ //$NON-NLS-1$
         "funded by the Advances in Biological Informatics Program, " + //$NON-NLS-1$
         "U.S. National Science Foundation  (Award DBI-0960913 and earlier awards).<br><br>" + //$NON-NLS-1$
-        "Specify 6 Copyright \u00A9 2014 University of Kansas Center for Research. " + 
+        "Specify 6 Copyright \u00A9 2015 University of Kansas Center for Research. " + 
         "Specify comes with ABSOLUTELY NO WARRANTY.<br><br>" + //$NON-NLS-1$
         "This is free software licensed under GNU General Public License 2 (GPL2).</P></font></html>"; //$NON-NLS-1$
 
