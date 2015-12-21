534f1898e1552a658aa28624c347d5544f0e6ac5
Tim Noble <timo@ku.edu>
2015-12-21 23:28:04 +0000

    fix for treatmenteventattachment save issue.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@12006 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/tasks/subpane/images/CollectionDataFetcher.java
+++ src/edu/ku/brc/specify/tasks/subpane/images/CollectionDataFetcher.java
@@ -105,6 +105,7 @@ import edu.ku.brc.specify.datamodel.StorageAttachment;
 import edu.ku.brc.specify.datamodel.Taxon;
 import edu.ku.brc.specify.datamodel.TaxonAttachment;
 import edu.ku.brc.specify.datamodel.TreatmentEvent;
+import edu.ku.brc.specify.datamodel.TreatmentEventAttachment;
 import edu.ku.brc.specify.datamodel.busrules.AgentBusRules;
 import edu.ku.brc.specify.ui.SpecifyUIFieldFormatterMgr;
 import edu.ku.brc.ui.DateWrapper;
@@ -162,7 +163,7 @@ public class CollectionDataFetcher
                 RepositoryAgreement.class, RepositoryAgreementAttachment.class,
                 Storage.class,            StorageAttachment.class,
                 Taxon.class,              TaxonAttachment.class,
-                TreatmentEvent.class,	  TreatmentEvent.class,
+                TreatmentEvent.class,	  TreatmentEventAttachment.class,
             };
         CollectionDataFetcher.attachmentClasses = new Class<?>[CollectionDataFetcher.attachmentClassesForMap.length/2];
         for (int i=0;i<CollectionDataFetcher.attachmentClassesForMap.length;i+=2)
