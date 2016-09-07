8f4d466ec37c363be2ca9f96a71706dd84cd19e6
Tim Noble <timo@ku.edu>
2016-09-07 20:34:50 +0000

    prevent deletes of SpecifyUsers who created schemamappings, queries, reports, workbenchtemplates, workbenches. #10208.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@12068 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/busrules/SpecifyUserBusRules.java
+++ src/edu/ku/brc/specify/datamodel/busrules/SpecifyUserBusRules.java
@@ -35,6 +35,7 @@ import edu.ku.brc.af.auth.UserAndMasterPasswordMgr;
 import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.ui.PasswordStrengthUI;
 import edu.ku.brc.af.ui.forms.BaseBusRules;
+import edu.ku.brc.af.ui.forms.BusinessRulesOkDeleteIFace;
 import edu.ku.brc.af.ui.forms.EditViewCompSwitcherPanel;
 import edu.ku.brc.af.ui.forms.FormDataObjIFace;
 import edu.ku.brc.af.ui.forms.Viewable;
@@ -396,4 +397,38 @@ public class SpecifyUserBusRules extends BaseBusRules
             }
         }
     }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.datamodel.busrules.BaseBusRules#okToDelete(java.lang.Object, edu.ku.brc.dbsupport.DataProviderSessionIFace, edu.ku.brc.ui.forms.BusinessRulesOkDeleteIFace)
+     */
+    @Override
+    public void okToDelete(final Object                     dataObj,
+                           final DataProviderSessionIFace   session,
+                           final BusinessRulesOkDeleteIFace deletable)
+    {
+        reasonList.clear();
+        
+        boolean isOK = false;
+        if (deletable != null)
+        {
+            SpecifyUser su = (SpecifyUser)dataObj;
+            
+            Integer id = su.getId();
+            if (id == null)
+            {
+                isOK = true;
+                
+            } else
+            {
+                isOK = okToDelete(0, new String[] {"spquery", "SpecifyUserID", "spreport", "SpecifyUserID", "workbench", "SpecifyUserID", "workbenchtemplate", "SpecifyUserID"}, 
+                		su.getId());
+            }
+            deletable.doDeleteDataObj(dataObj, session, isOK);
+            
+        } else
+        {
+            super.okToDelete(dataObj, session, deletable);
+        }
+    }
+
 }
