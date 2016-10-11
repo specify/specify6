241df4ec27e6c18ff7df9797f16731caa1aa3ccd
Tim Noble <timo@ku.edu>
2016-10-11 17:41:35 +0000

    repair for co.getCurrentDetermination() #10262.
    git-svn-id: https://svn.code.sf.net/p/specify/code/trunk/Specify@12091 ce9bf4bf-376f-1848-a496-a1c9fe3b1a39

--- src/edu/ku/brc/specify/datamodel/CollectionObject.java
+++ src/edu/ku/brc/specify/datamodel/CollectionObject.java
@@ -904,7 +904,7 @@ public void setReservedText3(String reservedText3) {
             	DataProviderSessionIFace session = null;
             	try {
             		session = DataProviderFactory.getInstance().createSession();
-            		String hql = "FROM determination WHERE collectionObjectId = " + this.getId() + " and isCurrent";
+            		String hql = "FROM Determination WHERE collectionObjectId = " + this.getId() + " and isCurrent";
             		return (Determination)session.getData(hql);
             	} finally {
             		if (session != null) {
