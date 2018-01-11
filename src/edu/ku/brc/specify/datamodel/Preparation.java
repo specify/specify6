c559313e79f5aaec91fdca75c8a501c9591509b1
timo <timo@ku.edu>
2018-01-11 16:52:24 -0600

    fixed problem with duplicate carried-forward prep guids #18

--- src/edu/ku/brc/specify/datamodel/Preparation.java
+++ src/edu/ku/brc/specify/datamodel/Preparation.java
@@ -985,7 +985,7 @@ public class Preparation extends CollectionMember implements AttachmentOwnerIFac
             obj.preparationAttrs.add(newPA);
             newPA.setPreparation(obj);
         }
-         
+        obj.setGUID();
         return obj;
     }
 
