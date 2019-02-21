b4ecd6118991c16817abd2e40e5c71e134f77ef9
timo <timo@ku.edu>
2019-02-21 13:06:38 -0600

    schema changes for field audit log. #239

--- src/edu/ku/brc/specify/datamodel/SpAuditLogField.java
+++ src/edu/ku/brc/specify/datamodel/SpAuditLogField.java
@@ -19,15 +19,7 @@
 */
 package edu.ku.brc.specify.datamodel;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
 /**
 
