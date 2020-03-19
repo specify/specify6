d67a1bd9b20adbe8b832603129d925e4e4cabc0f
timo11 <timo@ku.edu>
2020-03-19 12:55:31 -0500

    updated about form copyright info. #636

--- src/edu/ku/brc/specify/Specify.java
+++ src/edu/ku/brc/specify/Specify.java
@@ -2172,26 +2172,6 @@ public class Specify extends JPanel implements DatabaseLoginListener, CommandLis
         UIHelper.centerAndShow(aboutDlg);
     }
 
-    /* Copyright (C) 2020, Specify Collections Consortium
-     *
-     * Specify Collections Consortium, Biodiversity Institute, University of Kansas,
-     * 1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA, support@specifysoftware.org
-     *
-     * This program is free software; you can redistribute it and/or
-     * modify it under the terms of the GNU General Public License
-     * as published by the Free Software Foundation; either version 2
-     * of the License, or (at your option) any later version.
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with this program; if not, write to the Free Software
-     * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-     */
-
     /**
      * Returns a standard String for the about box
      * @param appNameArg the application name  
