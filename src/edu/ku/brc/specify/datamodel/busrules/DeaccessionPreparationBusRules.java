1fd39036784ff4c65505ad6be588ed2bd61d0eb9
timo11 <timo@ku.edu>
2020-05-06 14:34:24 -0500

    prep->deaccprep functional (in fish/manager/fish.views)

--- src/edu/ku/brc/specify/datamodel/busrules/DeaccessionPreparationBusRules.java
+++ src/edu/ku/brc/specify/datamodel/busrules/DeaccessionPreparationBusRules.java
@@ -20,7 +20,7 @@ package edu.ku.brc.specify.datamodel.busrules;
 import edu.ku.brc.af.ui.forms.BaseBusRules;
 import edu.ku.brc.specify.datamodel.DeaccessionPreparation;
 import edu.ku.brc.specify.datamodel.LoanReturnPreparation;
-import edu.ku.brc.specify.datamodel.Locality;
+//import edu.ku.brc.specify.datamodel.Preparation;
 import edu.ku.brc.ui.UIRegistry;
 
 public class DeaccessionPreparationBusRules extends BaseBusRules {
@@ -32,12 +32,15 @@ public class DeaccessionPreparationBusRules extends BaseBusRules {
     @Override
     public void afterFillForm(final Object dataObj) {
         super.afterFillForm(dataObj);
-        if (dataObj != null && isOnLoanReturnForm(dataObj)) {
-            DeaccessionPreparation dp = (DeaccessionPreparation)dataObj;
-            if (dp.getId() == null &&
-                    dp.getLoanReturnPreparation() != null &&
-                    dp.getLoanReturnPreparation().getLoanPreparation() != null) {
-                dp.setPreparation(dp.getLoanReturnPreparation().getLoanPreparation().getPreparation());
+        if (dataObj != null) {
+            DeaccessionPreparation dp = (DeaccessionPreparation) dataObj;
+            if (dp.getId() == null) {
+                if (isOnLoanReturnForm(dataObj)) {
+                    if (dp.getLoanReturnPreparation() != null &&
+                            dp.getLoanReturnPreparation().getLoanPreparation() != null) {
+                        dp.setPreparation(dp.getLoanReturnPreparation().getLoanPreparation().getPreparation());
+                    }
+                }
             }
         }
     }
@@ -53,6 +56,10 @@ public class DeaccessionPreparationBusRules extends BaseBusRules {
     private boolean isOnLoanReturnForm(final Object dataObj) {
         return LoanReturnPreparation.class.equals(getContext(dataObj));
     }
+
+//    private boolean isOnPreparationForm(final Object dataObj) {
+//        return Preparation.class.equals(getContext(dataObj));
+//    }
 
     /* (non-Javadoc)
      * @see edu.ku.brc.af.ui.forms.BaseBusRules#isOkToAddSibling(java.lang.Object)
