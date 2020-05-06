9b9f2ed6d0447d0e1469a4c37767f371886e6c20
timo11 <timo@ku.edu>
2020-05-06 11:32:08 -0500

    loanreturn->deaccession functional

--- src/edu/ku/brc/specify/datamodel/busrules/DeaccessionPreparationBusRules.java
+++ src/edu/ku/brc/specify/datamodel/busrules/DeaccessionPreparationBusRules.java
@@ -18,12 +18,45 @@
 package edu.ku.brc.specify.datamodel.busrules;
 
 import edu.ku.brc.af.ui.forms.BaseBusRules;
+import edu.ku.brc.af.ui.forms.FormViewObj;
+import edu.ku.brc.af.ui.forms.Viewable;
+import edu.ku.brc.af.ui.forms.validation.ValSpinner;
+import edu.ku.brc.specify.datamodel.DeaccessionPreparation;
+import edu.ku.brc.specify.datamodel.LoanReturnPreparation;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 
 
 public class DeaccessionPreparationBusRules extends BaseBusRules {
 
-    public DeaccessionPreparationBusRules()
-    {
+    public DeaccessionPreparationBusRules() {
         super(DeaccessionPreparationBusRules.class);
+    }
+
+    @Override
+    public void afterFillForm(final Object dataObj) {
+        super.afterFillForm(dataObj);
+        if (dataObj != null && isOnLoanReturnForm(dataObj)) {
+            DeaccessionPreparation dp = (DeaccessionPreparation)dataObj;
+            if (dp.getId() == null &&
+                    dp.getLoanReturnPreparation() != null &&
+                    dp.getLoanReturnPreparation().getLoanPreparation() != null) {
+                dp.setPreparation(dp.getLoanReturnPreparation().getLoanPreparation().getPreparation());
+            }
+        }
+    }
+
+    private Class<?> getContext(final Object dataObj) {
+        Class<?> result = null;
+        if (formViewObj != null && formViewObj.getParentDataObj() != null){
+            result = formViewObj.getParentDataObj().getClass();
+        }
+        return result;
+    }
+
+    private boolean isOnLoanReturnForm(final Object dataObj) {
+        return LoanReturnPreparation.class.equals(getContext(dataObj));
     }
 }
