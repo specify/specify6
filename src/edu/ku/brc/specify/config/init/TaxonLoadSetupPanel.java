7f2631ea626070c7c1bfa00bc3854a7dae29cf08
timo11 <timo@ku.edu>
2020-07-22 15:51:35 -0500

    #786, #788

--- src/edu/ku/brc/specify/config/init/TaxonLoadSetupPanel.java
+++ src/edu/ku/brc/specify/config/init/TaxonLoadSetupPanel.java
@@ -175,11 +175,11 @@ public class TaxonLoadSetupPanel extends BaseSetupPanel
         builder.add(createScrollPane(descTA),          cc.xy(3, row));
         row += 2;
         
-        othSep = builder.addSeparator(UIRegistry.getResourceString("TFD_SEP_OTH"), cc.xyw(1, row, 3));
+        //othSep = builder.addSeparator(UIRegistry.getResourceString("TFD_SEP_OTH"), cc.xyw(1, row, 3));
         row += 2;
         
-        builder.add(otherLbl = createI18NFormLabel("TFD_OTHER_LBL"), cc.xy(1, row));
-        builder.add(otherBrw,        cc.xy(3, row));
+        //builder.add(otherLbl = createI18NFormLabel("TFD_OTHER_LBL"), cc.xy(1, row));
+        //builder.add(otherBrw,        cc.xy(3, row));
         row += 2;
         
         updateBtnUI();
@@ -284,7 +284,7 @@ public class TaxonLoadSetupPanel extends BaseSetupPanel
             String downloadHTTP = UIRegistry.getResourceString(DWNLD_TAX_URL);
             if (StringUtils.isNotEmpty(downloadHTTP))
             {
-                url = new URL(downloadHTTP + "/taxonfiles.xml");
+                url = new URL(downloadHTTP + "/taxonfiles_2.xml");
                 URLConnection  conn = url.openConnection();
                 BufferedReader in   = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 String line;
@@ -387,7 +387,7 @@ public class TaxonLoadSetupPanel extends BaseSetupPanel
             } else
             {
                 fileCBX.setEnabled(false);
-                //preloadChk.setEnabled(false);
+                preloadChk.setEnabled(false);
             }
         }
     }
@@ -562,9 +562,9 @@ public class TaxonLoadSetupPanel extends BaseSetupPanel
         
         if (doAll)
         {
-            othSep.setEnabled(otherEnabled);
-            otherLbl.setEnabled(otherEnabled);
-            otherBrw.setEnabled(otherEnabled);
+            //othSep.setEnabled(otherEnabled);
+            //otherLbl.setEnabled(otherEnabled);
+            //otherBrw.setEnabled(otherEnabled);
         }
     }
     
