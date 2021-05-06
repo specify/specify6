56fa98f2d7d3b405e70474a24b0aa36bcd990670
timo <timo@ku.edu>
2021-05-06 15:31:14 -0500

    Return Loan unavailable when changes need saving. #897

--- src/edu/ku/brc/specify/dbsupport/SpecifySchemaUpdateService.java
+++ src/edu/ku/brc/specify/dbsupport/SpecifySchemaUpdateService.java
@@ -187,7 +187,7 @@ public class SpecifySchemaUpdateService extends SchemaUpdateService
 {
     protected static final Logger  log = Logger.getLogger(SpecifySchemaUpdateService.class);
     
-    private final int OVERALL_TOTAL = 65; //the number of incOverall() calls (+1 or +2)
+    private final int OVERALL_TOTAL = 66; //the number of incOverall() calls (+1 or +2)
 
     private static final String TINYINT4 = "TINYINT(4)";
     private static final String INT11    = "INT(11)";
