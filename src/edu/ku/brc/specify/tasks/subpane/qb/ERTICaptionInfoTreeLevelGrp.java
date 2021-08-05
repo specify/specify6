7e1117cbf0675068be7631fdc05a4f3dbf9062f6
timo11 <timo@ku.edu>
2021-08-05 17:21:44 -0500

    update copyright year to 2021

--- src/edu/ku/brc/specify/tasks/subpane/qb/ERTICaptionInfoTreeLevelGrp.java
+++ src/edu/ku/brc/specify/tasks/subpane/qb/ERTICaptionInfoTreeLevelGrp.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020, Specify Collections Consortium
+/* Copyright (C) 2021, Specify Collections Consortium
  * 
  * Modifications Copyright (C) 2013 President and Fellows of Harvard College
  * 
